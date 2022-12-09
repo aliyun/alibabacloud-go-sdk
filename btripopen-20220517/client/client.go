a422503b57caa99328e208ba74d31538c85a0ed2
sdk-team <sdk-team@alibabacloud.com>
2022-12-09 06:49:33 +0000

    Rebuild core verison form v1 to v2.

--- btripopen-20220517/client/client.go
+++ btripopen-20220517/client/client.go
@@ -5,12 +5,194 @@
 package client
 
 import (
-	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
+	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
 	endpointutil "github.com/alibabacloud-go/endpoint-util/service"
 	openapiutil "github.com/alibabacloud-go/openapi-util/service"
-	util "github.com/alibabacloud-go/tea-utils/service"
+	util "github.com/alibabacloud-go/tea-utils/v2/service"
 	"github.com/alibabacloud-go/tea/tea"
 )
+
+type ScanMsgListRequest struct {
+	Biz          *string `json:"Biz,omitempty" xml:"Biz,omitempty"`
+	ChannelType  *string `json:"ChannelType,omitempty" xml:"ChannelType,omitempty"`
+	CorpId       *string `json:"CorpId,omitempty" xml:"CorpId,omitempty"`
+	ScanSequence *int32  `json:"ScanSequence,omitempty" xml:"ScanSequence,omitempty"`
+	SequenceId   *int64  `json:"SequenceId,omitempty" xml:"SequenceId,omitempty"`
+	Size         *int32  `json:"Size,omitempty" xml:"Size,omitempty"`
+	UserId       *string `json:"UserId,omitempty" xml:"UserId,omitempty"`
+}
+
+func (s ScanMsgListRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ScanMsgListRequest) GoString() string {
+	return s.String()
+}
+
+func (s *ScanMsgListRequest) SetBiz(v string) *ScanMsgListRequest {
+	s.Biz = &v
+	return s
+}
+
+func (s *ScanMsgListRequest) SetChannelType(v string) *ScanMsgListRequest {
+	s.ChannelType = &v
+	return s
+}
+
+func (s *ScanMsgListRequest) SetCorpId(v string) *ScanMsgListRequest {
+	s.CorpId = &v
+	return s
+}
+
+func (s *ScanMsgListRequest) SetScanSequence(v int32) *ScanMsgListRequest {
+	s.ScanSequence = &v
+	return s
+}
+
+func (s *ScanMsgListRequest) SetSequenceId(v int64) *ScanMsgListRequest {
+	s.SequenceId = &v
+	return s
+}
+
+func (s *ScanMsgListRequest) SetSize(v int32) *ScanMsgListRequest {
+	s.Size = &v
+	return s
+}
+
+func (s *ScanMsgListRequest) SetUserId(v string) *ScanMsgListRequest {
+	s.UserId = &v
+	return s
+}
+
+type ScanMsgListResponseBody struct {
+	Code      *int32                           `json:"Code,omitempty" xml:"Code,omitempty"`
+	ExtMsg    *string                          `json:"ExtMsg,omitempty" xml:"ExtMsg,omitempty"`
+	Module    []*ScanMsgListResponseBodyModule `json:"Module,omitempty" xml:"Module,omitempty" type:"Repeated"`
+	Msg       *string                          `json:"Msg,omitempty" xml:"Msg,omitempty"`
+	RequestId *string                          `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	Success   *bool                            `json:"Success,omitempty" xml:"Success,omitempty"`
+}
+
+func (s ScanMsgListResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ScanMsgListResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *ScanMsgListResponseBody) SetCode(v int32) *ScanMsgListResponseBody {
+	s.Code = &v
+	return s
+}
+
+func (s *ScanMsgListResponseBody) SetExtMsg(v string) *ScanMsgListResponseBody {
+	s.ExtMsg = &v
+	return s
+}
+
+func (s *ScanMsgListResponseBody) SetModule(v []*ScanMsgListResponseBodyModule) *ScanMsgListResponseBody {
+	s.Module = v
+	return s
+}
+
+func (s *ScanMsgListResponseBody) SetMsg(v string) *ScanMsgListResponseBody {
+	s.Msg = &v
+	return s
+}
+
+func (s *ScanMsgListResponseBody) SetRequestId(v string) *ScanMsgListResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *ScanMsgListResponseBody) SetSuccess(v bool) *ScanMsgListResponseBody {
+	s.Success = &v
+	return s
+}
+
+type ScanMsgListResponseBodyModule struct {
+	Biz         *string `json:"Biz,omitempty" xml:"Biz,omitempty"`
+	Content     *string `json:"Content,omitempty" xml:"Content,omitempty"`
+	HaveRedDot  *bool   `json:"HaveRedDot,omitempty" xml:"HaveRedDot,omitempty"`
+	SendTime    *string `json:"SendTime,omitempty" xml:"SendTime,omitempty"`
+	SequenceId  *int64  `json:"SequenceId,omitempty" xml:"SequenceId,omitempty"`
+	ShowMsg     *bool   `json:"ShowMsg,omitempty" xml:"ShowMsg,omitempty"`
+	UnReadCount *int32  `json:"UnReadCount,omitempty" xml:"UnReadCount,omitempty"`
+}
+
+func (s ScanMsgListResponseBodyModule) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ScanMsgListResponseBodyModule) GoString() string {
+	return s.String()
+}
+
+func (s *ScanMsgListResponseBodyModule) SetBiz(v string) *ScanMsgListResponseBodyModule {
+	s.Biz = &v
+	return s
+}
+
+func (s *ScanMsgListResponseBodyModule) SetContent(v string) *ScanMsgListResponseBodyModule {
+	s.Content = &v
+	return s
+}
+
+func (s *ScanMsgListResponseBodyModule) SetHaveRedDot(v bool) *ScanMsgListResponseBodyModule {
+	s.HaveRedDot = &v
+	return s
+}
+
+func (s *ScanMsgListResponseBodyModule) SetSendTime(v string) *ScanMsgListResponseBodyModule {
+	s.SendTime = &v
+	return s
+}
+
+func (s *ScanMsgListResponseBodyModule) SetSequenceId(v int64) *ScanMsgListResponseBodyModule {
+	s.SequenceId = &v
+	return s
+}
+
+func (s *ScanMsgListResponseBodyModule) SetShowMsg(v bool) *ScanMsgListResponseBodyModule {
+	s.ShowMsg = &v
+	return s
+}
+
+func (s *ScanMsgListResponseBodyModule) SetUnReadCount(v int32) *ScanMsgListResponseBodyModule {
+	s.UnReadCount = &v
+	return s
+}
+
+type ScanMsgListResponse struct {
+	Headers    map[string]*string       `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
+	StatusCode *int32                   `json:"statusCode,omitempty" xml:"statusCode,omitempty" require:"true"`
+	Body       *ScanMsgListResponseBody `json:"body,omitempty" xml:"body,omitempty" require:"true"`
+}
+
+func (s ScanMsgListResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ScanMsgListResponse) GoString() string {
+	return s.String()
+}
+
+func (s *ScanMsgListResponse) SetHeaders(v map[string]*string) *ScanMsgListResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *ScanMsgListResponse) SetStatusCode(v int32) *ScanMsgListResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *ScanMsgListResponse) SetBody(v *ScanMsgListResponseBody) *ScanMsgListResponse {
+	s.Body = v
+	return s
+}
 
 type TakeAccessTokenRequest struct {
 	AppKey    *string `json:"app_key,omitempty" xml:"app_key,omitempty"`
@@ -36,12 +218,11 @@ func (s *TakeAccessTokenRequest) SetAppSecret(v string) *TakeAccessTokenRequest 
 }
 
 type TakeAccessTokenResponseBody struct {
-	Code    *string                          `json:"code,omitempty" xml:"code,omitempty"`
-	Data    *TakeAccessTokenResponseBodyData `json:"data,omitempty" xml:"data,omitempty" type:"Struct"`
-	Message *string                          `json:"message,omitempty" xml:"message,omitempty"`
-	// Id of the request
-	RequestId *string `json:"requestId,omitempty" xml:"requestId,omitempty"`
-	Success   *string `json:"success,omitempty" xml:"success,omitempty"`
+	Code      *string                          `json:"code,omitempty" xml:"code,omitempty"`
+	Data      *TakeAccessTokenResponseBodyData `json:"data,omitempty" xml:"data,omitempty" type:"Struct"`
+	Message   *string                          `json:"message,omitempty" xml:"message,omitempty"`
+	RequestId *string                          `json:"requestId,omitempty" xml:"requestId,omitempty"`
+	Success   *string                          `json:"success,omitempty" xml:"success,omitempty"`
 }
 
 func (s TakeAccessTokenResponseBody) String() string {
@@ -174,6 +355,76 @@ func (client *Client) GetEndpoint(productId *string, regionId *string, endpointR
 		return _result, _err
 	}
 	_result = _body
+	return _result, _err
+}
+
+func (client *Client) ScanMsgList(request *ScanMsgListRequest) (_result *ScanMsgListResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &ScanMsgListResponse{}
+	_body, _err := client.ScanMsgListWithOptions(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+func (client *Client) ScanMsgListWithOptions(request *ScanMsgListRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *ScanMsgListResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	body := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.Biz)) {
+		body["Biz"] = request.Biz
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.ChannelType)) {
+		body["ChannelType"] = request.ChannelType
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.CorpId)) {
+		body["CorpId"] = request.CorpId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.ScanSequence)) {
+		body["ScanSequence"] = request.ScanSequence
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.SequenceId)) {
+		body["SequenceId"] = request.SequenceId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.Size)) {
+		body["Size"] = request.Size
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.UserId)) {
+		body["UserId"] = request.UserId
+	}
+
+	req := &openapi.OpenApiRequest{
+		Headers: headers,
+		Body:    openapiutil.ParseToMap(body),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("ScanMsgList"),
+		Version:     tea.String("2022-05-17"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/ScanMsgList"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("ROA"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &ScanMsgListResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
 	return _result, _err
 }
 
--- btripopen-20220517/go.mod
+++ btripopen-20220517/go.mod
@@ -1,11 +1,11 @@
-module github.com/alibabacloud-go/btripopen-20220517
+module github.com/alibabacloud-go/btripopen-20220517/v2
 
 go 1.15
 
 require (
-	github.com/alibabacloud-go/darabonba-openapi v0.1.16
+	github.com/alibabacloud-go/darabonba-openapi/v2 v2.0.2
 	github.com/alibabacloud-go/endpoint-util v1.1.0
-	github.com/alibabacloud-go/openapi-util v0.0.11
-	github.com/alibabacloud-go/tea v1.1.17
-	github.com/alibabacloud-go/tea-utils v1.4.3
+	github.com/alibabacloud-go/openapi-util v0.1.0
+	github.com/alibabacloud-go/tea v1.1.19
+	github.com/alibabacloud-go/tea-utils/v2 v2.0.1
 )
