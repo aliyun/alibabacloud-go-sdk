d46dcfe230588ddd3524887b5480a620a89c47a9
sdk-team <sdk-team@alibabacloud.com>
2022-12-14 01:43:41 +0000

    Update api ExecuteServerSideVerification The base64 data format is changed from string byte to string.

--- facebody-20200910/client/client.go
+++ facebody-20200910/client/client.go
@@ -5,20 +5,17 @@
 package client
 
 import (
-	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
+	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
 	endpointutil "github.com/alibabacloud-go/endpoint-util/service"
 	openapiutil "github.com/alibabacloud-go/openapi-util/service"
-	util "github.com/alibabacloud-go/tea-utils/service"
+	util "github.com/alibabacloud-go/tea-utils/v2/service"
 	"github.com/alibabacloud-go/tea/tea"
 )
 
 type DetectIPCPedestrianOptimizedRequest struct {
-	// image data
+	Height    *int64 `json:"height,omitempty" xml:"height,omitempty"`
 	ImageData []byte `json:"imageData,omitempty" xml:"imageData,omitempty"`
-	// image width
-	Width *int64 `json:"width,omitempty" xml:"width,omitempty"`
-	// image height
-	Height *int64 `json:"height,omitempty" xml:"height,omitempty"`
+	Width     *int64 `json:"width,omitempty" xml:"width,omitempty"`
 }
 
 func (s DetectIPCPedestrianOptimizedRequest) String() string {
@@ -27,6 +24,11 @@ func (s DetectIPCPedestrianOptimizedRequest) String() string {
 
 func (s DetectIPCPedestrianOptimizedRequest) GoString() string {
 	return s.String()
+}
+
+func (s *DetectIPCPedestrianOptimizedRequest) SetHeight(v int64) *DetectIPCPedestrianOptimizedRequest {
+	s.Height = &v
+	return s
 }
 
 func (s *DetectIPCPedestrianOptimizedRequest) SetImageData(v []byte) *DetectIPCPedestrianOptimizedRequest {
@@ -39,16 +41,9 @@ func (s *DetectIPCPedestrianOptimizedRequest) SetWidth(v int64) *DetectIPCPedest
 	return s
 }
 
-func (s *DetectIPCPedestrianOptimizedRequest) SetHeight(v int64) *DetectIPCPedestrianOptimizedRequest {
-	s.Height = &v
-	return s
-}
-
 type DetectIPCPedestrianOptimizedResponseBody struct {
-	// data
-	Data *DetectIPCPedestrianOptimizedResponseBodyData `json:"Data,omitempty" xml:"Data,omitempty" type:"Struct"`
-	// requestId
-	RequestId *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	Data      *DetectIPCPedestrianOptimizedResponseBodyData `json:"Data,omitempty" xml:"Data,omitempty" type:"Struct"`
+	RequestId *string                                       `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
 }
 
 func (s DetectIPCPedestrianOptimizedResponseBody) String() string {
@@ -70,7 +65,6 @@ func (s *DetectIPCPedestrianOptimizedResponseBody) SetRequestId(v string) *Detec
 }
 
 type DetectIPCPedestrianOptimizedResponseBodyData struct {
-	// imageInfoList
 	ImageInfoList []*DetectIPCPedestrianOptimizedResponseBodyDataImageInfoList `json:"ImageInfoList,omitempty" xml:"ImageInfoList,omitempty" type:"Repeated"`
 }
 
@@ -88,7 +82,6 @@ func (s *DetectIPCPedestrianOptimizedResponseBodyData) SetImageInfoList(v []*Det
 }
 
 type DetectIPCPedestrianOptimizedResponseBodyDataImageInfoList struct {
-	// Elements
 	Elements []*DetectIPCPedestrianOptimizedResponseBodyDataImageInfoListElements `json:"Elements,omitempty" xml:"Elements,omitempty" type:"Repeated"`
 }
 
@@ -106,9 +99,7 @@ func (s *DetectIPCPedestrianOptimizedResponseBodyDataImageInfoList) SetElements(
 }
 
 type DetectIPCPedestrianOptimizedResponseBodyDataImageInfoListElements struct {
-	// box array
 	Boxes []*int64 `json:"Boxes,omitempty" xml:"Boxes,omitempty" type:"Repeated"`
-	// score
 	Score *float32 `json:"Score,omitempty" xml:"Score,omitempty"`
 }
 
@@ -131,8 +122,9 @@ func (s *DetectIPCPedestrianOptimizedResponseBodyDataImageInfoListElements) SetS
 }
 
 type DetectIPCPedestrianOptimizedResponse struct {
-	Headers map[string]*string                        `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
-	Body    *DetectIPCPedestrianOptimizedResponseBody `json:"body,omitempty" xml:"body,omitempty" require:"true"`
+	Headers    map[string]*string                        `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
+	StatusCode *int32                                    `json:"statusCode,omitempty" xml:"statusCode,omitempty" require:"true"`
+	Body       *DetectIPCPedestrianOptimizedResponseBody `json:"body,omitempty" xml:"body,omitempty" require:"true"`
 }
 
 func (s DetectIPCPedestrianOptimizedResponse) String() string {
@@ -148,6 +140,11 @@ func (s *DetectIPCPedestrianOptimizedResponse) SetHeaders(v map[string]*string) 
 	return s
 }
 
+func (s *DetectIPCPedestrianOptimizedResponse) SetStatusCode(v int32) *DetectIPCPedestrianOptimizedResponse {
+	s.StatusCode = &v
+	return s
+}
+
 func (s *DetectIPCPedestrianOptimizedResponse) SetBody(v *DetectIPCPedestrianOptimizedResponseBody) *DetectIPCPedestrianOptimizedResponse {
 	s.Body = v
 	return s
@@ -156,7 +153,7 @@ func (s *DetectIPCPedestrianOptimizedResponse) SetBody(v *DetectIPCPedestrianOpt
 type ExecuteServerSideVerificationRequest struct {
 	CertificateName   *string `json:"certificateName,omitempty" xml:"certificateName,omitempty"`
 	CertificateNumber *string `json:"certificateNumber,omitempty" xml:"certificateNumber,omitempty"`
-	FacialPictureData []byte  `json:"facialPictureData,omitempty" xml:"facialPictureData,omitempty"`
+	FacialPictureData *string `json:"facialPictureData,omitempty" xml:"facialPictureData,omitempty"`
 	FacialPictureUrl  *string `json:"facialPictureUrl,omitempty" xml:"facialPictureUrl,omitempty"`
 	SceneType         *string `json:"sceneType,omitempty" xml:"sceneType,omitempty"`
 }
@@ -179,8 +176,8 @@ func (s *ExecuteServerSideVerificationRequest) SetCertificateNumber(v string) *E
 	return s
 }
 
-func (s *ExecuteServerSideVerificationRequest) SetFacialPictureData(v []byte) *ExecuteServerSideVerificationRequest {
-	s.FacialPictureData = v
+func (s *ExecuteServerSideVerificationRequest) SetFacialPictureData(v string) *ExecuteServerSideVerificationRequest {
+	s.FacialPictureData = &v
 	return s
 }
 
@@ -195,11 +192,8 @@ func (s *ExecuteServerSideVerificationRequest) SetSceneType(v string) *ExecuteSe
 }
 
 type ExecuteServerSideVerificationResponseBody struct {
-	// Id of the request
-	RequestId *string                                        `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
 	Data      *ExecuteServerSideVerificationResponseBodyData `json:"Data,omitempty" xml:"Data,omitempty" type:"Struct"`
-	Code      *string                                        `json:"Code,omitempty" xml:"Code,omitempty"`
-	Message   *string                                        `json:"Message,omitempty" xml:"Message,omitempty"`
+	RequestId *string                                        `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
 }
 
 func (s ExecuteServerSideVerificationResponseBody) String() string {
@@ -210,30 +204,20 @@ func (s ExecuteServerSideVerificationResponseBody) GoString() string {
 	return s.String()
 }
 
-func (s *ExecuteServerSideVerificationResponseBody) SetRequestId(v string) *ExecuteServerSideVerificationResponseBody {
-	s.RequestId = &v
-	return s
-}
-
 func (s *ExecuteServerSideVerificationResponseBody) SetData(v *ExecuteServerSideVerificationResponseBodyData) *ExecuteServerSideVerificationResponseBody {
 	s.Data = v
 	return s
 }
 
-func (s *ExecuteServerSideVerificationResponseBody) SetCode(v string) *ExecuteServerSideVerificationResponseBody {
-	s.Code = &v
-	return s
-}
-
-func (s *ExecuteServerSideVerificationResponseBody) SetMessage(v string) *ExecuteServerSideVerificationResponseBody {
-	s.Message = &v
+func (s *ExecuteServerSideVerificationResponseBody) SetRequestId(v string) *ExecuteServerSideVerificationResponseBody {
+	s.RequestId = &v
 	return s
 }
 
 type ExecuteServerSideVerificationResponseBodyData struct {
 	Pass              *bool   `json:"Pass,omitempty" xml:"Pass,omitempty"`
-	VerificationToken *string `json:"VerificationToken,omitempty" xml:"VerificationToken,omitempty"`
 	Reason            *string `json:"Reason,omitempty" xml:"Reason,omitempty"`
+	VerificationToken *string `json:"VerificationToken,omitempty" xml:"VerificationToken,omitempty"`
 }
 
 func (s ExecuteServerSideVerificationResponseBodyData) String() string {
@@ -249,19 +233,20 @@ func (s *ExecuteServerSideVerificationResponseBodyData) SetPass(v bool) *Execute
 	return s
 }
 
-func (s *ExecuteServerSideVerificationResponseBodyData) SetVerificationToken(v string) *ExecuteServerSideVerificationResponseBodyData {
-	s.VerificationToken = &v
-	return s
-}
-
 func (s *ExecuteServerSideVerificationResponseBodyData) SetReason(v string) *ExecuteServerSideVerificationResponseBodyData {
 	s.Reason = &v
 	return s
 }
 
+func (s *ExecuteServerSideVerificationResponseBodyData) SetVerificationToken(v string) *ExecuteServerSideVerificationResponseBodyData {
+	s.VerificationToken = &v
+	return s
+}
+
 type ExecuteServerSideVerificationResponse struct {
-	Headers map[string]*string                         `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
-	Body    *ExecuteServerSideVerificationResponseBody `json:"body,omitempty" xml:"body,omitempty" require:"true"`
+	Headers    map[string]*string                         `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
+	StatusCode *int32                                     `json:"statusCode,omitempty" xml:"statusCode,omitempty" require:"true"`
+	Body       *ExecuteServerSideVerificationResponseBody `json:"body,omitempty" xml:"body,omitempty" require:"true"`
 }
 
 func (s ExecuteServerSideVerificationResponse) String() string {
@@ -274,6 +259,11 @@ func (s ExecuteServerSideVerificationResponse) GoString() string {
 
 func (s *ExecuteServerSideVerificationResponse) SetHeaders(v map[string]*string) *ExecuteServerSideVerificationResponse {
 	s.Headers = v
+	return s
+}
+
+func (s *ExecuteServerSideVerificationResponse) SetStatusCode(v int32) *ExecuteServerSideVerificationResponse {
+	s.StatusCode = &v
 	return s
 }
 
@@ -331,25 +321,22 @@ func (client *Client) GetEndpoint(productId *string, regionId *string, endpointR
 
 /**
  * 行人检测快速版
+ *
+ * @param request DetectIPCPedestrianOptimizedRequest
+ * @param headers map
+ * @param runtime runtime options for this request RuntimeOptions
+ * @return DetectIPCPedestrianOptimizedResponse
  */
-func (client *Client) DetectIPCPedestrianOptimized(request *DetectIPCPedestrianOptimizedRequest) (_result *DetectIPCPedestrianOptimizedResponse, _err error) {
-	runtime := &util.RuntimeOptions{}
-	headers := make(map[string]*string)
-	_result = &DetectIPCPedestrianOptimizedResponse{}
-	_body, _err := client.DetectIPCPedestrianOptimizedWithOptions(request, headers, runtime)
-	if _err != nil {
-		return _result, _err
-	}
-	_result = _body
-	return _result, _err
-}
-
 func (client *Client) DetectIPCPedestrianOptimizedWithOptions(request *DetectIPCPedestrianOptimizedRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *DetectIPCPedestrianOptimizedResponse, _err error) {
 	_err = util.ValidateModel(request)
 	if _err != nil {
 		return _result, _err
 	}
 	body := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.Height)) {
+		body["height"] = request.Height
+	}
+
 	if !tea.BoolValue(util.IsUnset(request.ImageData)) {
 		body["imageData"] = request.ImageData
 	}
@@ -358,16 +345,23 @@ func (client *Client) DetectIPCPedestrianOptimizedWithOptions(request *DetectIPC
 		body["width"] = request.Width
 	}
 
-	if !tea.BoolValue(util.IsUnset(request.Height)) {
-		body["height"] = request.Height
-	}
-
 	req := &openapi.OpenApiRequest{
 		Headers: headers,
 		Body:    openapiutil.ParseToMap(body),
 	}
+	params := &openapi.Params{
+		Action:      tea.String("DetectIPCPedestrianOptimized"),
+		Version:     tea.String("2020-09-10"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/viapi/k8s/facebody/detect-ipc-pedestrian-optimized"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("ROA"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
 	_result = &DetectIPCPedestrianOptimizedResponse{}
-	_body, _err := client.DoROARequestWithForm(tea.String("DetectIPCPedestrianOptimized"), tea.String("2020-09-10"), tea.String("HTTPS"), tea.String("POST"), tea.String("AK"), tea.String("/viapi/k8s/facebody/detect-ipc-pedestrian-optimized"), tea.String("json"), req, runtime)
+	_body, _err := client.CallApi(params, req, runtime)
 	if _err != nil {
 		return _result, _err
 	}
@@ -375,11 +369,17 @@ func (client *Client) DetectIPCPedestrianOptimizedWithOptions(request *DetectIPC
 	return _result, _err
 }
 
-func (client *Client) ExecuteServerSideVerification(request *ExecuteServerSideVerificationRequest) (_result *ExecuteServerSideVerificationResponse, _err error) {
+/**
+ * 行人检测快速版
+ *
+ * @param request DetectIPCPedestrianOptimizedRequest
+ * @return DetectIPCPedestrianOptimizedResponse
+ */
+func (client *Client) DetectIPCPedestrianOptimized(request *DetectIPCPedestrianOptimizedRequest) (_result *DetectIPCPedestrianOptimizedResponse, _err error) {
 	runtime := &util.RuntimeOptions{}
 	headers := make(map[string]*string)
-	_result = &ExecuteServerSideVerificationResponse{}
-	_body, _err := client.ExecuteServerSideVerificationWithOptions(request, headers, runtime)
+	_result = &DetectIPCPedestrianOptimizedResponse{}
+	_body, _err := client.DetectIPCPedestrianOptimizedWithOptions(request, headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
@@ -417,11 +417,34 @@ func (client *Client) ExecuteServerSideVerificationWithOptions(request *ExecuteS
 		Headers: headers,
 		Body:    openapiutil.ParseToMap(body),
 	}
+	params := &openapi.Params{
+		Action:      tea.String("ExecuteServerSideVerification"),
+		Version:     tea.String("2020-09-10"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/viapi/thirdparty/realperson/execServerSideVerification"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("ROA"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
 	_result = &ExecuteServerSideVerificationResponse{}
-	_body, _err := client.DoROARequestWithForm(tea.String("ExecuteServerSideVerification"), tea.String("2020-09-10"), tea.String("HTTPS"), tea.String("POST"), tea.String("AK"), tea.String("/viapi/thirdparty/realperson/execServerSideVerification"), tea.String("json"), req, runtime)
+	_body, _err := client.CallApi(params, req, runtime)
 	if _err != nil {
 		return _result, _err
 	}
 	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+func (client *Client) ExecuteServerSideVerification(request *ExecuteServerSideVerificationRequest) (_result *ExecuteServerSideVerificationResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &ExecuteServerSideVerificationResponse{}
+	_body, _err := client.ExecuteServerSideVerificationWithOptions(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
 	return _result, _err
 }
