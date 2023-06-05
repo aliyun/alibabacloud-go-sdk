e4afd899eab21cbacc8d87907524cbcb3b39b11e
sdk-team <sdk-team@alibabacloud.com>
2023-06-05 03:37:02 +0000

    Auto create user when GetAuthCode.

--- appstream-center-20210218/client/client.go
+++ appstream-center-20210218/client/client.go
@@ -12,7 +12,89 @@ import (
 	"github.com/alibabacloud-go/tea/tea"
 )
 
+type ExpireLoginTokenRequest struct {
+	EndUserId    *string `json:"EndUserId,omitempty" xml:"EndUserId,omitempty"`
+	LoginToken   *string `json:"LoginToken,omitempty" xml:"LoginToken,omitempty"`
+	OfficeSiteId *string `json:"OfficeSiteId,omitempty" xml:"OfficeSiteId,omitempty"`
+	SessionId    *string `json:"SessionId,omitempty" xml:"SessionId,omitempty"`
+}
+
+func (s ExpireLoginTokenRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ExpireLoginTokenRequest) GoString() string {
+	return s.String()
+}
+
+func (s *ExpireLoginTokenRequest) SetEndUserId(v string) *ExpireLoginTokenRequest {
+	s.EndUserId = &v
+	return s
+}
+
+func (s *ExpireLoginTokenRequest) SetLoginToken(v string) *ExpireLoginTokenRequest {
+	s.LoginToken = &v
+	return s
+}
+
+func (s *ExpireLoginTokenRequest) SetOfficeSiteId(v string) *ExpireLoginTokenRequest {
+	s.OfficeSiteId = &v
+	return s
+}
+
+func (s *ExpireLoginTokenRequest) SetSessionId(v string) *ExpireLoginTokenRequest {
+	s.SessionId = &v
+	return s
+}
+
+type ExpireLoginTokenResponseBody struct {
+	RequestId *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+}
+
+func (s ExpireLoginTokenResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ExpireLoginTokenResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *ExpireLoginTokenResponseBody) SetRequestId(v string) *ExpireLoginTokenResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+type ExpireLoginTokenResponse struct {
+	Headers    map[string]*string            `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
+	StatusCode *int32                        `json:"statusCode,omitempty" xml:"statusCode,omitempty" require:"true"`
+	Body       *ExpireLoginTokenResponseBody `json:"body,omitempty" xml:"body,omitempty" require:"true"`
+}
+
+func (s ExpireLoginTokenResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ExpireLoginTokenResponse) GoString() string {
+	return s.String()
+}
+
+func (s *ExpireLoginTokenResponse) SetHeaders(v map[string]*string) *ExpireLoginTokenResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *ExpireLoginTokenResponse) SetStatusCode(v int32) *ExpireLoginTokenResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *ExpireLoginTokenResponse) SetBody(v *ExpireLoginTokenResponseBody) *ExpireLoginTokenResponse {
+	s.Body = v
+	return s
+}
+
 type GetAuthCodeRequest struct {
+	AutoCreateUser *bool   `json:"AutoCreateUser,omitempty" xml:"AutoCreateUser,omitempty"`
 	EndUserId      *string `json:"EndUserId,omitempty" xml:"EndUserId,omitempty"`
 	ExternalUserId *string `json:"ExternalUserId,omitempty" xml:"ExternalUserId,omitempty"`
 	Policy         *string `json:"Policy,omitempty" xml:"Policy,omitempty"`
@@ -24,6 +106,11 @@ func (s GetAuthCodeRequest) String() string {
 
 func (s GetAuthCodeRequest) GoString() string {
 	return s.String()
+}
+
+func (s *GetAuthCodeRequest) SetAutoCreateUser(v bool) *GetAuthCodeRequest {
+	s.AutoCreateUser = &v
+	return s
 }
 
 func (s *GetAuthCodeRequest) SetEndUserId(v string) *GetAuthCodeRequest {
@@ -169,12 +256,72 @@ func (client *Client) GetEndpoint(productId *string, regionId *string, endpointR
 	return _result, _err
 }
 
+func (client *Client) ExpireLoginTokenWithOptions(request *ExpireLoginTokenRequest, runtime *util.RuntimeOptions) (_result *ExpireLoginTokenResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	body := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.EndUserId)) {
+		body["EndUserId"] = request.EndUserId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.LoginToken)) {
+		body["LoginToken"] = request.LoginToken
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.OfficeSiteId)) {
+		body["OfficeSiteId"] = request.OfficeSiteId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.SessionId)) {
+		body["SessionId"] = request.SessionId
+	}
+
+	req := &openapi.OpenApiRequest{
+		Body: openapiutil.ParseToMap(body),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("ExpireLoginToken"),
+		Version:     tea.String("2021-02-18"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &ExpireLoginTokenResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+func (client *Client) ExpireLoginToken(request *ExpireLoginTokenRequest) (_result *ExpireLoginTokenResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &ExpireLoginTokenResponse{}
+	_body, _err := client.ExpireLoginTokenWithOptions(request, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
 func (client *Client) GetAuthCodeWithOptions(request *GetAuthCodeRequest, runtime *util.RuntimeOptions) (_result *GetAuthCodeResponse, _err error) {
 	_err = util.ValidateModel(request)
 	if _err != nil {
 		return _result, _err
 	}
 	body := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.AutoCreateUser)) {
+		body["AutoCreateUser"] = request.AutoCreateUser
+	}
+
 	if !tea.BoolValue(util.IsUnset(request.EndUserId)) {
 		body["EndUserId"] = request.EndUserId
 	}
