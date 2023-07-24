c38f8d7712d56dc36b8a85137e9d769d900c3e6c
sdk-team <sdk-team@alibabacloud.com>
2023-07-24 01:56:56 +0000

    Add GetGraphQueryTemplates.

--- sasti-20200512/client/client.go
+++ sasti-20200512/client/client.go
@@ -13,8 +13,9 @@ import (
 )
 
 type DescribeDomainReportRequest struct {
-	Domain *string `json:"Domain,omitempty" xml:"Domain,omitempty"`
-	Field  *string `json:"Field,omitempty" xml:"Field,omitempty"`
+	Domain      *string `json:"Domain,omitempty" xml:"Domain,omitempty"`
+	Field       *string `json:"Field,omitempty" xml:"Field,omitempty"`
+	ServiceLang *string `json:"ServiceLang,omitempty" xml:"ServiceLang,omitempty"`
 }
 
 func (s DescribeDomainReportRequest) String() string {
@@ -32,6 +33,11 @@ func (s *DescribeDomainReportRequest) SetDomain(v string) *DescribeDomainReportR
 
 func (s *DescribeDomainReportRequest) SetField(v string) *DescribeDomainReportRequest {
 	s.Field = &v
+	return s
+}
+
+func (s *DescribeDomainReportRequest) SetServiceLang(v string) *DescribeDomainReportRequest {
+	s.ServiceLang = &v
 	return s
 }
 
@@ -159,6 +165,392 @@ func (s *DescribeDomainReportResponse) SetBody(v *DescribeDomainReportResponseBo
 	return s
 }
 
+type DescribeFileReportRequest struct {
+	Field       *string `json:"Field,omitempty" xml:"Field,omitempty"`
+	FileHash    *string `json:"FileHash,omitempty" xml:"FileHash,omitempty"`
+	ServiceLang *string `json:"ServiceLang,omitempty" xml:"ServiceLang,omitempty"`
+}
+
+func (s DescribeFileReportRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeFileReportRequest) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeFileReportRequest) SetField(v string) *DescribeFileReportRequest {
+	s.Field = &v
+	return s
+}
+
+func (s *DescribeFileReportRequest) SetFileHash(v string) *DescribeFileReportRequest {
+	s.FileHash = &v
+	return s
+}
+
+func (s *DescribeFileReportRequest) SetServiceLang(v string) *DescribeFileReportRequest {
+	s.ServiceLang = &v
+	return s
+}
+
+type DescribeFileReportResponseBody struct {
+	Basic         *string `json:"Basic,omitempty" xml:"Basic,omitempty"`
+	FileHash      *string `json:"FileHash,omitempty" xml:"FileHash,omitempty"`
+	Intelligences *string `json:"Intelligences,omitempty" xml:"Intelligences,omitempty"`
+	RequestId     *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	Sandbox       *string `json:"Sandbox,omitempty" xml:"Sandbox,omitempty"`
+	ThreatLevel   *string `json:"ThreatLevel,omitempty" xml:"ThreatLevel,omitempty"`
+	ThreatTypes   *string `json:"ThreatTypes,omitempty" xml:"ThreatTypes,omitempty"`
+}
+
+func (s DescribeFileReportResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeFileReportResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeFileReportResponseBody) SetBasic(v string) *DescribeFileReportResponseBody {
+	s.Basic = &v
+	return s
+}
+
+func (s *DescribeFileReportResponseBody) SetFileHash(v string) *DescribeFileReportResponseBody {
+	s.FileHash = &v
+	return s
+}
+
+func (s *DescribeFileReportResponseBody) SetIntelligences(v string) *DescribeFileReportResponseBody {
+	s.Intelligences = &v
+	return s
+}
+
+func (s *DescribeFileReportResponseBody) SetRequestId(v string) *DescribeFileReportResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *DescribeFileReportResponseBody) SetSandbox(v string) *DescribeFileReportResponseBody {
+	s.Sandbox = &v
+	return s
+}
+
+func (s *DescribeFileReportResponseBody) SetThreatLevel(v string) *DescribeFileReportResponseBody {
+	s.ThreatLevel = &v
+	return s
+}
+
+func (s *DescribeFileReportResponseBody) SetThreatTypes(v string) *DescribeFileReportResponseBody {
+	s.ThreatTypes = &v
+	return s
+}
+
+type DescribeFileReportResponse struct {
+	Headers    map[string]*string              `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
+	StatusCode *int32                          `json:"statusCode,omitempty" xml:"statusCode,omitempty" require:"true"`
+	Body       *DescribeFileReportResponseBody `json:"body,omitempty" xml:"body,omitempty" require:"true"`
+}
+
+func (s DescribeFileReportResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeFileReportResponse) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeFileReportResponse) SetHeaders(v map[string]*string) *DescribeFileReportResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *DescribeFileReportResponse) SetStatusCode(v int32) *DescribeFileReportResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *DescribeFileReportResponse) SetBody(v *DescribeFileReportResponseBody) *DescribeFileReportResponse {
+	s.Body = v
+	return s
+}
+
+type DescribeIpReportRequest struct {
+	Field       *string `json:"Field,omitempty" xml:"Field,omitempty"`
+	Ip          *string `json:"Ip,omitempty" xml:"Ip,omitempty"`
+	ServiceLang *string `json:"ServiceLang,omitempty" xml:"ServiceLang,omitempty"`
+}
+
+func (s DescribeIpReportRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeIpReportRequest) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeIpReportRequest) SetField(v string) *DescribeIpReportRequest {
+	s.Field = &v
+	return s
+}
+
+func (s *DescribeIpReportRequest) SetIp(v string) *DescribeIpReportRequest {
+	s.Ip = &v
+	return s
+}
+
+func (s *DescribeIpReportRequest) SetServiceLang(v string) *DescribeIpReportRequest {
+	s.ServiceLang = &v
+	return s
+}
+
+type DescribeIpReportResponseBody struct {
+	AttackCntByThreatType *string `json:"AttackCntByThreatType,omitempty" xml:"AttackCntByThreatType,omitempty"`
+	AttackPreferenceTop5  *string `json:"AttackPreferenceTop5,omitempty" xml:"AttackPreferenceTop5,omitempty"`
+	Confidence            *string `json:"Confidence,omitempty" xml:"Confidence,omitempty"`
+	Context               *string `json:"Context,omitempty" xml:"Context,omitempty"`
+	Group                 *string `json:"Group,omitempty" xml:"Group,omitempty"`
+	Intelligences         *string `json:"Intelligences,omitempty" xml:"Intelligences,omitempty"`
+	Ip                    *string `json:"Ip,omitempty" xml:"Ip,omitempty"`
+	RequestId             *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	Scenario              *string `json:"Scenario,omitempty" xml:"Scenario,omitempty"`
+	ThreatLevel           *string `json:"ThreatLevel,omitempty" xml:"ThreatLevel,omitempty"`
+	ThreatTypes           *string `json:"ThreatTypes,omitempty" xml:"ThreatTypes,omitempty"`
+	Whois                 *string `json:"Whois,omitempty" xml:"Whois,omitempty"`
+}
+
+func (s DescribeIpReportResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeIpReportResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeIpReportResponseBody) SetAttackCntByThreatType(v string) *DescribeIpReportResponseBody {
+	s.AttackCntByThreatType = &v
+	return s
+}
+
+func (s *DescribeIpReportResponseBody) SetAttackPreferenceTop5(v string) *DescribeIpReportResponseBody {
+	s.AttackPreferenceTop5 = &v
+	return s
+}
+
+func (s *DescribeIpReportResponseBody) SetConfidence(v string) *DescribeIpReportResponseBody {
+	s.Confidence = &v
+	return s
+}
+
+func (s *DescribeIpReportResponseBody) SetContext(v string) *DescribeIpReportResponseBody {
+	s.Context = &v
+	return s
+}
+
+func (s *DescribeIpReportResponseBody) SetGroup(v string) *DescribeIpReportResponseBody {
+	s.Group = &v
+	return s
+}
+
+func (s *DescribeIpReportResponseBody) SetIntelligences(v string) *DescribeIpReportResponseBody {
+	s.Intelligences = &v
+	return s
+}
+
+func (s *DescribeIpReportResponseBody) SetIp(v string) *DescribeIpReportResponseBody {
+	s.Ip = &v
+	return s
+}
+
+func (s *DescribeIpReportResponseBody) SetRequestId(v string) *DescribeIpReportResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *DescribeIpReportResponseBody) SetScenario(v string) *DescribeIpReportResponseBody {
+	s.Scenario = &v
+	return s
+}
+
+func (s *DescribeIpReportResponseBody) SetThreatLevel(v string) *DescribeIpReportResponseBody {
+	s.ThreatLevel = &v
+	return s
+}
+
+func (s *DescribeIpReportResponseBody) SetThreatTypes(v string) *DescribeIpReportResponseBody {
+	s.ThreatTypes = &v
+	return s
+}
+
+func (s *DescribeIpReportResponseBody) SetWhois(v string) *DescribeIpReportResponseBody {
+	s.Whois = &v
+	return s
+}
+
+type DescribeIpReportResponse struct {
+	Headers    map[string]*string            `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
+	StatusCode *int32                        `json:"statusCode,omitempty" xml:"statusCode,omitempty" require:"true"`
+	Body       *DescribeIpReportResponseBody `json:"body,omitempty" xml:"body,omitempty" require:"true"`
+}
+
+func (s DescribeIpReportResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeIpReportResponse) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeIpReportResponse) SetHeaders(v map[string]*string) *DescribeIpReportResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *DescribeIpReportResponse) SetStatusCode(v int32) *DescribeIpReportResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *DescribeIpReportResponse) SetBody(v *DescribeIpReportResponseBody) *DescribeIpReportResponse {
+	s.Body = v
+	return s
+}
+
+type GetGraphQueryTemplatesRequest struct {
+	Env         *string `json:"Env,omitempty" xml:"Env,omitempty"`
+	ServiceUnit *string `json:"ServiceUnit,omitempty" xml:"ServiceUnit,omitempty"`
+}
+
+func (s GetGraphQueryTemplatesRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetGraphQueryTemplatesRequest) GoString() string {
+	return s.String()
+}
+
+func (s *GetGraphQueryTemplatesRequest) SetEnv(v string) *GetGraphQueryTemplatesRequest {
+	s.Env = &v
+	return s
+}
+
+func (s *GetGraphQueryTemplatesRequest) SetServiceUnit(v string) *GetGraphQueryTemplatesRequest {
+	s.ServiceUnit = &v
+	return s
+}
+
+type GetGraphQueryTemplatesResponseBody struct {
+	Data      []*GetGraphQueryTemplatesResponseBodyData `json:"Data,omitempty" xml:"Data,omitempty" type:"Repeated"`
+	RequestId *string                                   `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+}
+
+func (s GetGraphQueryTemplatesResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetGraphQueryTemplatesResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *GetGraphQueryTemplatesResponseBody) SetData(v []*GetGraphQueryTemplatesResponseBodyData) *GetGraphQueryTemplatesResponseBody {
+	s.Data = v
+	return s
+}
+
+func (s *GetGraphQueryTemplatesResponseBody) SetRequestId(v string) *GetGraphQueryTemplatesResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+type GetGraphQueryTemplatesResponseBodyData struct {
+	GmtCreate       *string `json:"GmtCreate,omitempty" xml:"GmtCreate,omitempty"`
+	GmtModified     *string `json:"GmtModified,omitempty" xml:"GmtModified,omitempty"`
+	GremlinTemplate *string `json:"GremlinTemplate,omitempty" xml:"GremlinTemplate,omitempty"`
+	Id              *int64  `json:"Id,omitempty" xml:"Id,omitempty"`
+	LastModifier    *string `json:"LastModifier,omitempty" xml:"LastModifier,omitempty"`
+	Scenario        *string `json:"Scenario,omitempty" xml:"Scenario,omitempty"`
+	ScenarioDesc    *string `json:"ScenarioDesc,omitempty" xml:"ScenarioDesc,omitempty"`
+	Status          *int32  `json:"Status,omitempty" xml:"Status,omitempty"`
+}
+
+func (s GetGraphQueryTemplatesResponseBodyData) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetGraphQueryTemplatesResponseBodyData) GoString() string {
+	return s.String()
+}
+
+func (s *GetGraphQueryTemplatesResponseBodyData) SetGmtCreate(v string) *GetGraphQueryTemplatesResponseBodyData {
+	s.GmtCreate = &v
+	return s
+}
+
+func (s *GetGraphQueryTemplatesResponseBodyData) SetGmtModified(v string) *GetGraphQueryTemplatesResponseBodyData {
+	s.GmtModified = &v
+	return s
+}
+
+func (s *GetGraphQueryTemplatesResponseBodyData) SetGremlinTemplate(v string) *GetGraphQueryTemplatesResponseBodyData {
+	s.GremlinTemplate = &v
+	return s
+}
+
+func (s *GetGraphQueryTemplatesResponseBodyData) SetId(v int64) *GetGraphQueryTemplatesResponseBodyData {
+	s.Id = &v
+	return s
+}
+
+func (s *GetGraphQueryTemplatesResponseBodyData) SetLastModifier(v string) *GetGraphQueryTemplatesResponseBodyData {
+	s.LastModifier = &v
+	return s
+}
+
+func (s *GetGraphQueryTemplatesResponseBodyData) SetScenario(v string) *GetGraphQueryTemplatesResponseBodyData {
+	s.Scenario = &v
+	return s
+}
+
+func (s *GetGraphQueryTemplatesResponseBodyData) SetScenarioDesc(v string) *GetGraphQueryTemplatesResponseBodyData {
+	s.ScenarioDesc = &v
+	return s
+}
+
+func (s *GetGraphQueryTemplatesResponseBodyData) SetStatus(v int32) *GetGraphQueryTemplatesResponseBodyData {
+	s.Status = &v
+	return s
+}
+
+type GetGraphQueryTemplatesResponse struct {
+	Headers    map[string]*string                  `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
+	StatusCode *int32                              `json:"statusCode,omitempty" xml:"statusCode,omitempty" require:"true"`
+	Body       *GetGraphQueryTemplatesResponseBody `json:"body,omitempty" xml:"body,omitempty" require:"true"`
+}
+
+func (s GetGraphQueryTemplatesResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetGraphQueryTemplatesResponse) GoString() string {
+	return s.String()
+}
+
+func (s *GetGraphQueryTemplatesResponse) SetHeaders(v map[string]*string) *GetGraphQueryTemplatesResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *GetGraphQueryTemplatesResponse) SetStatusCode(v int32) *GetGraphQueryTemplatesResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *GetGraphQueryTemplatesResponse) SetBody(v *GetGraphQueryTemplatesResponseBody) *GetGraphQueryTemplatesResponse {
+	s.Body = v
+	return s
+}
+
 type Client struct {
 	openapi.Client
 }
@@ -220,6 +612,10 @@ func (client *Client) DescribeDomainReportWithOptions(request *DescribeDomainRep
 		query["Field"] = request.Field
 	}
 
+	if !tea.BoolValue(util.IsUnset(request.ServiceLang)) {
+		query["ServiceLang"] = request.ServiceLang
+	}
+
 	req := &openapi.OpenApiRequest{
 		Query: openapiutil.Query(query),
 	}
@@ -247,6 +643,158 @@ func (client *Client) DescribeDomainReport(request *DescribeDomainReportRequest)
 	runtime := &util.RuntimeOptions{}
 	_result = &DescribeDomainReportResponse{}
 	_body, _err := client.DescribeDomainReportWithOptions(request, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+func (client *Client) DescribeFileReportWithOptions(request *DescribeFileReportRequest, runtime *util.RuntimeOptions) (_result *DescribeFileReportResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	query := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.Field)) {
+		query["Field"] = request.Field
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.FileHash)) {
+		query["FileHash"] = request.FileHash
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.ServiceLang)) {
+		query["ServiceLang"] = request.ServiceLang
+	}
+
+	req := &openapi.OpenApiRequest{
+		Query: openapiutil.Query(query),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("DescribeFileReport"),
+		Version:     tea.String("2020-05-12"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &DescribeFileReportResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+func (client *Client) DescribeFileReport(request *DescribeFileReportRequest) (_result *DescribeFileReportResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &DescribeFileReportResponse{}
+	_body, _err := client.DescribeFileReportWithOptions(request, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+func (client *Client) DescribeIpReportWithOptions(request *DescribeIpReportRequest, runtime *util.RuntimeOptions) (_result *DescribeIpReportResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	query := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.Field)) {
+		query["Field"] = request.Field
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.Ip)) {
+		query["Ip"] = request.Ip
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.ServiceLang)) {
+		query["ServiceLang"] = request.ServiceLang
+	}
+
+	req := &openapi.OpenApiRequest{
+		Query: openapiutil.Query(query),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("DescribeIpReport"),
+		Version:     tea.String("2020-05-12"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &DescribeIpReportResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+func (client *Client) DescribeIpReport(request *DescribeIpReportRequest) (_result *DescribeIpReportResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &DescribeIpReportResponse{}
+	_body, _err := client.DescribeIpReportWithOptions(request, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+func (client *Client) GetGraphQueryTemplatesWithOptions(request *GetGraphQueryTemplatesRequest, runtime *util.RuntimeOptions) (_result *GetGraphQueryTemplatesResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	body := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.Env)) {
+		body["Env"] = request.Env
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.ServiceUnit)) {
+		body["ServiceUnit"] = request.ServiceUnit
+	}
+
+	req := &openapi.OpenApiRequest{
+		Body: openapiutil.ParseToMap(body),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("GetGraphQueryTemplates"),
+		Version:     tea.String("2020-05-12"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &GetGraphQueryTemplatesResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+func (client *Client) GetGraphQueryTemplates(request *GetGraphQueryTemplatesRequest) (_result *GetGraphQueryTemplatesResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &GetGraphQueryTemplatesResponse{}
+	_body, _err := client.GetGraphQueryTemplatesWithOptions(request, runtime)
 	if _err != nil {
 		return _result, _err
 	}
