cc1d817a6c3d5ffc972280f1f1500261b0f3d681
sdk-team <sdk-team@alibabacloud.com>
2024-01-03 17:10:40 +0000

    Generated golang 2021-09-31 for BPStudio.

--- bpstudio-20210931/client/client.go
+++ bpstudio-20210931/client/client.go
@@ -1843,6 +1843,106 @@ func (s *ListApplicationResponse) SetBody(v *ListApplicationResponseBody) *ListA
 	return s
 }
 
+type ListFoCreatedAppsResponseBody struct {
+	Code    *int32                               `json:"Code,omitempty" xml:"Code,omitempty"`
+	Data    []*ListFoCreatedAppsResponseBodyData `json:"Data,omitempty" xml:"Data,omitempty" type:"Repeated"`
+	Message *string                              `json:"Message,omitempty" xml:"Message,omitempty"`
+	// Id of the request
+	RequestId *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+}
+
+func (s ListFoCreatedAppsResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ListFoCreatedAppsResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *ListFoCreatedAppsResponseBody) SetCode(v int32) *ListFoCreatedAppsResponseBody {
+	s.Code = &v
+	return s
+}
+
+func (s *ListFoCreatedAppsResponseBody) SetData(v []*ListFoCreatedAppsResponseBodyData) *ListFoCreatedAppsResponseBody {
+	s.Data = v
+	return s
+}
+
+func (s *ListFoCreatedAppsResponseBody) SetMessage(v string) *ListFoCreatedAppsResponseBody {
+	s.Message = &v
+	return s
+}
+
+func (s *ListFoCreatedAppsResponseBody) SetRequestId(v string) *ListFoCreatedAppsResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+type ListFoCreatedAppsResponseBodyData struct {
+	AppId     *string `json:"AppId,omitempty" xml:"AppId,omitempty"`
+	ReportUrl *string `json:"ReportUrl,omitempty" xml:"ReportUrl,omitempty"`
+	Status    *string `json:"Status,omitempty" xml:"Status,omitempty"`
+	Title     *string `json:"Title,omitempty" xml:"Title,omitempty"`
+}
+
+func (s ListFoCreatedAppsResponseBodyData) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ListFoCreatedAppsResponseBodyData) GoString() string {
+	return s.String()
+}
+
+func (s *ListFoCreatedAppsResponseBodyData) SetAppId(v string) *ListFoCreatedAppsResponseBodyData {
+	s.AppId = &v
+	return s
+}
+
+func (s *ListFoCreatedAppsResponseBodyData) SetReportUrl(v string) *ListFoCreatedAppsResponseBodyData {
+	s.ReportUrl = &v
+	return s
+}
+
+func (s *ListFoCreatedAppsResponseBodyData) SetStatus(v string) *ListFoCreatedAppsResponseBodyData {
+	s.Status = &v
+	return s
+}
+
+func (s *ListFoCreatedAppsResponseBodyData) SetTitle(v string) *ListFoCreatedAppsResponseBodyData {
+	s.Title = &v
+	return s
+}
+
+type ListFoCreatedAppsResponse struct {
+	Headers    map[string]*string             `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
+	StatusCode *int32                         `json:"statusCode,omitempty" xml:"statusCode,omitempty" require:"true"`
+	Body       *ListFoCreatedAppsResponseBody `json:"body,omitempty" xml:"body,omitempty" require:"true"`
+}
+
+func (s ListFoCreatedAppsResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ListFoCreatedAppsResponse) GoString() string {
+	return s.String()
+}
+
+func (s *ListFoCreatedAppsResponse) SetHeaders(v map[string]*string) *ListFoCreatedAppsResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *ListFoCreatedAppsResponse) SetStatusCode(v int32) *ListFoCreatedAppsResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *ListFoCreatedAppsResponse) SetBody(v *ListFoCreatedAppsResponseBody) *ListFoCreatedAppsResponse {
+	s.Body = v
+	return s
+}
+
 type ListTagResourcesRequest struct {
 	// The client token that is used to ensure the idempotence of the request.
 	ClientToken *string `json:"ClientToken,omitempty" xml:"ClientToken,omitempty"`
@@ -3518,6 +3618,39 @@ func (client *Client) ListApplication(request *ListApplicationRequest) (_result 
 	runtime := &util.RuntimeOptions{}
 	_result = &ListApplicationResponse{}
 	_body, _err := client.ListApplicationWithOptions(request, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+func (client *Client) ListFoCreatedAppsWithOptions(runtime *util.RuntimeOptions) (_result *ListFoCreatedAppsResponse, _err error) {
+	req := &openapi.OpenApiRequest{}
+	params := &openapi.Params{
+		Action:      tea.String("ListFoCreatedApps"),
+		Version:     tea.String("2021-09-31"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &ListFoCreatedAppsResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+func (client *Client) ListFoCreatedApps() (_result *ListFoCreatedAppsResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &ListFoCreatedAppsResponse{}
+	_body, _err := client.ListFoCreatedAppsWithOptions(runtime)
 	if _err != nil {
 		return _result, _err
 	}
