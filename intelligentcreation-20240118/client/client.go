b6ab407afbfbcb0aaf008cf7bcf211b8f8ddf448
sdk-team <sdk-team@alibabacloud.com>
2024-03-01 07:26:31 +0000

    Support API ActualDeductResources.

--- intelligentcreation-20240118/client/client.go
+++ intelligentcreation-20240118/client/client.go
@@ -85,6 +85,7 @@ func (s *ActualDeductResourceResult) SetSuccess(v bool) *ActualDeductResourceRes
 type DirectDeductResourceCmd struct {
 	AccountId    *string `json:"accountId,omitempty" xml:"accountId,omitempty"`
 	Cost         *int64  `json:"cost,omitempty" xml:"cost,omitempty"`
+	DeductScene  *string `json:"deductScene,omitempty" xml:"deductScene,omitempty"`
 	ExtraInfo    *string `json:"extraInfo,omitempty" xml:"extraInfo,omitempty"`
 	IdempotentId *string `json:"idempotentId,omitempty" xml:"idempotentId,omitempty"`
 	ResourceType *int64  `json:"resourceType,omitempty" xml:"resourceType,omitempty"`
@@ -107,6 +108,11 @@ func (s *DirectDeductResourceCmd) SetAccountId(v string) *DirectDeductResourceCm
 
 func (s *DirectDeductResourceCmd) SetCost(v int64) *DirectDeductResourceCmd {
 	s.Cost = &v
+	return s
+}
+
+func (s *DirectDeductResourceCmd) SetDeductScene(v string) *DirectDeductResourceCmd {
+	s.DeductScene = &v
 	return s
 }
 
@@ -282,9 +288,9 @@ func (s *ActualDeductResourceRequest) SetBody(v *ActualDeductResourceCmd) *Actua
 }
 
 type ActualDeductResourceResponse struct {
-	Headers    map[string]*string          `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
-	StatusCode *int32                      `json:"statusCode,omitempty" xml:"statusCode,omitempty" require:"true"`
-	Body       *ActualDeductResourceResult `json:"body,omitempty" xml:"body,omitempty" require:"true"`
+	Headers    map[string]*string          `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                      `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *ActualDeductResourceResult `json:"body,omitempty" xml:"body,omitempty"`
 }
 
 func (s ActualDeductResourceResponse) String() string {
@@ -310,6 +316,52 @@ func (s *ActualDeductResourceResponse) SetBody(v *ActualDeductResourceResult) *A
 	return s
 }
 
+type ActualDeductResourcesRequest struct {
+	Body *ActualDeductResourceCmd `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s ActualDeductResourcesRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ActualDeductResourcesRequest) GoString() string {
+	return s.String()
+}
+
+func (s *ActualDeductResourcesRequest) SetBody(v *ActualDeductResourceCmd) *ActualDeductResourcesRequest {
+	s.Body = v
+	return s
+}
+
+type ActualDeductResourcesResponse struct {
+	Headers    map[string]*string          `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                      `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *ActualDeductResourceResult `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s ActualDeductResourcesResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ActualDeductResourcesResponse) GoString() string {
+	return s.String()
+}
+
+func (s *ActualDeductResourcesResponse) SetHeaders(v map[string]*string) *ActualDeductResourcesResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *ActualDeductResourcesResponse) SetStatusCode(v int32) *ActualDeductResourcesResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *ActualDeductResourcesResponse) SetBody(v *ActualDeductResourceResult) *ActualDeductResourcesResponse {
+	s.Body = v
+	return s
+}
+
 type DirectDeductResourceRequest struct {
 	Body *DirectDeductResourceCmd `json:"body,omitempty" xml:"body,omitempty"`
 }
@@ -328,9 +380,9 @@ func (s *DirectDeductResourceRequest) SetBody(v *DirectDeductResourceCmd) *Direc
 }
 
 type DirectDeductResourceResponse struct {
-	Headers    map[string]*string          `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
-	StatusCode *int32                      `json:"statusCode,omitempty" xml:"statusCode,omitempty" require:"true"`
-	Body       *DirectDeductResourceResult `json:"body,omitempty" xml:"body,omitempty" require:"true"`
+	Headers    map[string]*string          `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                      `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *DirectDeductResourceResult `json:"body,omitempty" xml:"body,omitempty"`
 }
 
 func (s DirectDeductResourceResponse) String() string {
@@ -356,6 +408,52 @@ func (s *DirectDeductResourceResponse) SetBody(v *DirectDeductResourceResult) *D
 	return s
 }
 
+type DirectDeductResourcesRequest struct {
+	Body *DirectDeductResourceCmd `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s DirectDeductResourcesRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DirectDeductResourcesRequest) GoString() string {
+	return s.String()
+}
+
+func (s *DirectDeductResourcesRequest) SetBody(v *DirectDeductResourceCmd) *DirectDeductResourcesRequest {
+	s.Body = v
+	return s
+}
+
+type DirectDeductResourcesResponse struct {
+	Headers    map[string]*string          `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                      `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *DirectDeductResourceResult `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s DirectDeductResourcesResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DirectDeductResourcesResponse) GoString() string {
+	return s.String()
+}
+
+func (s *DirectDeductResourcesResponse) SetHeaders(v map[string]*string) *DirectDeductResourcesResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *DirectDeductResourcesResponse) SetStatusCode(v int32) *DirectDeductResourcesResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *DirectDeductResourcesResponse) SetBody(v *DirectDeductResourceResult) *DirectDeductResourcesResponse {
+	s.Body = v
+	return s
+}
+
 type ExpectDeductResourceRequest struct {
 	Body *ExpectDeductResourceCmd `json:"body,omitempty" xml:"body,omitempty"`
 }
@@ -374,9 +472,9 @@ func (s *ExpectDeductResourceRequest) SetBody(v *ExpectDeductResourceCmd) *Expec
 }
 
 type ExpectDeductResourceResponse struct {
-	Headers    map[string]*string          `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
-	StatusCode *int32                      `json:"statusCode,omitempty" xml:"statusCode,omitempty" require:"true"`
-	Body       *ExpectDeductResourceResult `json:"body,omitempty" xml:"body,omitempty" require:"true"`
+	Headers    map[string]*string          `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                      `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *ExpectDeductResourceResult `json:"body,omitempty" xml:"body,omitempty"`
 }
 
 func (s ExpectDeductResourceResponse) String() string {
@@ -398,6 +496,152 @@ func (s *ExpectDeductResourceResponse) SetStatusCode(v int32) *ExpectDeductResou
 }
 
 func (s *ExpectDeductResourceResponse) SetBody(v *ExpectDeductResourceResult) *ExpectDeductResourceResponse {
+	s.Body = v
+	return s
+}
+
+type ExpectDeductResourcesRequest struct {
+	Body *ExpectDeductResourceCmd `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s ExpectDeductResourcesRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ExpectDeductResourcesRequest) GoString() string {
+	return s.String()
+}
+
+func (s *ExpectDeductResourcesRequest) SetBody(v *ExpectDeductResourceCmd) *ExpectDeductResourcesRequest {
+	s.Body = v
+	return s
+}
+
+type ExpectDeductResourcesResponse struct {
+	Headers    map[string]*string          `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                      `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *ExpectDeductResourceResult `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s ExpectDeductResourcesResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ExpectDeductResourcesResponse) GoString() string {
+	return s.String()
+}
+
+func (s *ExpectDeductResourcesResponse) SetHeaders(v map[string]*string) *ExpectDeductResourcesResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *ExpectDeductResourcesResponse) SetStatusCode(v int32) *ExpectDeductResourcesResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *ExpectDeductResourcesResponse) SetBody(v *ExpectDeductResourceResult) *ExpectDeductResourcesResponse {
+	s.Body = v
+	return s
+}
+
+type GetRemainResourceRequest struct {
+	AccountId    *string `json:"accountId,omitempty" xml:"accountId,omitempty"`
+	ResourceType *string `json:"resourceType,omitempty" xml:"resourceType,omitempty"`
+	SubAccountId *string `json:"subAccountId,omitempty" xml:"subAccountId,omitempty"`
+}
+
+func (s GetRemainResourceRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetRemainResourceRequest) GoString() string {
+	return s.String()
+}
+
+func (s *GetRemainResourceRequest) SetAccountId(v string) *GetRemainResourceRequest {
+	s.AccountId = &v
+	return s
+}
+
+func (s *GetRemainResourceRequest) SetResourceType(v string) *GetRemainResourceRequest {
+	s.ResourceType = &v
+	return s
+}
+
+func (s *GetRemainResourceRequest) SetSubAccountId(v string) *GetRemainResourceRequest {
+	s.SubAccountId = &v
+	return s
+}
+
+type GetRemainResourceResponseBody struct {
+	ErrorCode    *string `json:"errorCode,omitempty" xml:"errorCode,omitempty"`
+	ErrorMessage *string `json:"errorMessage,omitempty" xml:"errorMessage,omitempty"`
+	RemainCount  *int32  `json:"remainCount,omitempty" xml:"remainCount,omitempty"`
+	// Id of the request
+	RequestId *string `json:"requestId,omitempty" xml:"requestId,omitempty"`
+	Success   *bool   `json:"success,omitempty" xml:"success,omitempty"`
+}
+
+func (s GetRemainResourceResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetRemainResourceResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *GetRemainResourceResponseBody) SetErrorCode(v string) *GetRemainResourceResponseBody {
+	s.ErrorCode = &v
+	return s
+}
+
+func (s *GetRemainResourceResponseBody) SetErrorMessage(v string) *GetRemainResourceResponseBody {
+	s.ErrorMessage = &v
+	return s
+}
+
+func (s *GetRemainResourceResponseBody) SetRemainCount(v int32) *GetRemainResourceResponseBody {
+	s.RemainCount = &v
+	return s
+}
+
+func (s *GetRemainResourceResponseBody) SetRequestId(v string) *GetRemainResourceResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *GetRemainResourceResponseBody) SetSuccess(v bool) *GetRemainResourceResponseBody {
+	s.Success = &v
+	return s
+}
+
+type GetRemainResourceResponse struct {
+	Headers    map[string]*string             `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                         `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *GetRemainResourceResponseBody `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s GetRemainResourceResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetRemainResourceResponse) GoString() string {
+	return s.String()
+}
+
+func (s *GetRemainResourceResponse) SetHeaders(v map[string]*string) *GetRemainResourceResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *GetRemainResourceResponse) SetStatusCode(v int32) *GetRemainResourceResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *GetRemainResourceResponse) SetBody(v *GetRemainResourceResponseBody) *GetRemainResourceResponse {
 	s.Body = v
 	return s
 }
@@ -490,6 +734,47 @@ func (client *Client) ActualDeductResource(request *ActualDeductResourceRequest)
 	return _result, _err
 }
 
+func (client *Client) ActualDeductResourcesWithOptions(request *ActualDeductResourcesRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *ActualDeductResourcesResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	req := &openapi.OpenApiRequest{
+		Headers: headers,
+		Body:    openapiutil.ParseToMap(request.Body),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("ActualDeductResources"),
+		Version:     tea.String("2024-01-18"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/yic/yic-console/openService/v1/digitalHuman/commands/actualDeductResources"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("ROA"),
+		ReqBodyType: tea.String("json"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &ActualDeductResourcesResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+func (client *Client) ActualDeductResources(request *ActualDeductResourcesRequest) (_result *ActualDeductResourcesResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &ActualDeductResourcesResponse{}
+	_body, _err := client.ActualDeductResourcesWithOptions(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
 func (client *Client) DirectDeductResourceWithOptions(request *DirectDeductResourceRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *DirectDeductResourceResponse, _err error) {
 	_err = util.ValidateModel(request)
 	if _err != nil {
@@ -531,6 +816,47 @@ func (client *Client) DirectDeductResource(request *DirectDeductResourceRequest)
 	return _result, _err
 }
 
+func (client *Client) DirectDeductResourcesWithOptions(request *DirectDeductResourcesRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *DirectDeductResourcesResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	req := &openapi.OpenApiRequest{
+		Headers: headers,
+		Body:    openapiutil.ParseToMap(request.Body),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("DirectDeductResources"),
+		Version:     tea.String("2024-01-18"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/yic/yic-console/openService/v1/digitalHuman/commands/directDeductResources"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("ROA"),
+		ReqBodyType: tea.String("json"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &DirectDeductResourcesResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+func (client *Client) DirectDeductResources(request *DirectDeductResourcesRequest) (_result *DirectDeductResourcesResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &DirectDeductResourcesResponse{}
+	_body, _err := client.DirectDeductResourcesWithOptions(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
 func (client *Client) ExpectDeductResourceWithOptions(request *ExpectDeductResourceRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *ExpectDeductResourceResponse, _err error) {
 	_err = util.ValidateModel(request)
 	if _err != nil {
@@ -565,6 +891,101 @@ func (client *Client) ExpectDeductResource(request *ExpectDeductResourceRequest)
 	headers := make(map[string]*string)
 	_result = &ExpectDeductResourceResponse{}
 	_body, _err := client.ExpectDeductResourceWithOptions(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+func (client *Client) ExpectDeductResourcesWithOptions(request *ExpectDeductResourcesRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *ExpectDeductResourcesResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	req := &openapi.OpenApiRequest{
+		Headers: headers,
+		Body:    openapiutil.ParseToMap(request.Body),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("ExpectDeductResources"),
+		Version:     tea.String("2024-01-18"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/yic/yic-console/openService/v1/digitalHuman/commands/expectDeductResources"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("ROA"),
+		ReqBodyType: tea.String("json"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &ExpectDeductResourcesResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+func (client *Client) ExpectDeductResources(request *ExpectDeductResourcesRequest) (_result *ExpectDeductResourcesResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &ExpectDeductResourcesResponse{}
+	_body, _err := client.ExpectDeductResourcesWithOptions(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+func (client *Client) GetRemainResourceWithOptions(request *GetRemainResourceRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *GetRemainResourceResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	query := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.AccountId)) {
+		query["accountId"] = request.AccountId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.ResourceType)) {
+		query["resourceType"] = request.ResourceType
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.SubAccountId)) {
+		query["subAccountId"] = request.SubAccountId
+	}
+
+	req := &openapi.OpenApiRequest{
+		Headers: headers,
+		Query:   openapiutil.Query(query),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("GetRemainResource"),
+		Version:     tea.String("2024-01-18"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/yic/yic-console/openService/v1/digitalHuman/commands/getRemainResource"),
+		Method:      tea.String("GET"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("ROA"),
+		ReqBodyType: tea.String("json"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &GetRemainResourceResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+func (client *Client) GetRemainResource(request *GetRemainResourceRequest) (_result *GetRemainResourceResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &GetRemainResourceResponse{}
+	_body, _err := client.GetRemainResourceWithOptions(request, headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
