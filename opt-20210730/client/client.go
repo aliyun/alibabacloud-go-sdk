c4cf5de1fc0df0af695c0683ffa6ab9b4f0d9bc0
sdk-team <sdk-team@alibabacloud.com>
2024-04-12 17:11:43 +0000

    Generated golang 2021-07-30 for opt.

--- opt-20210730/client/client.go
+++ opt-20210730/client/client.go
@@ -5,95 +5,79 @@
 package client
 
 import (
-	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
+	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
 	endpointutil "github.com/alibabacloud-go/endpoint-util/service"
-	util "github.com/alibabacloud-go/tea-utils/service"
+	openapiutil "github.com/alibabacloud-go/openapi-util/service"
+	util "github.com/alibabacloud-go/tea-utils/v2/service"
 	"github.com/alibabacloud-go/tea/tea"
 )
 
-type GetOrderUsageRequest struct {
-	RelService   *string `json:"RelService,omitempty" xml:"RelService,omitempty"`
-	ResourceType *int32  `json:"ResourceType,omitempty" xml:"ResourceType,omitempty"`
-	TimeRange    *int32  `json:"TimeRange,omitempty" xml:"TimeRange,omitempty"`
+type GetOpenStatusResponseBody struct {
+	Code      *int32                 `json:"Code,omitempty" xml:"Code,omitempty"`
+	Data      map[string]interface{} `json:"Data,omitempty" xml:"Data,omitempty"`
+	Message   *string                `json:"Message,omitempty" xml:"Message,omitempty"`
+	RequestId *string                `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	Success   *bool                  `json:"Success,omitempty" xml:"Success,omitempty"`
 }
 
-func (s GetOrderUsageRequest) String() string {
+func (s GetOpenStatusResponseBody) String() string {
 	return tea.Prettify(s)
 }
 
-func (s GetOrderUsageRequest) GoString() string {
+func (s GetOpenStatusResponseBody) GoString() string {
 	return s.String()
 }
 
-func (s *GetOrderUsageRequest) SetRelService(v string) *GetOrderUsageRequest {
-	s.RelService = &v
+func (s *GetOpenStatusResponseBody) SetCode(v int32) *GetOpenStatusResponseBody {
+	s.Code = &v
 	return s
 }
 
-func (s *GetOrderUsageRequest) SetResourceType(v int32) *GetOrderUsageRequest {
-	s.ResourceType = &v
-	return s
-}
-
-func (s *GetOrderUsageRequest) SetTimeRange(v int32) *GetOrderUsageRequest {
-	s.TimeRange = &v
-	return s
-}
-
-type GetOrderUsageResponseBody struct {
-	Message   *string                  `json:"Message,omitempty" xml:"Message,omitempty"`
-	Data      []map[string]interface{} `json:"Data,omitempty" xml:"Data,omitempty" type:"Repeated"`
-	RequestId *string                  `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
-	Success   *bool                    `json:"Success,omitempty" xml:"Success,omitempty"`
-}
-
-func (s GetOrderUsageResponseBody) String() string {
-	return tea.Prettify(s)
-}
-
-func (s GetOrderUsageResponseBody) GoString() string {
-	return s.String()
-}
-
-func (s *GetOrderUsageResponseBody) SetMessage(v string) *GetOrderUsageResponseBody {
-	s.Message = &v
-	return s
-}
-
-func (s *GetOrderUsageResponseBody) SetData(v []map[string]interface{}) *GetOrderUsageResponseBody {
+func (s *GetOpenStatusResponseBody) SetData(v map[string]interface{}) *GetOpenStatusResponseBody {
 	s.Data = v
 	return s
 }
 
-func (s *GetOrderUsageResponseBody) SetRequestId(v string) *GetOrderUsageResponseBody {
+func (s *GetOpenStatusResponseBody) SetMessage(v string) *GetOpenStatusResponseBody {
+	s.Message = &v
+	return s
+}
+
+func (s *GetOpenStatusResponseBody) SetRequestId(v string) *GetOpenStatusResponseBody {
 	s.RequestId = &v
 	return s
 }
 
-func (s *GetOrderUsageResponseBody) SetSuccess(v bool) *GetOrderUsageResponseBody {
+func (s *GetOpenStatusResponseBody) SetSuccess(v bool) *GetOpenStatusResponseBody {
 	s.Success = &v
 	return s
 }
 
-type GetOrderUsageResponse struct {
-	Headers map[string]*string         `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
-	Body    *GetOrderUsageResponseBody `json:"body,omitempty" xml:"body,omitempty" require:"true"`
+type GetOpenStatusResponse struct {
+	Headers    map[string]*string         `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                     `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *GetOpenStatusResponseBody `json:"body,omitempty" xml:"body,omitempty"`
 }
 
-func (s GetOrderUsageResponse) String() string {
+func (s GetOpenStatusResponse) String() string {
 	return tea.Prettify(s)
 }
 
-func (s GetOrderUsageResponse) GoString() string {
+func (s GetOpenStatusResponse) GoString() string {
 	return s.String()
 }
 
-func (s *GetOrderUsageResponse) SetHeaders(v map[string]*string) *GetOrderUsageResponse {
+func (s *GetOpenStatusResponse) SetHeaders(v map[string]*string) *GetOpenStatusResponse {
 	s.Headers = v
 	return s
 }
 
-func (s *GetOrderUsageResponse) SetBody(v *GetOrderUsageResponseBody) *GetOrderUsageResponse {
+func (s *GetOpenStatusResponse) SetStatusCode(v int32) *GetOpenStatusResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *GetOpenStatusResponse) SetBody(v *GetOpenStatusResponseBody) *GetOpenStatusResponse {
 	s.Body = v
 	return s
 }
@@ -122,11 +106,11 @@ func (s *GetOrderInfoRequest) SetResourceType(v int32) *GetOrderInfoRequest {
 }
 
 type GetOrderInfoResponseBody struct {
-	Code      *string                `json:"Code,omitempty" xml:"Code,omitempty"`
-	Message   *string                `json:"Message,omitempty" xml:"Message,omitempty"`
-	Data      map[string]interface{} `json:"Data,omitempty" xml:"Data,omitempty"`
-	RequestId *string                `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
-	Success   *bool                  `json:"Success,omitempty" xml:"Success,omitempty"`
+	Code      *string                         `json:"Code,omitempty" xml:"Code,omitempty"`
+	Data      []*GetOrderInfoResponseBodyData `json:"Data,omitempty" xml:"Data,omitempty" type:"Repeated"`
+	Message   *string                         `json:"Message,omitempty" xml:"Message,omitempty"`
+	RequestId *string                         `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	Success   *bool                           `json:"Success,omitempty" xml:"Success,omitempty"`
 }
 
 func (s GetOrderInfoResponseBody) String() string {
@@ -142,13 +126,13 @@ func (s *GetOrderInfoResponseBody) SetCode(v string) *GetOrderInfoResponseBody {
 	return s
 }
 
-func (s *GetOrderInfoResponseBody) SetMessage(v string) *GetOrderInfoResponseBody {
-	s.Message = &v
+func (s *GetOrderInfoResponseBody) SetData(v []*GetOrderInfoResponseBodyData) *GetOrderInfoResponseBody {
+	s.Data = v
 	return s
 }
 
-func (s *GetOrderInfoResponseBody) SetData(v map[string]interface{}) *GetOrderInfoResponseBody {
-	s.Data = v
+func (s *GetOrderInfoResponseBody) SetMessage(v string) *GetOrderInfoResponseBody {
+	s.Message = &v
 	return s
 }
 
@@ -162,9 +146,63 @@ func (s *GetOrderInfoResponseBody) SetSuccess(v bool) *GetOrderInfoResponseBody 
 	return s
 }
 
+type GetOrderInfoResponseBodyData struct {
+	BizType            *string `json:"bizType,omitempty" xml:"bizType,omitempty"`
+	CurrentConcurrency *int32  `json:"currentConcurrency,omitempty" xml:"currentConcurrency,omitempty"`
+	CurrentDays        *int32  `json:"currentDays,omitempty" xml:"currentDays,omitempty"`
+	InstanceId         *string `json:"instanceId,omitempty" xml:"instanceId,omitempty"`
+	LicenseKey         *string `json:"licenseKey,omitempty" xml:"licenseKey,omitempty"`
+	Remark             *string `json:"remark,omitempty" xml:"remark,omitempty"`
+	TotalDays          *int32  `json:"totalDays,omitempty" xml:"totalDays,omitempty"`
+}
+
+func (s GetOrderInfoResponseBodyData) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetOrderInfoResponseBodyData) GoString() string {
+	return s.String()
+}
+
+func (s *GetOrderInfoResponseBodyData) SetBizType(v string) *GetOrderInfoResponseBodyData {
+	s.BizType = &v
+	return s
+}
+
+func (s *GetOrderInfoResponseBodyData) SetCurrentConcurrency(v int32) *GetOrderInfoResponseBodyData {
+	s.CurrentConcurrency = &v
+	return s
+}
+
+func (s *GetOrderInfoResponseBodyData) SetCurrentDays(v int32) *GetOrderInfoResponseBodyData {
+	s.CurrentDays = &v
+	return s
+}
+
+func (s *GetOrderInfoResponseBodyData) SetInstanceId(v string) *GetOrderInfoResponseBodyData {
+	s.InstanceId = &v
+	return s
+}
+
+func (s *GetOrderInfoResponseBodyData) SetLicenseKey(v string) *GetOrderInfoResponseBodyData {
+	s.LicenseKey = &v
+	return s
+}
+
+func (s *GetOrderInfoResponseBodyData) SetRemark(v string) *GetOrderInfoResponseBodyData {
+	s.Remark = &v
+	return s
+}
+
+func (s *GetOrderInfoResponseBodyData) SetTotalDays(v int32) *GetOrderInfoResponseBodyData {
+	s.TotalDays = &v
+	return s
+}
+
 type GetOrderInfoResponse struct {
-	Headers map[string]*string        `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
-	Body    *GetOrderInfoResponseBody `json:"body,omitempty" xml:"body,omitempty" require:"true"`
+	Headers    map[string]*string        `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                    `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *GetOrderInfoResponseBody `json:"body,omitempty" xml:"body,omitempty"`
 }
 
 func (s GetOrderInfoResponse) String() string {
@@ -180,71 +218,111 @@ func (s *GetOrderInfoResponse) SetHeaders(v map[string]*string) *GetOrderInfoRes
 	return s
 }
 
+func (s *GetOrderInfoResponse) SetStatusCode(v int32) *GetOrderInfoResponse {
+	s.StatusCode = &v
+	return s
+}
+
 func (s *GetOrderInfoResponse) SetBody(v *GetOrderInfoResponseBody) *GetOrderInfoResponse {
 	s.Body = v
 	return s
 }
 
-type GetOpenStatusResponseBody struct {
-	Code      *int32                 `json:"Code,omitempty" xml:"Code,omitempty"`
-	Message   *string                `json:"Message,omitempty" xml:"Message,omitempty"`
-	Data      map[string]interface{} `json:"Data,omitempty" xml:"Data,omitempty"`
-	RequestId *string                `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
-	Success   *bool                  `json:"Success,omitempty" xml:"Success,omitempty"`
+type GetOrderUsageRequest struct {
+	LicenseKey   *string `json:"LicenseKey,omitempty" xml:"LicenseKey,omitempty"`
+	RelService   *string `json:"RelService,omitempty" xml:"RelService,omitempty"`
+	ResourceType *int32  `json:"ResourceType,omitempty" xml:"ResourceType,omitempty"`
+	TimeRange    *int32  `json:"TimeRange,omitempty" xml:"TimeRange,omitempty"`
 }
 
-func (s GetOpenStatusResponseBody) String() string {
+func (s GetOrderUsageRequest) String() string {
 	return tea.Prettify(s)
 }
 
-func (s GetOpenStatusResponseBody) GoString() string {
+func (s GetOrderUsageRequest) GoString() string {
 	return s.String()
 }
 
-func (s *GetOpenStatusResponseBody) SetCode(v int32) *GetOpenStatusResponseBody {
-	s.Code = &v
+func (s *GetOrderUsageRequest) SetLicenseKey(v string) *GetOrderUsageRequest {
+	s.LicenseKey = &v
 	return s
 }
 
-func (s *GetOpenStatusResponseBody) SetMessage(v string) *GetOpenStatusResponseBody {
-	s.Message = &v
+func (s *GetOrderUsageRequest) SetRelService(v string) *GetOrderUsageRequest {
+	s.RelService = &v
 	return s
 }
 
-func (s *GetOpenStatusResponseBody) SetData(v map[string]interface{}) *GetOpenStatusResponseBody {
+func (s *GetOrderUsageRequest) SetResourceType(v int32) *GetOrderUsageRequest {
+	s.ResourceType = &v
+	return s
+}
+
+func (s *GetOrderUsageRequest) SetTimeRange(v int32) *GetOrderUsageRequest {
+	s.TimeRange = &v
+	return s
+}
+
+type GetOrderUsageResponseBody struct {
+	Data      []map[string]interface{} `json:"Data,omitempty" xml:"Data,omitempty" type:"Repeated"`
+	Message   *string                  `json:"Message,omitempty" xml:"Message,omitempty"`
+	RequestId *string                  `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	Success   *bool                    `json:"Success,omitempty" xml:"Success,omitempty"`
+}
+
+func (s GetOrderUsageResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetOrderUsageResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *GetOrderUsageResponseBody) SetData(v []map[string]interface{}) *GetOrderUsageResponseBody {
 	s.Data = v
 	return s
 }
 
-func (s *GetOpenStatusResponseBody) SetRequestId(v string) *GetOpenStatusResponseBody {
+func (s *GetOrderUsageResponseBody) SetMessage(v string) *GetOrderUsageResponseBody {
+	s.Message = &v
+	return s
+}
+
+func (s *GetOrderUsageResponseBody) SetRequestId(v string) *GetOrderUsageResponseBody {
 	s.RequestId = &v
 	return s
 }
 
-func (s *GetOpenStatusResponseBody) SetSuccess(v bool) *GetOpenStatusResponseBody {
+func (s *GetOrderUsageResponseBody) SetSuccess(v bool) *GetOrderUsageResponseBody {
 	s.Success = &v
 	return s
 }
 
-type GetOpenStatusResponse struct {
-	Headers map[string]*string         `json:"headers,omitempty" xml:"headers,omitempty" require:"true"`
-	Body    *GetOpenStatusResponseBody `json:"body,omitempty" xml:"body,omitempty" require:"true"`
+type GetOrderUsageResponse struct {
+	Headers    map[string]*string         `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                     `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *GetOrderUsageResponseBody `json:"body,omitempty" xml:"body,omitempty"`
 }
 
-func (s GetOpenStatusResponse) String() string {
+func (s GetOrderUsageResponse) String() string {
 	return tea.Prettify(s)
 }
 
-func (s GetOpenStatusResponse) GoString() string {
+func (s GetOrderUsageResponse) GoString() string {
 	return s.String()
 }
 
-func (s *GetOpenStatusResponse) SetHeaders(v map[string]*string) *GetOpenStatusResponse {
+func (s *GetOrderUsageResponse) SetHeaders(v map[string]*string) *GetOrderUsageResponse {
 	s.Headers = v
 	return s
 }
 
-func (s *GetOpenStatusResponse) SetBody(v *GetOpenStatusResponseBody) *GetOpenStatusResponse {
+func (s *GetOrderUsageResponse) SetStatusCode(v int32) *GetOrderUsageResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *GetOrderUsageResponse) SetBody(v *GetOrderUsageResponseBody) *GetOrderUsageResponse {
 	s.Body = v
 	return s
 }
@@ -296,16 +374,21 @@ func (client *Client) GetEndpoint(productId *string, regionId *string, endpointR
 	return _result, _err
 }
 
-func (client *Client) GetOrderUsageWithOptions(request *GetOrderUsageRequest, runtime *util.RuntimeOptions) (_result *GetOrderUsageResponse, _err error) {
-	_err = util.ValidateModel(request)
-	if _err != nil {
-		return _result, _err
+func (client *Client) GetOpenStatusWithOptions(runtime *util.RuntimeOptions) (_result *GetOpenStatusResponse, _err error) {
+	req := &openapi.OpenApiRequest{}
+	params := &openapi.Params{
+		Action:      tea.String("GetOpenStatus"),
+		Version:     tea.String("2021-07-30"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
 	}
-	req := &openapi.OpenApiRequest{
-		Body: util.ToMap(request),
-	}
-	_result = &GetOrderUsageResponse{}
-	_body, _err := client.DoRPCRequest(tea.String("GetOrderUsage"), tea.String("2021-07-30"), tea.String("HTTPS"), tea.String("POST"), tea.String("AK"), tea.String("json"), req, runtime)
+	_result = &GetOpenStatusResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
 	if _err != nil {
 		return _result, _err
 	}
@@ -313,10 +396,10 @@ func (client *Client) GetOrderUsageWithOptions(request *GetOrderUsageRequest, ru
 	return _result, _err
 }
 
-func (client *Client) GetOrderUsage(request *GetOrderUsageRequest) (_result *GetOrderUsageResponse, _err error) {
+func (client *Client) GetOpenStatus() (_result *GetOpenStatusResponse, _err error) {
 	runtime := &util.RuntimeOptions{}
-	_result = &GetOrderUsageResponse{}
-	_body, _err := client.GetOrderUsageWithOptions(request, runtime)
+	_result = &GetOpenStatusResponse{}
+	_body, _err := client.GetOpenStatusWithOptions(runtime)
 	if _err != nil {
 		return _result, _err
 	}
@@ -329,11 +412,31 @@ func (client *Client) GetOrderInfoWithOptions(request *GetOrderInfoRequest, runt
 	if _err != nil {
 		return _result, _err
 	}
+	query := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.RelService)) {
+		query["RelService"] = request.RelService
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.ResourceType)) {
+		query["ResourceType"] = request.ResourceType
+	}
+
 	req := &openapi.OpenApiRequest{
-		Body: util.ToMap(request),
+		Query: openapiutil.Query(query),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("GetOrderInfo"),
+		Version:     tea.String("2021-07-30"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
 	}
 	_result = &GetOrderInfoResponse{}
-	_body, _err := client.DoRPCRequest(tea.String("GetOrderInfo"), tea.String("2021-07-30"), tea.String("HTTPS"), tea.String("POST"), tea.String("AK"), tea.String("json"), req, runtime)
+	_body, _err := client.CallApi(params, req, runtime)
 	if _err != nil {
 		return _result, _err
 	}
@@ -352,10 +455,44 @@ func (client *Client) GetOrderInfo(request *GetOrderInfoRequest) (_result *GetOr
 	return _result, _err
 }
 
-func (client *Client) GetOpenStatusWithOptions(runtime *util.RuntimeOptions) (_result *GetOpenStatusResponse, _err error) {
-	req := &openapi.OpenApiRequest{}
-	_result = &GetOpenStatusResponse{}
-	_body, _err := client.DoRPCRequest(tea.String("GetOpenStatus"), tea.String("2021-07-30"), tea.String("HTTPS"), tea.String("POST"), tea.String("AK"), tea.String("json"), req, runtime)
+func (client *Client) GetOrderUsageWithOptions(request *GetOrderUsageRequest, runtime *util.RuntimeOptions) (_result *GetOrderUsageResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	query := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.LicenseKey)) {
+		query["LicenseKey"] = request.LicenseKey
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.RelService)) {
+		query["RelService"] = request.RelService
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.ResourceType)) {
+		query["ResourceType"] = request.ResourceType
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.TimeRange)) {
+		query["TimeRange"] = request.TimeRange
+	}
+
+	req := &openapi.OpenApiRequest{
+		Query: openapiutil.Query(query),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("GetOrderUsage"),
+		Version:     tea.String("2021-07-30"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &GetOrderUsageResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
 	if _err != nil {
 		return _result, _err
 	}
@@ -363,10 +500,10 @@ func (client *Client) GetOpenStatusWithOptions(runtime *util.RuntimeOptions) (_r
 	return _result, _err
 }
 
-func (client *Client) GetOpenStatus() (_result *GetOpenStatusResponse, _err error) {
+func (client *Client) GetOrderUsage(request *GetOrderUsageRequest) (_result *GetOrderUsageResponse, _err error) {
 	runtime := &util.RuntimeOptions{}
-	_result = &GetOpenStatusResponse{}
-	_body, _err := client.GetOpenStatusWithOptions(runtime)
+	_result = &GetOrderUsageResponse{}
+	_body, _err := client.GetOrderUsageWithOptions(request, runtime)
 	if _err != nil {
 		return _result, _err
 	}
--- opt-20210730/go.mod
+++ opt-20210730/go.mod
@@ -3,8 +3,9 @@ module github.com/alibabacloud-go/opt-20210730
 go 1.15
 
 require (
-	github.com/alibabacloud-go/darabonba-openapi v0.1.4
+	github.com/alibabacloud-go/darabonba-openapi/v2 v2.0.6
 	github.com/alibabacloud-go/endpoint-util v1.1.0
-	github.com/alibabacloud-go/tea v1.1.15
-	github.com/alibabacloud-go/tea-utils v1.3.9
+	github.com/alibabacloud-go/openapi-util v0.1.0
+	github.com/alibabacloud-go/tea v1.2.1
+	github.com/alibabacloud-go/tea-utils/v2 v2.0.5
 )
