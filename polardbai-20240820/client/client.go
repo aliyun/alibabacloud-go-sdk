a6ce3b6d11899c8c728d9fb81ed2850726dd3c3f
sdk-team <sdk-team@alibabacloud.com>
2024-12-12 03:56:56 +0000

    Support API Predict.

--- polardbai-20240820/client/client.go
+++ polardbai-20240820/client/client.go
@@ -20,6 +20,140 @@ func (s OpenApiModelsPredictCmd) GoString() string {
 	return s.String()
 }
 
+type PredictRequest struct {
+	// example:
+	//
+	// db_test
+	DbName *string `json:"dbName,omitempty" xml:"dbName,omitempty"`
+	Input  *string `json:"input,omitempty" xml:"input,omitempty"`
+	// example:
+	//
+	// pc-2ze454l20me07****
+	InstanceName *string `json:"instanceName,omitempty" xml:"instanceName,omitempty"`
+	// example:
+	//
+	// _polar4ai_tongyi
+	ModelClass *string                `json:"modelClass,omitempty" xml:"modelClass,omitempty"`
+	Parameters map[string]interface{} `json:"parameters,omitempty" xml:"parameters,omitempty"`
+}
+
+func (s PredictRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s PredictRequest) GoString() string {
+	return s.String()
+}
+
+func (s *PredictRequest) SetDbName(v string) *PredictRequest {
+	s.DbName = &v
+	return s
+}
+
+func (s *PredictRequest) SetInput(v string) *PredictRequest {
+	s.Input = &v
+	return s
+}
+
+func (s *PredictRequest) SetInstanceName(v string) *PredictRequest {
+	s.InstanceName = &v
+	return s
+}
+
+func (s *PredictRequest) SetModelClass(v string) *PredictRequest {
+	s.ModelClass = &v
+	return s
+}
+
+func (s *PredictRequest) SetParameters(v map[string]interface{}) *PredictRequest {
+	s.Parameters = v
+	return s
+}
+
+type PredictResponseBody struct {
+	Data interface{} `json:"data,omitempty" xml:"data,omitempty"`
+	// example:
+	//
+	// ER_ILLEGAL_MODEL_CLASS
+	ErrCode *string `json:"errCode,omitempty" xml:"errCode,omitempty"`
+	// example:
+	//
+	// Illegal model class.
+	ErrMessage *string `json:"errMessage,omitempty" xml:"errMessage,omitempty"`
+	// Id of the request
+	//
+	// example:
+	//
+	// 983863E2-****-1D15-A4AE-3468CD75383D
+	RequestId *string `json:"requestId,omitempty" xml:"requestId,omitempty"`
+	// example:
+	//
+	// true
+	Success *bool `json:"success,omitempty" xml:"success,omitempty"`
+}
+
+func (s PredictResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s PredictResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *PredictResponseBody) SetData(v interface{}) *PredictResponseBody {
+	s.Data = v
+	return s
+}
+
+func (s *PredictResponseBody) SetErrCode(v string) *PredictResponseBody {
+	s.ErrCode = &v
+	return s
+}
+
+func (s *PredictResponseBody) SetErrMessage(v string) *PredictResponseBody {
+	s.ErrMessage = &v
+	return s
+}
+
+func (s *PredictResponseBody) SetRequestId(v string) *PredictResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *PredictResponseBody) SetSuccess(v bool) *PredictResponseBody {
+	s.Success = &v
+	return s
+}
+
+type PredictResponse struct {
+	Headers    map[string]*string   `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32               `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *PredictResponseBody `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s PredictResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s PredictResponse) GoString() string {
+	return s.String()
+}
+
+func (s *PredictResponse) SetHeaders(v map[string]*string) *PredictResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *PredictResponse) SetStatusCode(v int32) *PredictResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *PredictResponse) SetBody(v *PredictResponseBody) *PredictResponse {
+	s.Body = v
+	return s
+}
+
 type PredictSseRequest struct {
 	// example:
 	//
@@ -197,6 +331,86 @@ func (client *Client) GetEndpoint(productId *string, regionId *string, endpointR
 	}
 
 	_body, _err := endpointutil.GetEndpointRules(productId, regionId, endpointRule, network, suffix)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+// Summary:
+//
+// 模型预测
+//
+// @param request - PredictRequest
+//
+// @param headers - map
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return PredictResponse
+func (client *Client) PredictWithOptions(request *PredictRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *PredictResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	body := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.DbName)) {
+		body["dbName"] = request.DbName
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.Input)) {
+		body["input"] = request.Input
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.InstanceName)) {
+		body["instanceName"] = request.InstanceName
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.ModelClass)) {
+		body["modelClass"] = request.ModelClass
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.Parameters)) {
+		body["parameters"] = request.Parameters
+	}
+
+	req := &openapi.OpenApiRequest{
+		Headers: headers,
+		Body:    openapiutil.ParseToMap(body),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("Predict"),
+		Version:     tea.String("2024-08-20"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/v1/openapi/models/predict"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("ROA"),
+		ReqBodyType: tea.String("json"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &PredictResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+// Summary:
+//
+// 模型预测
+//
+// @param request - PredictRequest
+//
+// @return PredictResponse
+func (client *Client) Predict(request *PredictRequest) (_result *PredictResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &PredictResponse{}
+	_body, _err := client.PredictWithOptions(request, headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
