5fd22ab6e1b655b04baa3b9ee6f6c2c009f0a84b
sdk-team <sdk-team@alibabacloud.com>
2024-12-26 17:15:44 +0000

    Support API ChatMessage.

--- aimath-20241114/client/client.go
+++ aimath-20241114/client/client.go
@@ -9,6 +9,416 @@ import (
 	"github.com/alibabacloud-go/tea/tea"
 )
 
+type ChatMessageRequest struct {
+	// This parameter is required.
+	//
+	// example:
+	//
+	// hello world！
+	Content *string `json:"Content,omitempty" xml:"Content,omitempty"`
+	// This parameter is required.
+	//
+	// example:
+	//
+	// 723b7f0f-c28a-4123-86e7-094d3d3863f8
+	ConversationId *string `json:"ConversationId,omitempty" xml:"ConversationId,omitempty"`
+	// This parameter is required.
+	//
+	// example:
+	//
+	// c28a-4123-86e7
+	UserId *string `json:"UserId,omitempty" xml:"UserId,omitempty"`
+}
+
+func (s ChatMessageRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ChatMessageRequest) GoString() string {
+	return s.String()
+}
+
+func (s *ChatMessageRequest) SetContent(v string) *ChatMessageRequest {
+	s.Content = &v
+	return s
+}
+
+func (s *ChatMessageRequest) SetConversationId(v string) *ChatMessageRequest {
+	s.ConversationId = &v
+	return s
+}
+
+func (s *ChatMessageRequest) SetUserId(v string) *ChatMessageRequest {
+	s.UserId = &v
+	return s
+}
+
+type ChatMessageResponseBody struct {
+	Content *string `json:"Content,omitempty" xml:"Content,omitempty"`
+	// example:
+	//
+	// Param.Invalid
+	ErrCode *string `json:"ErrCode,omitempty" xml:"ErrCode,omitempty"`
+	// example:
+	//
+	// The specified parameter is not valid.
+	ErrMsg    *string `json:"ErrMsg,omitempty" xml:"ErrMsg,omitempty"`
+	EventType *string `json:"EventType,omitempty" xml:"EventType,omitempty"`
+	// Id of the request
+	//
+	// example:
+	//
+	// 696acaa9-eb29-4c1f-b48a-1f901579acc5
+	RequestId *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	// example:
+	//
+	// true
+	Success *bool `json:"Success,omitempty" xml:"Success,omitempty"`
+}
+
+func (s ChatMessageResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ChatMessageResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *ChatMessageResponseBody) SetContent(v string) *ChatMessageResponseBody {
+	s.Content = &v
+	return s
+}
+
+func (s *ChatMessageResponseBody) SetErrCode(v string) *ChatMessageResponseBody {
+	s.ErrCode = &v
+	return s
+}
+
+func (s *ChatMessageResponseBody) SetErrMsg(v string) *ChatMessageResponseBody {
+	s.ErrMsg = &v
+	return s
+}
+
+func (s *ChatMessageResponseBody) SetEventType(v string) *ChatMessageResponseBody {
+	s.EventType = &v
+	return s
+}
+
+func (s *ChatMessageResponseBody) SetRequestId(v string) *ChatMessageResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *ChatMessageResponseBody) SetSuccess(v bool) *ChatMessageResponseBody {
+	s.Success = &v
+	return s
+}
+
+type ChatMessageResponse struct {
+	Headers    map[string]*string       `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                   `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *ChatMessageResponseBody `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s ChatMessageResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ChatMessageResponse) GoString() string {
+	return s.String()
+}
+
+func (s *ChatMessageResponse) SetHeaders(v map[string]*string) *ChatMessageResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *ChatMessageResponse) SetStatusCode(v int32) *ChatMessageResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *ChatMessageResponse) SetBody(v *ChatMessageResponseBody) *ChatMessageResponse {
+	s.Body = v
+	return s
+}
+
+type CreateConversationRequest struct {
+	ExerciseAnalysis *string `json:"ExerciseAnalysis,omitempty" xml:"ExerciseAnalysis,omitempty"`
+	ExerciseAnswer   *string `json:"ExerciseAnswer,omitempty" xml:"ExerciseAnswer,omitempty"`
+	// This parameter is required.
+	ExerciseContent *string `json:"ExerciseContent,omitempty" xml:"ExerciseContent,omitempty"`
+	ExerciseType    *string `json:"ExerciseType,omitempty" xml:"ExerciseType,omitempty"`
+	// This parameter is required.
+	//
+	// example:
+	//
+	// 2r560eHAbsknrfHXVZO4L
+	OuterBizId *string `json:"OuterBizId,omitempty" xml:"OuterBizId,omitempty"`
+	// This parameter is required.
+	//
+	// example:
+	//
+	// wx-xx-yy
+	UserId *string `json:"UserId,omitempty" xml:"UserId,omitempty"`
+}
+
+func (s CreateConversationRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s CreateConversationRequest) GoString() string {
+	return s.String()
+}
+
+func (s *CreateConversationRequest) SetExerciseAnalysis(v string) *CreateConversationRequest {
+	s.ExerciseAnalysis = &v
+	return s
+}
+
+func (s *CreateConversationRequest) SetExerciseAnswer(v string) *CreateConversationRequest {
+	s.ExerciseAnswer = &v
+	return s
+}
+
+func (s *CreateConversationRequest) SetExerciseContent(v string) *CreateConversationRequest {
+	s.ExerciseContent = &v
+	return s
+}
+
+func (s *CreateConversationRequest) SetExerciseType(v string) *CreateConversationRequest {
+	s.ExerciseType = &v
+	return s
+}
+
+func (s *CreateConversationRequest) SetOuterBizId(v string) *CreateConversationRequest {
+	s.OuterBizId = &v
+	return s
+}
+
+func (s *CreateConversationRequest) SetUserId(v string) *CreateConversationRequest {
+	s.UserId = &v
+	return s
+}
+
+type CreateConversationResponseBody struct {
+	// example:
+	//
+	// a499fef7-fef7-453c-a6b2-6a34089613e8
+	ConversationId *string `json:"ConversationId,omitempty" xml:"ConversationId,omitempty"`
+	// example:
+	//
+	// InternalError
+	ErrCode *string `json:"ErrCode,omitempty" xml:"ErrCode,omitempty"`
+	// example:
+	//
+	// The request processing has failed due to some unknown error.
+	ErrMsg *string `json:"ErrMsg,omitempty" xml:"ErrMsg,omitempty"`
+	// Id of the request
+	//
+	// example:
+	//
+	// 89C27D03-4C85-5420-9752-989130878F4D
+	RequestId *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	// example:
+	//
+	// true
+	Success *bool `json:"Success,omitempty" xml:"Success,omitempty"`
+}
+
+func (s CreateConversationResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s CreateConversationResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *CreateConversationResponseBody) SetConversationId(v string) *CreateConversationResponseBody {
+	s.ConversationId = &v
+	return s
+}
+
+func (s *CreateConversationResponseBody) SetErrCode(v string) *CreateConversationResponseBody {
+	s.ErrCode = &v
+	return s
+}
+
+func (s *CreateConversationResponseBody) SetErrMsg(v string) *CreateConversationResponseBody {
+	s.ErrMsg = &v
+	return s
+}
+
+func (s *CreateConversationResponseBody) SetRequestId(v string) *CreateConversationResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *CreateConversationResponseBody) SetSuccess(v bool) *CreateConversationResponseBody {
+	s.Success = &v
+	return s
+}
+
+type CreateConversationResponse struct {
+	Headers    map[string]*string              `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                          `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *CreateConversationResponseBody `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s CreateConversationResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s CreateConversationResponse) GoString() string {
+	return s.String()
+}
+
+func (s *CreateConversationResponse) SetHeaders(v map[string]*string) *CreateConversationResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *CreateConversationResponse) SetStatusCode(v int32) *CreateConversationResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *CreateConversationResponse) SetBody(v *CreateConversationResponseBody) *CreateConversationResponse {
+	s.Body = v
+	return s
+}
+
+type CreateRelatedConversationRequest struct {
+	// This parameter is required.
+	//
+	// example:
+	//
+	// Ex_pop_1731848070815_funI
+	ExerciseCode *string `json:"ExerciseCode,omitempty" xml:"ExerciseCode,omitempty"`
+	// This parameter is required.
+	//
+	// example:
+	//
+	// 725e5550-8f81-42e0-a4db-d2de1be52afc
+	OuterBizId *string `json:"OuterBizId,omitempty" xml:"OuterBizId,omitempty"`
+	// This parameter is required.
+	//
+	// example:
+	//
+	// pop_1731848070815
+	UserId *string `json:"UserId,omitempty" xml:"UserId,omitempty"`
+}
+
+func (s CreateRelatedConversationRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s CreateRelatedConversationRequest) GoString() string {
+	return s.String()
+}
+
+func (s *CreateRelatedConversationRequest) SetExerciseCode(v string) *CreateRelatedConversationRequest {
+	s.ExerciseCode = &v
+	return s
+}
+
+func (s *CreateRelatedConversationRequest) SetOuterBizId(v string) *CreateRelatedConversationRequest {
+	s.OuterBizId = &v
+	return s
+}
+
+func (s *CreateRelatedConversationRequest) SetUserId(v string) *CreateRelatedConversationRequest {
+	s.UserId = &v
+	return s
+}
+
+type CreateRelatedConversationResponseBody struct {
+	// example:
+	//
+	// 96d36ed0-ebde-11ee-806f-f35ee6682ec5
+	ConversationId *string `json:"ConversationId,omitempty" xml:"ConversationId,omitempty"`
+	// example:
+	//
+	// InternalError
+	ErrCode *string `json:"ErrCode,omitempty" xml:"ErrCode,omitempty"`
+	// example:
+	//
+	// The request processing has failed due to some unknown error.
+	ErrMsg *string `json:"ErrMsg,omitempty" xml:"ErrMsg,omitempty"`
+	// Id of the request
+	//
+	// example:
+	//
+	// 1d31b11b-4b82-4db1-b3c0-001529fc78eb
+	RequestId *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	// example:
+	//
+	// true
+	Success *bool `json:"Success,omitempty" xml:"Success,omitempty"`
+}
+
+func (s CreateRelatedConversationResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s CreateRelatedConversationResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *CreateRelatedConversationResponseBody) SetConversationId(v string) *CreateRelatedConversationResponseBody {
+	s.ConversationId = &v
+	return s
+}
+
+func (s *CreateRelatedConversationResponseBody) SetErrCode(v string) *CreateRelatedConversationResponseBody {
+	s.ErrCode = &v
+	return s
+}
+
+func (s *CreateRelatedConversationResponseBody) SetErrMsg(v string) *CreateRelatedConversationResponseBody {
+	s.ErrMsg = &v
+	return s
+}
+
+func (s *CreateRelatedConversationResponseBody) SetRequestId(v string) *CreateRelatedConversationResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *CreateRelatedConversationResponseBody) SetSuccess(v bool) *CreateRelatedConversationResponseBody {
+	s.Success = &v
+	return s
+}
+
+type CreateRelatedConversationResponse struct {
+	Headers    map[string]*string                     `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                                 `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *CreateRelatedConversationResponseBody `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s CreateRelatedConversationResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s CreateRelatedConversationResponse) GoString() string {
+	return s.String()
+}
+
+func (s *CreateRelatedConversationResponse) SetHeaders(v map[string]*string) *CreateRelatedConversationResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *CreateRelatedConversationResponse) SetStatusCode(v int32) *CreateRelatedConversationResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *CreateRelatedConversationResponse) SetBody(v *CreateRelatedConversationResponseBody) *CreateRelatedConversationResponse {
+	s.Body = v
+	return s
+}
+
 type GenAnalysisRequest struct {
 	// This parameter is required.
 	ExerciseContent *string `json:"ExerciseContent,omitempty" xml:"ExerciseContent,omitempty"`
@@ -596,6 +1006,222 @@ func (client *Client) GetEndpoint(productId *string, regionId *string, endpointR
 	}
 
 	_body, _err := endpointutil.GetEndpointRules(productId, regionId, endpointRule, network, suffix)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+// Summary:
+//
+// 聊天消息API
+//
+// @param request - ChatMessageRequest
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return ChatMessageResponse
+func (client *Client) ChatMessageWithOptions(request *ChatMessageRequest, runtime *util.RuntimeOptions) (_result *ChatMessageResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	body := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.Content)) {
+		body["Content"] = request.Content
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.ConversationId)) {
+		body["ConversationId"] = request.ConversationId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.UserId)) {
+		body["UserId"] = request.UserId
+	}
+
+	req := &openapi.OpenApiRequest{
+		Body: openapiutil.ParseToMap(body),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("ChatMessage"),
+		Version:     tea.String("2024-11-14"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &ChatMessageResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+// Summary:
+//
+// 聊天消息API
+//
+// @param request - ChatMessageRequest
+//
+// @return ChatMessageResponse
+func (client *Client) ChatMessage(request *ChatMessageRequest) (_result *ChatMessageResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &ChatMessageResponse{}
+	_body, _err := client.ChatMessageWithOptions(request, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+// Summary:
+//
+// 创建试题相应的对话。
+//
+// @param request - CreateConversationRequest
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return CreateConversationResponse
+func (client *Client) CreateConversationWithOptions(request *CreateConversationRequest, runtime *util.RuntimeOptions) (_result *CreateConversationResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	body := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.ExerciseAnalysis)) {
+		body["ExerciseAnalysis"] = request.ExerciseAnalysis
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.ExerciseAnswer)) {
+		body["ExerciseAnswer"] = request.ExerciseAnswer
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.ExerciseContent)) {
+		body["ExerciseContent"] = request.ExerciseContent
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.ExerciseType)) {
+		body["ExerciseType"] = request.ExerciseType
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.OuterBizId)) {
+		body["OuterBizId"] = request.OuterBizId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.UserId)) {
+		body["UserId"] = request.UserId
+	}
+
+	req := &openapi.OpenApiRequest{
+		Body: openapiutil.ParseToMap(body),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("CreateConversation"),
+		Version:     tea.String("2024-11-14"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &CreateConversationResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+// Summary:
+//
+// 创建试题相应的对话。
+//
+// @param request - CreateConversationRequest
+//
+// @return CreateConversationResponse
+func (client *Client) CreateConversation(request *CreateConversationRequest) (_result *CreateConversationResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &CreateConversationResponse{}
+	_body, _err := client.CreateConversationWithOptions(request, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+// Summary:
+//
+// 创建关联的对话，输入试题code即可开启对话
+//
+// @param request - CreateRelatedConversationRequest
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return CreateRelatedConversationResponse
+func (client *Client) CreateRelatedConversationWithOptions(request *CreateRelatedConversationRequest, runtime *util.RuntimeOptions) (_result *CreateRelatedConversationResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	body := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.ExerciseCode)) {
+		body["ExerciseCode"] = request.ExerciseCode
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.OuterBizId)) {
+		body["OuterBizId"] = request.OuterBizId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.UserId)) {
+		body["UserId"] = request.UserId
+	}
+
+	req := &openapi.OpenApiRequest{
+		Body: openapiutil.ParseToMap(body),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("CreateRelatedConversation"),
+		Version:     tea.String("2024-11-14"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &CreateRelatedConversationResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+// Summary:
+//
+// 创建关联的对话，输入试题code即可开启对话
+//
+// @param request - CreateRelatedConversationRequest
+//
+// @return CreateRelatedConversationResponse
+func (client *Client) CreateRelatedConversation(request *CreateRelatedConversationRequest) (_result *CreateRelatedConversationResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &CreateRelatedConversationResponse{}
+	_body, _err := client.CreateRelatedConversationWithOptions(request, runtime)
 	if _err != nil {
 		return _result, _err
 	}
