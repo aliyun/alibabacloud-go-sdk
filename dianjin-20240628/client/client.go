c043dcd0014907214f8f0b70d67dca00911a3d54
sdk-team <sdk-team@alibabacloud.com>
2025-01-02 09:49:31 +0000

    Support API GetDialogAnalysisResult.

--- dianjin-20240628/client/client.go
+++ dianjin-20240628/client/client.go
@@ -3097,6 +3097,293 @@ func (s *GetChatQuestionRespResponse) SetBody(v *GetChatQuestionRespResponseBody
 	return s
 }
 
+type GetDialogAnalysisResultRequest struct {
+	// example:
+	//
+	// true
+	Asc *bool `json:"asc,omitempty" xml:"asc,omitempty"`
+	// example:
+	//
+	// 2024-09-23 09:20:02
+	EndTime    *string   `json:"endTime,omitempty" xml:"endTime,omitempty"`
+	SessionIds []*string `json:"sessionIds,omitempty" xml:"sessionIds,omitempty" type:"Repeated"`
+	// example:
+	//
+	// 2024-09-14 09:11:00
+	StartTime *string `json:"startTime,omitempty" xml:"startTime,omitempty"`
+	// example:
+	//
+	// true
+	UseUrl *bool `json:"useUrl,omitempty" xml:"useUrl,omitempty"`
+}
+
+func (s GetDialogAnalysisResultRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetDialogAnalysisResultRequest) GoString() string {
+	return s.String()
+}
+
+func (s *GetDialogAnalysisResultRequest) SetAsc(v bool) *GetDialogAnalysisResultRequest {
+	s.Asc = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultRequest) SetEndTime(v string) *GetDialogAnalysisResultRequest {
+	s.EndTime = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultRequest) SetSessionIds(v []*string) *GetDialogAnalysisResultRequest {
+	s.SessionIds = v
+	return s
+}
+
+func (s *GetDialogAnalysisResultRequest) SetStartTime(v string) *GetDialogAnalysisResultRequest {
+	s.StartTime = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultRequest) SetUseUrl(v bool) *GetDialogAnalysisResultRequest {
+	s.UseUrl = &v
+	return s
+}
+
+type GetDialogAnalysisResultResponseBody struct {
+	// example:
+	//
+	// null
+	Cost *int64                                   `json:"cost,omitempty" xml:"cost,omitempty"`
+	Data *GetDialogAnalysisResultResponseBodyData `json:"data,omitempty" xml:"data,omitempty" type:"Struct"`
+	// example:
+	//
+	// null
+	DataType *string `json:"dataType,omitempty" xml:"dataType,omitempty"`
+	// example:
+	//
+	// 0
+	ErrCode *string `json:"errCode,omitempty" xml:"errCode,omitempty"`
+	// example:
+	//
+	// ok
+	Message *string `json:"message,omitempty" xml:"message,omitempty"`
+	// example:
+	//
+	// 88A006F0-B565-53BA-B38A-DBDF9D0B2935
+	RequestId *string `json:"requestId,omitempty" xml:"requestId,omitempty"`
+	// example:
+	//
+	// true
+	Success *bool `json:"success,omitempty" xml:"success,omitempty"`
+	// example:
+	//
+	// 2024-04-24 11:54:34
+	Time *string `json:"time,omitempty" xml:"time,omitempty"`
+}
+
+func (s GetDialogAnalysisResultResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetDialogAnalysisResultResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *GetDialogAnalysisResultResponseBody) SetCost(v int64) *GetDialogAnalysisResultResponseBody {
+	s.Cost = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponseBody) SetData(v *GetDialogAnalysisResultResponseBodyData) *GetDialogAnalysisResultResponseBody {
+	s.Data = v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponseBody) SetDataType(v string) *GetDialogAnalysisResultResponseBody {
+	s.DataType = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponseBody) SetErrCode(v string) *GetDialogAnalysisResultResponseBody {
+	s.ErrCode = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponseBody) SetMessage(v string) *GetDialogAnalysisResultResponseBody {
+	s.Message = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponseBody) SetRequestId(v string) *GetDialogAnalysisResultResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponseBody) SetSuccess(v bool) *GetDialogAnalysisResultResponseBody {
+	s.Success = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponseBody) SetTime(v string) *GetDialogAnalysisResultResponseBody {
+	s.Time = &v
+	return s
+}
+
+type GetDialogAnalysisResultResponseBodyData struct {
+	DialogAnalysisRespList []*GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList `json:"dialogAnalysisRespList,omitempty" xml:"dialogAnalysisRespList,omitempty" type:"Repeated"`
+}
+
+func (s GetDialogAnalysisResultResponseBodyData) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetDialogAnalysisResultResponseBodyData) GoString() string {
+	return s.String()
+}
+
+func (s *GetDialogAnalysisResultResponseBodyData) SetDialogAnalysisRespList(v []*GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList) *GetDialogAnalysisResultResponseBodyData {
+	s.DialogAnalysisRespList = v
+	return s
+}
+
+type GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList struct {
+	AnalysisResp *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisResp `json:"analysisResp,omitempty" xml:"analysisResp,omitempty" type:"Struct"`
+	// example:
+	//
+	// 2024-04-24 11:54:34
+	GmtCreate *string `json:"gmtCreate,omitempty" xml:"gmtCreate,omitempty"`
+	// example:
+	//
+	// https://xxx.oss-cn-beijing.aliyuncs.com/dialog-analysis/2024-12-30/2/1826661605606129665
+	OssUrl *string `json:"ossUrl,omitempty" xml:"ossUrl,omitempty"`
+	// example:
+	//
+	// 183764873624
+	SessionId *string `json:"sessionId,omitempty" xml:"sessionId,omitempty"`
+	// example:
+	//
+	// running
+	Status *string `json:"status,omitempty" xml:"status,omitempty"`
+}
+
+func (s GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList) GoString() string {
+	return s.String()
+}
+
+func (s *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList) SetAnalysisResp(v *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisResp) *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList {
+	s.AnalysisResp = v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList) SetGmtCreate(v string) *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList {
+	s.GmtCreate = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList) SetOssUrl(v string) *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList {
+	s.OssUrl = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList) SetSessionId(v string) *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList {
+	s.SessionId = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList) SetStatus(v string) *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespList {
+	s.Status = &v
+	return s
+}
+
+type GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisResp struct {
+	DialogExecPlan *string                                                                                  `json:"dialogExecPlan,omitempty" xml:"dialogExecPlan,omitempty"`
+	DialogLabels   []*GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisRespDialogLabels `json:"dialogLabels,omitempty" xml:"dialogLabels,omitempty" type:"Repeated"`
+	DialogSummary  *string                                                                                  `json:"dialogSummary,omitempty" xml:"dialogSummary,omitempty"`
+}
+
+func (s GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisResp) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisResp) GoString() string {
+	return s.String()
+}
+
+func (s *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisResp) SetDialogExecPlan(v string) *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisResp {
+	s.DialogExecPlan = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisResp) SetDialogLabels(v []*GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisRespDialogLabels) *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisResp {
+	s.DialogLabels = v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisResp) SetDialogSummary(v string) *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisResp {
+	s.DialogSummary = &v
+	return s
+}
+
+type GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisRespDialogLabels struct {
+	Name *string `json:"name,omitempty" xml:"name,omitempty"`
+	// example:
+	//
+	// 0
+	Value *string `json:"value,omitempty" xml:"value,omitempty"`
+}
+
+func (s GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisRespDialogLabels) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisRespDialogLabels) GoString() string {
+	return s.String()
+}
+
+func (s *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisRespDialogLabels) SetName(v string) *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisRespDialogLabels {
+	s.Name = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisRespDialogLabels) SetValue(v string) *GetDialogAnalysisResultResponseBodyDataDialogAnalysisRespListAnalysisRespDialogLabels {
+	s.Value = &v
+	return s
+}
+
+type GetDialogAnalysisResultResponse struct {
+	Headers    map[string]*string                   `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                               `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *GetDialogAnalysisResultResponseBody `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s GetDialogAnalysisResultResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetDialogAnalysisResultResponse) GoString() string {
+	return s.String()
+}
+
+func (s *GetDialogAnalysisResultResponse) SetHeaders(v map[string]*string) *GetDialogAnalysisResultResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponse) SetStatusCode(v int32) *GetDialogAnalysisResultResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *GetDialogAnalysisResultResponse) SetBody(v *GetDialogAnalysisResultResponseBody) *GetDialogAnalysisResultResponse {
+	s.Body = v
+	return s
+}
+
 type GetDocumentChunkListRequest struct {
 	ChunkIdList []*string `json:"chunkIdList,omitempty" xml:"chunkIdList,omitempty" type:"Repeated"`
 	// This parameter is required.
@@ -13114,6 +13401,86 @@ func (client *Client) GetChatQuestionResp(workspaceId *string, request *GetChatQ
 	headers := make(map[string]*string)
 	_result = &GetChatQuestionRespResponse{}
 	_body, _err := client.GetChatQuestionRespWithOptions(workspaceId, request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+// Summary:
+//
+// 获取外呼会话分析结果
+//
+// @param request - GetDialogAnalysisResultRequest
+//
+// @param headers - map
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return GetDialogAnalysisResultResponse
+func (client *Client) GetDialogAnalysisResultWithOptions(workspaceId *string, request *GetDialogAnalysisResultRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *GetDialogAnalysisResultResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	body := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.Asc)) {
+		body["asc"] = request.Asc
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.EndTime)) {
+		body["endTime"] = request.EndTime
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.SessionIds)) {
+		body["sessionIds"] = request.SessionIds
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.StartTime)) {
+		body["startTime"] = request.StartTime
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.UseUrl)) {
+		body["useUrl"] = request.UseUrl
+	}
+
+	req := &openapi.OpenApiRequest{
+		Headers: headers,
+		Body:    openapiutil.ParseToMap(body),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("GetDialogAnalysisResult"),
+		Version:     tea.String("2024-06-28"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/" + tea.StringValue(openapiutil.GetEncodeParam(workspaceId)) + "/api/virtualHuman/dialog/analysis"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("ROA"),
+		ReqBodyType: tea.String("json"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &GetDialogAnalysisResultResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+// Summary:
+//
+// 获取外呼会话分析结果
+//
+// @param request - GetDialogAnalysisResultRequest
+//
+// @return GetDialogAnalysisResultResponse
+func (client *Client) GetDialogAnalysisResult(workspaceId *string, request *GetDialogAnalysisResultRequest) (_result *GetDialogAnalysisResultResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &GetDialogAnalysisResultResponse{}
+	_body, _err := client.GetDialogAnalysisResultWithOptions(workspaceId, request, headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
