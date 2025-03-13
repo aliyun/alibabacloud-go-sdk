ccf5fc6d3245450ad52cceac937e4a4c6b1c8be9
sdk-team <sdk-team@alibabacloud.com>
2025-03-13 10:13:25 +0000

    Support API DescribeSellerInstances.

--- marketplaceintl-20221230/client/client.go
+++ marketplaceintl-20221230/client/client.go
@@ -9,6 +9,283 @@ import (
 	"github.com/alibabacloud-go/tea/tea"
 )
 
+type DescribeSellerInstancesRequest struct {
+	// example:
+	//
+	// 5000002763123
+	InstanceId *int64 `json:"InstanceId,omitempty" xml:"InstanceId,omitempty"`
+	// example:
+	//
+	// OPENED
+	InstanceStatus *string `json:"InstanceStatus,omitempty" xml:"InstanceStatus,omitempty"`
+	// example:
+	//
+	// 1
+	PageIndex *int32 `json:"PageIndex,omitempty" xml:"PageIndex,omitempty"`
+	// example:
+	//
+	// 10
+	PageSize *int64 `json:"PageSize,omitempty" xml:"PageSize,omitempty"`
+	// example:
+	//
+	// 5322460655123456
+	UserId *int64 `json:"UserId,omitempty" xml:"UserId,omitempty"`
+}
+
+func (s DescribeSellerInstancesRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeSellerInstancesRequest) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeSellerInstancesRequest) SetInstanceId(v int64) *DescribeSellerInstancesRequest {
+	s.InstanceId = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesRequest) SetInstanceStatus(v string) *DescribeSellerInstancesRequest {
+	s.InstanceStatus = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesRequest) SetPageIndex(v int32) *DescribeSellerInstancesRequest {
+	s.PageIndex = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesRequest) SetPageSize(v int64) *DescribeSellerInstancesRequest {
+	s.PageSize = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesRequest) SetUserId(v int64) *DescribeSellerInstancesRequest {
+	s.UserId = &v
+	return s
+}
+
+type DescribeSellerInstancesResponseBody struct {
+	// example:
+	//
+	// 200
+	Code *string `json:"Code,omitempty" xml:"Code,omitempty"`
+	// example:
+	//
+	// 10
+	Count *int64 `json:"Count,omitempty" xml:"Count,omitempty"`
+	// fatal
+	//
+	// example:
+	//
+	// False
+	Fatal *bool `json:"Fatal,omitempty" xml:"Fatal,omitempty"`
+	// example:
+	//
+	// Instance 5723f7ee-952d-411f-94f4-b942a550d9b8 does not exist.
+	Message *string `json:"Message,omitempty" xml:"Message,omitempty"`
+	// example:
+	//
+	// 1
+	PageNumber *int64 `json:"PageNumber,omitempty" xml:"PageNumber,omitempty"`
+	// example:
+	//
+	// 10
+	PageSize *int64 `json:"PageSize,omitempty" xml:"PageSize,omitempty"`
+	// example:
+	//
+	// A6A33748-D573-593C-A3BC-593E33D68311
+	RequestId *string                                      `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	Result    []*DescribeSellerInstancesResponseBodyResult `json:"Result,omitempty" xml:"Result,omitempty" type:"Repeated"`
+	// example:
+	//
+	// True
+	Success *bool `json:"Success,omitempty" xml:"Success,omitempty"`
+	// example:
+	//
+	// 103
+	Version *string `json:"Version,omitempty" xml:"Version,omitempty"`
+}
+
+func (s DescribeSellerInstancesResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeSellerInstancesResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeSellerInstancesResponseBody) SetCode(v string) *DescribeSellerInstancesResponseBody {
+	s.Code = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBody) SetCount(v int64) *DescribeSellerInstancesResponseBody {
+	s.Count = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBody) SetFatal(v bool) *DescribeSellerInstancesResponseBody {
+	s.Fatal = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBody) SetMessage(v string) *DescribeSellerInstancesResponseBody {
+	s.Message = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBody) SetPageNumber(v int64) *DescribeSellerInstancesResponseBody {
+	s.PageNumber = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBody) SetPageSize(v int64) *DescribeSellerInstancesResponseBody {
+	s.PageSize = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBody) SetRequestId(v string) *DescribeSellerInstancesResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBody) SetResult(v []*DescribeSellerInstancesResponseBodyResult) *DescribeSellerInstancesResponseBody {
+	s.Result = v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBody) SetSuccess(v bool) *DescribeSellerInstancesResponseBody {
+	s.Success = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBody) SetVersion(v string) *DescribeSellerInstancesResponseBody {
+	s.Version = &v
+	return s
+}
+
+type DescribeSellerInstancesResponseBodyResult struct {
+	// example:
+	//
+	// {\\"authUrl\\":\\"https://marketplace.alibabacloud.com/\\"}
+	AppInfo *string `json:"AppInfo,omitempty" xml:"AppInfo,omitempty"`
+	// example:
+	//
+	// 1
+	ChargeType *int32 `json:"ChargeType,omitempty" xml:"ChargeType,omitempty"`
+	// example:
+	//
+	// sgcmgj000356
+	CommodityCode *string `json:"CommodityCode,omitempty" xml:"CommodityCode,omitempty"`
+	// example:
+	//
+	// 1741752000000
+	CreatedOn *int64 `json:"CreatedOn,omitempty" xml:"CreatedOn,omitempty"`
+	// example:
+	//
+	// {\\"userName\\":\\"marketplace\\"}
+	HostInfo *string `json:"HostInfo,omitempty" xml:"HostInfo,omitempty"`
+	// example:
+	//
+	// {\\"userName\\":\\"marketplace\\"}
+	Info *string `json:"Info,omitempty" xml:"Info,omitempty"`
+	// example:
+	//
+	// 5000002763123
+	InstanceId *int64 `json:"InstanceId,omitempty" xml:"InstanceId,omitempty"`
+	// example:
+	//
+	// OPENED
+	InstanceStatus *string `json:"InstanceStatus,omitempty" xml:"InstanceStatus,omitempty"`
+	// example:
+	//
+	// 5322460655123456
+	UserId *int64 `json:"UserId,omitempty" xml:"UserId,omitempty"`
+}
+
+func (s DescribeSellerInstancesResponseBodyResult) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeSellerInstancesResponseBodyResult) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeSellerInstancesResponseBodyResult) SetAppInfo(v string) *DescribeSellerInstancesResponseBodyResult {
+	s.AppInfo = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBodyResult) SetChargeType(v int32) *DescribeSellerInstancesResponseBodyResult {
+	s.ChargeType = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBodyResult) SetCommodityCode(v string) *DescribeSellerInstancesResponseBodyResult {
+	s.CommodityCode = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBodyResult) SetCreatedOn(v int64) *DescribeSellerInstancesResponseBodyResult {
+	s.CreatedOn = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBodyResult) SetHostInfo(v string) *DescribeSellerInstancesResponseBodyResult {
+	s.HostInfo = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBodyResult) SetInfo(v string) *DescribeSellerInstancesResponseBodyResult {
+	s.Info = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBodyResult) SetInstanceId(v int64) *DescribeSellerInstancesResponseBodyResult {
+	s.InstanceId = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBodyResult) SetInstanceStatus(v string) *DescribeSellerInstancesResponseBodyResult {
+	s.InstanceStatus = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponseBodyResult) SetUserId(v int64) *DescribeSellerInstancesResponseBodyResult {
+	s.UserId = &v
+	return s
+}
+
+type DescribeSellerInstancesResponse struct {
+	Headers    map[string]*string                   `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                               `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *DescribeSellerInstancesResponseBody `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s DescribeSellerInstancesResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeSellerInstancesResponse) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeSellerInstancesResponse) SetHeaders(v map[string]*string) *DescribeSellerInstancesResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponse) SetStatusCode(v int32) *DescribeSellerInstancesResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *DescribeSellerInstancesResponse) SetBody(v *DescribeSellerInstancesResponseBody) *DescribeSellerInstancesResponse {
+	s.Body = v
+	return s
+}
+
 type NoticeInstanceUserRequest struct {
 	// example:
 	//
@@ -413,6 +690,93 @@ func (client *Client) GetEndpoint(productId *string, regionId *string, endpointR
 	}
 
 	_body, _err := endpointutil.GetEndpointRules(productId, regionId, endpointRule, network, suffix)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+// Summary:
+//
+// 卖家查询实例列表
+//
+// @param request - DescribeSellerInstancesRequest
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return DescribeSellerInstancesResponse
+func (client *Client) DescribeSellerInstancesWithOptions(request *DescribeSellerInstancesRequest, runtime *util.RuntimeOptions) (_result *DescribeSellerInstancesResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	query := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.InstanceId)) {
+		query["InstanceId"] = request.InstanceId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.InstanceStatus)) {
+		query["InstanceStatus"] = request.InstanceStatus
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.PageIndex)) {
+		query["PageIndex"] = request.PageIndex
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.PageSize)) {
+		query["PageSize"] = request.PageSize
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.UserId)) {
+		query["UserId"] = request.UserId
+	}
+
+	req := &openapi.OpenApiRequest{
+		Query: openapiutil.Query(query),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("DescribeSellerInstances"),
+		Version:     tea.String("2022-12-30"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	if tea.BoolValue(util.IsUnset(client.SignatureVersion)) || !tea.BoolValue(util.EqualString(client.SignatureVersion, tea.String("v4"))) {
+		_result = &DescribeSellerInstancesResponse{}
+		_body, _err := client.CallApi(params, req, runtime)
+		if _err != nil {
+			return _result, _err
+		}
+		_err = tea.Convert(_body, &_result)
+		return _result, _err
+	} else {
+		_result = &DescribeSellerInstancesResponse{}
+		_body, _err := client.Execute(params, req, runtime)
+		if _err != nil {
+			return _result, _err
+		}
+		_err = tea.Convert(_body, &_result)
+		return _result, _err
+	}
+
+}
+
+// Summary:
+//
+// 卖家查询实例列表
+//
+// @param request - DescribeSellerInstancesRequest
+//
+// @return DescribeSellerInstancesResponse
+func (client *Client) DescribeSellerInstances(request *DescribeSellerInstancesRequest) (_result *DescribeSellerInstancesResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &DescribeSellerInstancesResponse{}
+	_body, _err := client.DescribeSellerInstancesWithOptions(request, runtime)
 	if _err != nil {
 		return _result, _err
 	}
