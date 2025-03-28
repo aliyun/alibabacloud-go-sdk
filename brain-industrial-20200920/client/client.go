501bb7a52cfac9e055c7b5e215213105237633a6
sdk-team <sdk-team@alibabacloud.com>
2025-03-28 09:33:37 +0000

    Support API AicsOpenApiInvoke.

--- brain-industrial-20200920/client/client.go
+++ brain-industrial-20200920/client/client.go
@@ -381,6 +381,191 @@ func (s *ActivateLicenseResponse) SetBody(v *ActivateLicenseResponseBody) *Activ
 	return s
 }
 
+type AicsOpenApiInvokeRequest struct {
+	// example:
+	//
+	// 119397
+	NodeId *string `json:"NodeId,omitempty" xml:"NodeId,omitempty"`
+	// example:
+	//
+	// {"a":1}
+	Param map[string]interface{} `json:"Param,omitempty" xml:"Param,omitempty"`
+	// This parameter is required.
+	//
+	// example:
+	//
+	// ae5f9884c9914ed7af72b07e6c1616f9
+	ServiceId *string `json:"ServiceId,omitempty" xml:"ServiceId,omitempty"`
+	// example:
+	//
+	// EXPERIMENT
+	Type *string `json:"Type,omitempty" xml:"Type,omitempty"`
+}
+
+func (s AicsOpenApiInvokeRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s AicsOpenApiInvokeRequest) GoString() string {
+	return s.String()
+}
+
+func (s *AicsOpenApiInvokeRequest) SetNodeId(v string) *AicsOpenApiInvokeRequest {
+	s.NodeId = &v
+	return s
+}
+
+func (s *AicsOpenApiInvokeRequest) SetParam(v map[string]interface{}) *AicsOpenApiInvokeRequest {
+	s.Param = v
+	return s
+}
+
+func (s *AicsOpenApiInvokeRequest) SetServiceId(v string) *AicsOpenApiInvokeRequest {
+	s.ServiceId = &v
+	return s
+}
+
+func (s *AicsOpenApiInvokeRequest) SetType(v string) *AicsOpenApiInvokeRequest {
+	s.Type = &v
+	return s
+}
+
+type AicsOpenApiInvokeShrinkRequest struct {
+	// example:
+	//
+	// 119397
+	NodeId *string `json:"NodeId,omitempty" xml:"NodeId,omitempty"`
+	// example:
+	//
+	// {"a":1}
+	ParamShrink *string `json:"Param,omitempty" xml:"Param,omitempty"`
+	// This parameter is required.
+	//
+	// example:
+	//
+	// ae5f9884c9914ed7af72b07e6c1616f9
+	ServiceId *string `json:"ServiceId,omitempty" xml:"ServiceId,omitempty"`
+	// example:
+	//
+	// EXPERIMENT
+	Type *string `json:"Type,omitempty" xml:"Type,omitempty"`
+}
+
+func (s AicsOpenApiInvokeShrinkRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s AicsOpenApiInvokeShrinkRequest) GoString() string {
+	return s.String()
+}
+
+func (s *AicsOpenApiInvokeShrinkRequest) SetNodeId(v string) *AicsOpenApiInvokeShrinkRequest {
+	s.NodeId = &v
+	return s
+}
+
+func (s *AicsOpenApiInvokeShrinkRequest) SetParamShrink(v string) *AicsOpenApiInvokeShrinkRequest {
+	s.ParamShrink = &v
+	return s
+}
+
+func (s *AicsOpenApiInvokeShrinkRequest) SetServiceId(v string) *AicsOpenApiInvokeShrinkRequest {
+	s.ServiceId = &v
+	return s
+}
+
+func (s *AicsOpenApiInvokeShrinkRequest) SetType(v string) *AicsOpenApiInvokeShrinkRequest {
+	s.Type = &v
+	return s
+}
+
+type AicsOpenApiInvokeResponseBody struct {
+	// example:
+	//
+	// 200
+	Code *string `json:"Code,omitempty" xml:"Code,omitempty"`
+	// example:
+	//
+	// {"c":2}
+	Data interface{} `json:"Data,omitempty" xml:"Data,omitempty"`
+	// example:
+	//
+	// successful
+	Message *string `json:"Message,omitempty" xml:"Message,omitempty"`
+	// Id of the request
+	//
+	// example:
+	//
+	// 68738E75-43C1-5AE5-9F3A-AFEF576D7B5F
+	RequestId *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	// example:
+	//
+	// True
+	Success *string `json:"Success,omitempty" xml:"Success,omitempty"`
+}
+
+func (s AicsOpenApiInvokeResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s AicsOpenApiInvokeResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *AicsOpenApiInvokeResponseBody) SetCode(v string) *AicsOpenApiInvokeResponseBody {
+	s.Code = &v
+	return s
+}
+
+func (s *AicsOpenApiInvokeResponseBody) SetData(v interface{}) *AicsOpenApiInvokeResponseBody {
+	s.Data = v
+	return s
+}
+
+func (s *AicsOpenApiInvokeResponseBody) SetMessage(v string) *AicsOpenApiInvokeResponseBody {
+	s.Message = &v
+	return s
+}
+
+func (s *AicsOpenApiInvokeResponseBody) SetRequestId(v string) *AicsOpenApiInvokeResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *AicsOpenApiInvokeResponseBody) SetSuccess(v string) *AicsOpenApiInvokeResponseBody {
+	s.Success = &v
+	return s
+}
+
+type AicsOpenApiInvokeResponse struct {
+	Headers    map[string]*string             `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                         `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *AicsOpenApiInvokeResponseBody `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s AicsOpenApiInvokeResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s AicsOpenApiInvokeResponse) GoString() string {
+	return s.String()
+}
+
+func (s *AicsOpenApiInvokeResponse) SetHeaders(v map[string]*string) *AicsOpenApiInvokeResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *AicsOpenApiInvokeResponse) SetStatusCode(v int32) *AicsOpenApiInvokeResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *AicsOpenApiInvokeResponse) SetBody(v *AicsOpenApiInvokeResponseBody) *AicsOpenApiInvokeResponse {
+	s.Body = v
+	return s
+}
+
 type CreateEssOptJobRequest struct {
 	// example:
 	//
@@ -3442,6 +3627,97 @@ func (client *Client) ActivateLicense(request *ActivateLicenseRequest) (_result 
 	runtime := &util.RuntimeOptions{}
 	_result = &ActivateLicenseResponse{}
 	_body, _err := client.ActivateLicenseWithOptions(request, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+// Summary:
+//
+// 调用aics openapi
+//
+// @param tmpReq - AicsOpenApiInvokeRequest
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return AicsOpenApiInvokeResponse
+func (client *Client) AicsOpenApiInvokeWithOptions(tmpReq *AicsOpenApiInvokeRequest, runtime *util.RuntimeOptions) (_result *AicsOpenApiInvokeResponse, _err error) {
+	_err = util.ValidateModel(tmpReq)
+	if _err != nil {
+		return _result, _err
+	}
+	request := &AicsOpenApiInvokeShrinkRequest{}
+	openapiutil.Convert(tmpReq, request)
+	if !tea.BoolValue(util.IsUnset(tmpReq.Param)) {
+		request.ParamShrink = openapiutil.ArrayToStringWithSpecifiedStyle(tmpReq.Param, tea.String("Param"), tea.String("json"))
+	}
+
+	query := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.NodeId)) {
+		query["NodeId"] = request.NodeId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.ServiceId)) {
+		query["ServiceId"] = request.ServiceId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.Type)) {
+		query["Type"] = request.Type
+	}
+
+	body := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.ParamShrink)) {
+		body["Param"] = request.ParamShrink
+	}
+
+	req := &openapi.OpenApiRequest{
+		Query: openapiutil.Query(query),
+		Body:  openapiutil.ParseToMap(body),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("AicsOpenApiInvoke"),
+		Version:     tea.String("2020-09-20"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	if tea.BoolValue(util.IsUnset(client.SignatureVersion)) || !tea.BoolValue(util.EqualString(client.SignatureVersion, tea.String("v4"))) {
+		_result = &AicsOpenApiInvokeResponse{}
+		_body, _err := client.CallApi(params, req, runtime)
+		if _err != nil {
+			return _result, _err
+		}
+		_err = tea.Convert(_body, &_result)
+		return _result, _err
+	} else {
+		_result = &AicsOpenApiInvokeResponse{}
+		_body, _err := client.Execute(params, req, runtime)
+		if _err != nil {
+			return _result, _err
+		}
+		_err = tea.Convert(_body, &_result)
+		return _result, _err
+	}
+
+}
+
+// Summary:
+//
+// 调用aics openapi
+//
+// @param request - AicsOpenApiInvokeRequest
+//
+// @return AicsOpenApiInvokeResponse
+func (client *Client) AicsOpenApiInvoke(request *AicsOpenApiInvokeRequest) (_result *AicsOpenApiInvokeResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &AicsOpenApiInvokeResponse{}
+	_body, _err := client.AicsOpenApiInvokeWithOptions(request, runtime)
 	if _err != nil {
 		return _result, _err
 	}
