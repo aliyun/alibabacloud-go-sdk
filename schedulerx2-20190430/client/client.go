ef510af609d5fa4785cf20f8c771d492364f35ac
sdk-team <sdk-team@alibabacloud.com>
2025-06-10 10:04:27 +0000

    Support API ReadSchedulerxDesignateInfo.

--- schedulerx2-20190430/client/client.go
+++ schedulerx2-20190430/client/client.go
@@ -10150,6 +10150,477 @@ func (s *ReadSchedulerxDesignateDetailResponse) SetBody(v *ReadSchedulerxDesigna
 	return s
 }
 
+type ReadSchedulerxDesignateInfoRequest struct {
+	// This parameter is required.
+	//
+	// example:
+	//
+	// test.defalutGroup
+	GroupId *string `json:"GroupId,omitempty" xml:"GroupId,omitempty"`
+	// This parameter is required.
+	//
+	// example:
+	//
+	// 368
+	JobId *int64 `json:"JobId,omitempty" xml:"JobId,omitempty"`
+	// This parameter is required.
+	//
+	// example:
+	//
+	// adcfc35d-e2fe-4fe9-bbaa-20e90ffc****
+	Namespace *string `json:"Namespace,omitempty" xml:"Namespace,omitempty"`
+	// example:
+	//
+	// schedulerx
+	NamespaceSource *string `json:"NamespaceSource,omitempty" xml:"NamespaceSource,omitempty"`
+	// This parameter is required.
+	//
+	// example:
+	//
+	// cn-hangzhou
+	RegionId *string `json:"RegionId,omitempty" xml:"RegionId,omitempty"`
+}
+
+func (s ReadSchedulerxDesignateInfoRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ReadSchedulerxDesignateInfoRequest) GoString() string {
+	return s.String()
+}
+
+func (s *ReadSchedulerxDesignateInfoRequest) SetGroupId(v string) *ReadSchedulerxDesignateInfoRequest {
+	s.GroupId = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoRequest) SetJobId(v int64) *ReadSchedulerxDesignateInfoRequest {
+	s.JobId = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoRequest) SetNamespace(v string) *ReadSchedulerxDesignateInfoRequest {
+	s.Namespace = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoRequest) SetNamespaceSource(v string) *ReadSchedulerxDesignateInfoRequest {
+	s.NamespaceSource = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoRequest) SetRegionId(v string) *ReadSchedulerxDesignateInfoRequest {
+	s.RegionId = &v
+	return s
+}
+
+type ReadSchedulerxDesignateInfoResponseBody struct {
+	AccessDeniedDetail *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail `json:"AccessDeniedDetail,omitempty" xml:"AccessDeniedDetail,omitempty" type:"Struct"`
+	// example:
+	//
+	// 200
+	Code *int32 `json:"Code,omitempty" xml:"Code,omitempty"`
+	// -
+	Data *ReadSchedulerxDesignateInfoResponseBodyData `json:"Data,omitempty" xml:"Data,omitempty" type:"Struct"`
+	// example:
+	//
+	// jobId=xxx is not existed
+	Message *string `json:"Message,omitempty" xml:"Message,omitempty"`
+	// example:
+	//
+	// 4F68ABED-AC31-4412-9297-D9A8F0401108
+	RequestId *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	// example:
+	//
+	// true
+	Success *bool `json:"Success,omitempty" xml:"Success,omitempty"`
+}
+
+func (s ReadSchedulerxDesignateInfoResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ReadSchedulerxDesignateInfoResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBody) SetAccessDeniedDetail(v *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail) *ReadSchedulerxDesignateInfoResponseBody {
+	s.AccessDeniedDetail = v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBody) SetCode(v int32) *ReadSchedulerxDesignateInfoResponseBody {
+	s.Code = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBody) SetData(v *ReadSchedulerxDesignateInfoResponseBodyData) *ReadSchedulerxDesignateInfoResponseBody {
+	s.Data = v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBody) SetMessage(v string) *ReadSchedulerxDesignateInfoResponseBody {
+	s.Message = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBody) SetRequestId(v string) *ReadSchedulerxDesignateInfoResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBody) SetSuccess(v bool) *ReadSchedulerxDesignateInfoResponseBody {
+	s.Success = &v
+	return s
+}
+
+type ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail struct {
+	// example:
+	//
+	// edas:ReadSchedulerxDesignateInfo
+	AuthAction *string `json:"AuthAction,omitempty" xml:"AuthAction,omitempty"`
+	// example:
+	//
+	// 209312833131416xxx
+	AuthPrincipalDisplayName *string `json:"AuthPrincipalDisplayName,omitempty" xml:"AuthPrincipalDisplayName,omitempty"`
+	// example:
+	//
+	// 1827811800526xxx
+	AuthPrincipalOwnerId *string `json:"AuthPrincipalOwnerId,omitempty" xml:"AuthPrincipalOwnerId,omitempty"`
+	// example:
+	//
+	// SubUser
+	AuthPrincipalType *string `json:"AuthPrincipalType,omitempty" xml:"AuthPrincipalType,omitempty"`
+	// example:
+	//
+	// AQFn/cLPZ/3Cz0YxQkZBMjVGLTY0REUtNTlGNS05NzUwLTgyMUE4M0MwMTFDRQ==
+	EncodedDiagnosticMessage *string `json:"EncodedDiagnosticMessage,omitempty" xml:"EncodedDiagnosticMessage,omitempty"`
+	// example:
+	//
+	// ImplicitDeny
+	NoPermissionType *string `json:"NoPermissionType,omitempty" xml:"NoPermissionType,omitempty"`
+	// example:
+	//
+	// AccountLevelIdentityBasedPolicy
+	PolicyType *string `json:"PolicyType,omitempty" xml:"PolicyType,omitempty"`
+}
+
+func (s ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail) GoString() string {
+	return s.String()
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail) SetAuthAction(v string) *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail {
+	s.AuthAction = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail) SetAuthPrincipalDisplayName(v string) *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail {
+	s.AuthPrincipalDisplayName = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail) SetAuthPrincipalOwnerId(v string) *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail {
+	s.AuthPrincipalOwnerId = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail) SetAuthPrincipalType(v string) *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail {
+	s.AuthPrincipalType = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail) SetEncodedDiagnosticMessage(v string) *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail {
+	s.EncodedDiagnosticMessage = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail) SetNoPermissionType(v string) *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail {
+	s.NoPermissionType = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail) SetPolicyType(v string) *ReadSchedulerxDesignateInfoResponseBodyAccessDeniedDetail {
+	s.PolicyType = &v
+	return s
+}
+
+type ReadSchedulerxDesignateInfoResponseBodyData struct {
+	// -
+	DesignateDetailVos []*ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos `json:"DesignateDetailVos,omitempty" xml:"DesignateDetailVos,omitempty" type:"Repeated"`
+	// example:
+	//
+	// 1
+	DesignateType *int32 `json:"DesignateType,omitempty" xml:"DesignateType,omitempty"`
+	// example:
+	//
+	// true
+	Transferable *bool `json:"Transferable,omitempty" xml:"Transferable,omitempty"`
+}
+
+func (s ReadSchedulerxDesignateInfoResponseBodyData) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ReadSchedulerxDesignateInfoResponseBodyData) GoString() string {
+	return s.String()
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyData) SetDesignateDetailVos(v []*ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos) *ReadSchedulerxDesignateInfoResponseBodyData {
+	s.DesignateDetailVos = v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyData) SetDesignateType(v int32) *ReadSchedulerxDesignateInfoResponseBodyData {
+	s.DesignateType = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyData) SetTransferable(v bool) *ReadSchedulerxDesignateInfoResponseBodyData {
+	s.Transferable = &v
+	return s
+}
+
+type ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos struct {
+	// example:
+	//
+	// FREE
+	Busy *string `json:"Busy,omitempty" xml:"Busy,omitempty"`
+	// example:
+	//
+	// true
+	Checked *bool `json:"Checked,omitempty" xml:"Checked,omitempty"`
+	// example:
+	//
+	// 10.52.169.25
+	Key     *string                                                               `json:"Key,omitempty" xml:"Key,omitempty"`
+	Metrics *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics `json:"Metrics,omitempty" xml:"Metrics,omitempty" type:"Struct"`
+	// example:
+	//
+	// fasle
+	Offline *bool `json:"Offline,omitempty" xml:"Offline,omitempty"`
+	// example:
+	//
+	// 1
+	Size *int32 `json:"Size,omitempty" xml:"Size,omitempty"`
+	// example:
+	//
+	// pod
+	Starter *string `json:"Starter,omitempty" xml:"Starter,omitempty"`
+	// example:
+	//
+	// 1.12.5
+	Version *string `json:"Version,omitempty" xml:"Version,omitempty"`
+}
+
+func (s ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos) GoString() string {
+	return s.String()
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos) SetBusy(v string) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos {
+	s.Busy = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos) SetChecked(v bool) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos {
+	s.Checked = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos) SetKey(v string) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos {
+	s.Key = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos) SetMetrics(v *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos {
+	s.Metrics = v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos) SetOffline(v bool) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos {
+	s.Offline = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos) SetSize(v int32) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos {
+	s.Size = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos) SetStarter(v string) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos {
+	s.Starter = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos) SetVersion(v string) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVos {
+	s.Version = &v
+	return s
+}
+
+type ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics struct {
+	// example:
+	//
+	// 0
+	CpuLoad1 *float64 `json:"CpuLoad1,omitempty" xml:"CpuLoad1,omitempty"`
+	// example:
+	//
+	// 0
+	CpuLoad5 *float64 `json:"CpuLoad5,omitempty" xml:"CpuLoad5,omitempty"`
+	// example:
+	//
+	// 1
+	CpuProcessors *int32 `json:"CpuProcessors,omitempty" xml:"CpuProcessors,omitempty"`
+	// example:
+	//
+	// 1024
+	DiskMax *int32 `json:"DiskMax,omitempty" xml:"DiskMax,omitempty"`
+	// example:
+	//
+	// 0.19142496008515167
+	DiskUsage *float64 `json:"DiskUsage,omitempty" xml:"DiskUsage,omitempty"`
+	// example:
+	//
+	// 148
+	DiskUsed *int32 `json:"DiskUsed,omitempty" xml:"DiskUsed,omitempty"`
+	// example:
+	//
+	// 56
+	ExecCount *int64 `json:"ExecCount,omitempty" xml:"ExecCount,omitempty"`
+	// example:
+	//
+	// 0.06072874493927125
+	Heap1Usage *float64 `json:"Heap1Usage,omitempty" xml:"Heap1Usage,omitempty"`
+	// example:
+	//
+	// 14
+	Heap1Used *float64 `json:"Heap1Used,omitempty" xml:"Heap1Used,omitempty"`
+	// example:
+	//
+	// 0.06477732793522267
+	Heap5Usage *float64 `json:"Heap5Usage,omitempty" xml:"Heap5Usage,omitempty"`
+	// example:
+	//
+	// 500
+	HeapMax *int32 `json:"HeapMax,omitempty" xml:"HeapMax,omitempty"`
+	// example:
+	//
+	// 72
+	SharePoolAvailableSize *int32 `json:"SharePoolAvailableSize,omitempty" xml:"SharePoolAvailableSize,omitempty"`
+	// example:
+	//
+	// 1
+	SharePoolQueueSize *int32 `json:"SharePoolQueueSize,omitempty" xml:"SharePoolQueueSize,omitempty"`
+}
+
+func (s ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) GoString() string {
+	return s.String()
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) SetCpuLoad1(v float64) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics {
+	s.CpuLoad1 = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) SetCpuLoad5(v float64) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics {
+	s.CpuLoad5 = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) SetCpuProcessors(v int32) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics {
+	s.CpuProcessors = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) SetDiskMax(v int32) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics {
+	s.DiskMax = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) SetDiskUsage(v float64) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics {
+	s.DiskUsage = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) SetDiskUsed(v int32) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics {
+	s.DiskUsed = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) SetExecCount(v int64) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics {
+	s.ExecCount = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) SetHeap1Usage(v float64) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics {
+	s.Heap1Usage = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) SetHeap1Used(v float64) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics {
+	s.Heap1Used = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) SetHeap5Usage(v float64) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics {
+	s.Heap5Usage = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) SetHeapMax(v int32) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics {
+	s.HeapMax = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) SetSharePoolAvailableSize(v int32) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics {
+	s.SharePoolAvailableSize = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics) SetSharePoolQueueSize(v int32) *ReadSchedulerxDesignateInfoResponseBodyDataDesignateDetailVosMetrics {
+	s.SharePoolQueueSize = &v
+	return s
+}
+
+type ReadSchedulerxDesignateInfoResponse struct {
+	Headers    map[string]*string                       `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                                   `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *ReadSchedulerxDesignateInfoResponseBody `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s ReadSchedulerxDesignateInfoResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ReadSchedulerxDesignateInfoResponse) GoString() string {
+	return s.String()
+}
+
+func (s *ReadSchedulerxDesignateInfoResponse) SetHeaders(v map[string]*string) *ReadSchedulerxDesignateInfoResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponse) SetStatusCode(v int32) *ReadSchedulerxDesignateInfoResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *ReadSchedulerxDesignateInfoResponse) SetBody(v *ReadSchedulerxDesignateInfoResponseBody) *ReadSchedulerxDesignateInfoResponse {
+	s.Body = v
+	return s
+}
+
 type RerunJobRequest struct {
 	// The data timestamp of the job. Specify a string in the HH:mm:ss format.
 	//
@@ -15617,6 +16088,82 @@ func (client *Client) ReadSchedulerxDesignateDetail(request *ReadSchedulerxDesig
 	runtime := &util.RuntimeOptions{}
 	_result = &ReadSchedulerxDesignateDetailResponse{}
 	_body, _err := client.ReadSchedulerxDesignateDetailWithOptions(request, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+// Summary:
+//
+// 获取指定机器基本信息
+//
+// @param request - ReadSchedulerxDesignateInfoRequest
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return ReadSchedulerxDesignateInfoResponse
+func (client *Client) ReadSchedulerxDesignateInfoWithOptions(request *ReadSchedulerxDesignateInfoRequest, runtime *util.RuntimeOptions) (_result *ReadSchedulerxDesignateInfoResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	query := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.GroupId)) {
+		query["GroupId"] = request.GroupId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.JobId)) {
+		query["JobId"] = request.JobId
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.Namespace)) {
+		query["Namespace"] = request.Namespace
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.NamespaceSource)) {
+		query["NamespaceSource"] = request.NamespaceSource
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.RegionId)) {
+		query["RegionId"] = request.RegionId
+	}
+
+	req := &openapi.OpenApiRequest{
+		Query: openapiutil.Query(query),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("ReadSchedulerxDesignateInfo"),
+		Version:     tea.String("2019-04-30"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &ReadSchedulerxDesignateInfoResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+// Summary:
+//
+// 获取指定机器基本信息
+//
+// @param request - ReadSchedulerxDesignateInfoRequest
+//
+// @return ReadSchedulerxDesignateInfoResponse
+func (client *Client) ReadSchedulerxDesignateInfo(request *ReadSchedulerxDesignateInfoRequest) (_result *ReadSchedulerxDesignateInfoResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &ReadSchedulerxDesignateInfoResponse{}
+	_body, _err := client.ReadSchedulerxDesignateInfoWithOptions(request, runtime)
 	if _err != nil {
 		return _result, _err
 	}
