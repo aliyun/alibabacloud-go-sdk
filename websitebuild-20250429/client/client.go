1ffb47b90a56dfe14408fb4d2e08a31a2784e62b
sdk-team <sdk-team@alibabacloud.com>
2025-06-12 08:42:10 +0000

    Support API OperateAppInstanceForPartner.

--- websitebuild-20250429/client/client.go
+++ websitebuild-20250429/client/client.go
@@ -9,6 +9,110 @@ import (
 	"github.com/alibabacloud-go/tea/tea"
 )
 
+type OperateAppInstanceForPartnerRequest struct {
+	Extend *string `json:"Extend,omitempty" xml:"Extend,omitempty"`
+	// example:
+	//
+	// SERVICE_DELIVERY_PROCESS
+	OperateEvent *string `json:"OperateEvent,omitempty" xml:"OperateEvent,omitempty"`
+}
+
+func (s OperateAppInstanceForPartnerRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s OperateAppInstanceForPartnerRequest) GoString() string {
+	return s.String()
+}
+
+func (s *OperateAppInstanceForPartnerRequest) SetExtend(v string) *OperateAppInstanceForPartnerRequest {
+	s.Extend = &v
+	return s
+}
+
+func (s *OperateAppInstanceForPartnerRequest) SetOperateEvent(v string) *OperateAppInstanceForPartnerRequest {
+	s.OperateEvent = &v
+	return s
+}
+
+type OperateAppInstanceForPartnerResponseBody struct {
+	// example:
+	//
+	// 0
+	ErrorCode *string `json:"ErrorCode,omitempty" xml:"ErrorCode,omitempty"`
+	// example:
+	//
+	// aliuid:1998006665794443 assumeRole not exist,serviceName:realtimelogpush.dcdnservices.aliyuncs.com
+	ErrorMsg *string `json:"ErrorMsg,omitempty" xml:"ErrorMsg,omitempty"`
+	// Id of the request
+	//
+	// example:
+	//
+	// 6C6B99AC-39EC-5350-874C-204128C905E6
+	RequestId *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	// example:
+	//
+	// true
+	Success *string `json:"Success,omitempty" xml:"Success,omitempty"`
+}
+
+func (s OperateAppInstanceForPartnerResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s OperateAppInstanceForPartnerResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *OperateAppInstanceForPartnerResponseBody) SetErrorCode(v string) *OperateAppInstanceForPartnerResponseBody {
+	s.ErrorCode = &v
+	return s
+}
+
+func (s *OperateAppInstanceForPartnerResponseBody) SetErrorMsg(v string) *OperateAppInstanceForPartnerResponseBody {
+	s.ErrorMsg = &v
+	return s
+}
+
+func (s *OperateAppInstanceForPartnerResponseBody) SetRequestId(v string) *OperateAppInstanceForPartnerResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *OperateAppInstanceForPartnerResponseBody) SetSuccess(v string) *OperateAppInstanceForPartnerResponseBody {
+	s.Success = &v
+	return s
+}
+
+type OperateAppInstanceForPartnerResponse struct {
+	Headers    map[string]*string                        `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                                    `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *OperateAppInstanceForPartnerResponseBody `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s OperateAppInstanceForPartnerResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s OperateAppInstanceForPartnerResponse) GoString() string {
+	return s.String()
+}
+
+func (s *OperateAppInstanceForPartnerResponse) SetHeaders(v map[string]*string) *OperateAppInstanceForPartnerResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *OperateAppInstanceForPartnerResponse) SetStatusCode(v int32) *OperateAppInstanceForPartnerResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *OperateAppInstanceForPartnerResponse) SetBody(v *OperateAppInstanceForPartnerResponseBody) *OperateAppInstanceForPartnerResponse {
+	s.Body = v
+	return s
+}
+
 type OperateAppServiceForPartnerRequest struct {
 	// example:
 	//
@@ -174,6 +278,70 @@ func (client *Client) GetEndpoint(productId *string, regionId *string, endpointR
 	}
 
 	_body, _err := endpointutil.GetEndpointRules(productId, regionId, endpointRule, network, suffix)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+// Summary:
+//
+// 合作伙伴操作应用
+//
+// @param request - OperateAppInstanceForPartnerRequest
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return OperateAppInstanceForPartnerResponse
+func (client *Client) OperateAppInstanceForPartnerWithOptions(request *OperateAppInstanceForPartnerRequest, runtime *util.RuntimeOptions) (_result *OperateAppInstanceForPartnerResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	query := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.Extend)) {
+		query["Extend"] = request.Extend
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.OperateEvent)) {
+		query["OperateEvent"] = request.OperateEvent
+	}
+
+	req := &openapi.OpenApiRequest{
+		Query: openapiutil.Query(query),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("OperateAppInstanceForPartner"),
+		Version:     tea.String("2025-04-29"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &OperateAppInstanceForPartnerResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+// Summary:
+//
+// 合作伙伴操作应用
+//
+// @param request - OperateAppInstanceForPartnerRequest
+//
+// @return OperateAppInstanceForPartnerResponse
+func (client *Client) OperateAppInstanceForPartner(request *OperateAppInstanceForPartnerRequest) (_result *OperateAppInstanceForPartnerResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &OperateAppInstanceForPartnerResponse{}
+	_body, _err := client.OperateAppInstanceForPartnerWithOptions(request, runtime)
 	if _err != nil {
 		return _result, _err
 	}
