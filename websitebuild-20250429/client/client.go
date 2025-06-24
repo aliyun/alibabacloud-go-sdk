b3a103e612b713ad6ed7788f7695cc62e82b6006
sdk-team <sdk-team@alibabacloud.com>
2025-06-24 16:27:25 +0000

    Support API CreateLogoTask.

--- websitebuild-20250429/client/client.go
+++ websitebuild-20250429/client/client.go
@@ -9,6 +9,273 @@ import (
 	"github.com/alibabacloud-go/tea/tea"
 )
 
+type CreateLogoTaskRequest struct {
+	NegativePrompt *string `json:"NegativePrompt,omitempty" xml:"NegativePrompt,omitempty"`
+	// example:
+	//
+	// {\\"ehpcutilParam\\":\\"sched job_submit --commandline \\\\\\"/ehpcdata/data/usersTest/huangqiaoyi-1725933699384/huangqiaoyi-1725933699384.slurm\\\\\\" --runasuser TestGfjnSimworks\\"}
+	Parameters *string `json:"Parameters,omitempty" xml:"Parameters,omitempty"`
+	Prompt     *string `json:"Prompt,omitempty" xml:"Prompt,omitempty"`
+	// example:
+	//
+	// 1.0.2
+	Version *string `json:"Version,omitempty" xml:"Version,omitempty"`
+}
+
+func (s CreateLogoTaskRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s CreateLogoTaskRequest) GoString() string {
+	return s.String()
+}
+
+func (s *CreateLogoTaskRequest) SetNegativePrompt(v string) *CreateLogoTaskRequest {
+	s.NegativePrompt = &v
+	return s
+}
+
+func (s *CreateLogoTaskRequest) SetParameters(v string) *CreateLogoTaskRequest {
+	s.Parameters = &v
+	return s
+}
+
+func (s *CreateLogoTaskRequest) SetPrompt(v string) *CreateLogoTaskRequest {
+	s.Prompt = &v
+	return s
+}
+
+func (s *CreateLogoTaskRequest) SetVersion(v string) *CreateLogoTaskRequest {
+	s.Version = &v
+	return s
+}
+
+type CreateLogoTaskResponseBody struct {
+	// example:
+	//
+	// 0
+	ErrorCode *string `json:"ErrorCode,omitempty" xml:"ErrorCode,omitempty"`
+	// example:
+	//
+	// aliuid:1998006665794443 assumeRole not exist,serviceName:realtimelogpush.dcdnservices.aliyuncs.com
+	ErrorMsg *string `json:"ErrorMsg,omitempty" xml:"ErrorMsg,omitempty"`
+	// Id of the request
+	//
+	// example:
+	//
+	// 6C6B99AC-39EC-5350-874C-204128C905E6
+	RequestId *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	// example:
+	//
+	// true
+	Success *bool `json:"Success,omitempty" xml:"Success,omitempty"`
+	// example:
+	//
+	// c3r127e325at9yd
+	TaskId *string `json:"TaskId,omitempty" xml:"TaskId,omitempty"`
+}
+
+func (s CreateLogoTaskResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s CreateLogoTaskResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *CreateLogoTaskResponseBody) SetErrorCode(v string) *CreateLogoTaskResponseBody {
+	s.ErrorCode = &v
+	return s
+}
+
+func (s *CreateLogoTaskResponseBody) SetErrorMsg(v string) *CreateLogoTaskResponseBody {
+	s.ErrorMsg = &v
+	return s
+}
+
+func (s *CreateLogoTaskResponseBody) SetRequestId(v string) *CreateLogoTaskResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *CreateLogoTaskResponseBody) SetSuccess(v bool) *CreateLogoTaskResponseBody {
+	s.Success = &v
+	return s
+}
+
+func (s *CreateLogoTaskResponseBody) SetTaskId(v string) *CreateLogoTaskResponseBody {
+	s.TaskId = &v
+	return s
+}
+
+type CreateLogoTaskResponse struct {
+	Headers    map[string]*string          `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                      `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *CreateLogoTaskResponseBody `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s CreateLogoTaskResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s CreateLogoTaskResponse) GoString() string {
+	return s.String()
+}
+
+func (s *CreateLogoTaskResponse) SetHeaders(v map[string]*string) *CreateLogoTaskResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *CreateLogoTaskResponse) SetStatusCode(v int32) *CreateLogoTaskResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *CreateLogoTaskResponse) SetBody(v *CreateLogoTaskResponseBody) *CreateLogoTaskResponse {
+	s.Body = v
+	return s
+}
+
+type GetCreateLogoTaskRequest struct {
+	// example:
+	//
+	// 20051349
+	TaskId *string `json:"TaskId,omitempty" xml:"TaskId,omitempty"`
+}
+
+func (s GetCreateLogoTaskRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetCreateLogoTaskRequest) GoString() string {
+	return s.String()
+}
+
+func (s *GetCreateLogoTaskRequest) SetTaskId(v string) *GetCreateLogoTaskRequest {
+	s.TaskId = &v
+	return s
+}
+
+type GetCreateLogoTaskResponseBody struct {
+	// example:
+	//
+	// 0
+	ErrorCode *string `json:"ErrorCode,omitempty" xml:"ErrorCode,omitempty"`
+	// example:
+	//
+	// aliuid:1998006665794443 assumeRole not exist,serviceName:realtimelogpush.dcdnservices.aliyuncs.com
+	ErrorMsg *string `json:"ErrorMsg,omitempty" xml:"ErrorMsg,omitempty"`
+	// Id of the request
+	//
+	// example:
+	//
+	// 6C6B99AC-39EC-5350-874C-204128C905E6
+	RequestId *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	// example:
+	//
+	// true
+	Success *bool                              `json:"Success,omitempty" xml:"Success,omitempty"`
+	Task    *GetCreateLogoTaskResponseBodyTask `json:"Task,omitempty" xml:"Task,omitempty" type:"Struct"`
+}
+
+func (s GetCreateLogoTaskResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetCreateLogoTaskResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *GetCreateLogoTaskResponseBody) SetErrorCode(v string) *GetCreateLogoTaskResponseBody {
+	s.ErrorCode = &v
+	return s
+}
+
+func (s *GetCreateLogoTaskResponseBody) SetErrorMsg(v string) *GetCreateLogoTaskResponseBody {
+	s.ErrorMsg = &v
+	return s
+}
+
+func (s *GetCreateLogoTaskResponseBody) SetRequestId(v string) *GetCreateLogoTaskResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+func (s *GetCreateLogoTaskResponseBody) SetSuccess(v bool) *GetCreateLogoTaskResponseBody {
+	s.Success = &v
+	return s
+}
+
+func (s *GetCreateLogoTaskResponseBody) SetTask(v *GetCreateLogoTaskResponseBodyTask) *GetCreateLogoTaskResponseBody {
+	s.Task = v
+	return s
+}
+
+type GetCreateLogoTaskResponseBodyTask struct {
+	// example:
+	//
+	// 604860995
+	TaskId *string `json:"TaskId,omitempty" xml:"TaskId,omitempty"`
+	// example:
+	//
+	// SUCCESS
+	TaskStatus *string   `json:"TaskStatus,omitempty" xml:"TaskStatus,omitempty"`
+	Urls       []*string `json:"Urls,omitempty" xml:"Urls,omitempty" type:"Repeated"`
+}
+
+func (s GetCreateLogoTaskResponseBodyTask) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetCreateLogoTaskResponseBodyTask) GoString() string {
+	return s.String()
+}
+
+func (s *GetCreateLogoTaskResponseBodyTask) SetTaskId(v string) *GetCreateLogoTaskResponseBodyTask {
+	s.TaskId = &v
+	return s
+}
+
+func (s *GetCreateLogoTaskResponseBodyTask) SetTaskStatus(v string) *GetCreateLogoTaskResponseBodyTask {
+	s.TaskStatus = &v
+	return s
+}
+
+func (s *GetCreateLogoTaskResponseBodyTask) SetUrls(v []*string) *GetCreateLogoTaskResponseBodyTask {
+	s.Urls = v
+	return s
+}
+
+type GetCreateLogoTaskResponse struct {
+	Headers    map[string]*string             `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                         `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *GetCreateLogoTaskResponseBody `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s GetCreateLogoTaskResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s GetCreateLogoTaskResponse) GoString() string {
+	return s.String()
+}
+
+func (s *GetCreateLogoTaskResponse) SetHeaders(v map[string]*string) *GetCreateLogoTaskResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *GetCreateLogoTaskResponse) SetStatusCode(v int32) *GetCreateLogoTaskResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *GetCreateLogoTaskResponse) SetBody(v *GetCreateLogoTaskResponseBody) *GetCreateLogoTaskResponse {
+	s.Body = v
+	return s
+}
+
 type OperateAppInstanceForPartnerRequest struct {
 	Extend *string `json:"Extend,omitempty" xml:"Extend,omitempty"`
 	// example:
@@ -278,6 +545,138 @@ func (client *Client) GetEndpoint(productId *string, regionId *string, endpointR
 	}
 
 	_body, _err := endpointutil.GetEndpointRules(productId, regionId, endpointRule, network, suffix)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+// Summary:
+//
+// 提交创建Logo任务
+//
+// @param request - CreateLogoTaskRequest
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return CreateLogoTaskResponse
+func (client *Client) CreateLogoTaskWithOptions(request *CreateLogoTaskRequest, runtime *util.RuntimeOptions) (_result *CreateLogoTaskResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	query := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.NegativePrompt)) {
+		query["NegativePrompt"] = request.NegativePrompt
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.Parameters)) {
+		query["Parameters"] = request.Parameters
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.Prompt)) {
+		query["Prompt"] = request.Prompt
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.Version)) {
+		query["Version"] = request.Version
+	}
+
+	req := &openapi.OpenApiRequest{
+		Query: openapiutil.Query(query),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("CreateLogoTask"),
+		Version:     tea.String("2025-04-29"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &CreateLogoTaskResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+// Summary:
+//
+// 提交创建Logo任务
+//
+// @param request - CreateLogoTaskRequest
+//
+// @return CreateLogoTaskResponse
+func (client *Client) CreateLogoTask(request *CreateLogoTaskRequest) (_result *CreateLogoTaskResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &CreateLogoTaskResponse{}
+	_body, _err := client.CreateLogoTaskWithOptions(request, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+// Summary:
+//
+// 查询Logo创建任务
+//
+// @param request - GetCreateLogoTaskRequest
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return GetCreateLogoTaskResponse
+func (client *Client) GetCreateLogoTaskWithOptions(request *GetCreateLogoTaskRequest, runtime *util.RuntimeOptions) (_result *GetCreateLogoTaskResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	query := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.TaskId)) {
+		query["TaskId"] = request.TaskId
+	}
+
+	req := &openapi.OpenApiRequest{
+		Query: openapiutil.Query(query),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("GetCreateLogoTask"),
+		Version:     tea.String("2025-04-29"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &GetCreateLogoTaskResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+// Summary:
+//
+// 查询Logo创建任务
+//
+// @param request - GetCreateLogoTaskRequest
+//
+// @return GetCreateLogoTaskResponse
+func (client *Client) GetCreateLogoTask(request *GetCreateLogoTaskRequest) (_result *GetCreateLogoTaskResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &GetCreateLogoTaskResponse{}
+	_body, _err := client.GetCreateLogoTaskWithOptions(request, runtime)
 	if _err != nil {
 		return _result, _err
 	}
