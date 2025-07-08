4e863af64bbcda47625e2aa723e331dc3f478656
sdk-team <sdk-team@alibabacloud.com>
2025-07-08 07:13:46 +0000

    Support API DescribeMetricLast.

--- eds-aic-20230930/client/client.go
+++ eds-aic-20230930/client/client.go
@@ -7825,6 +7825,306 @@ func (s *DescribeKeyPairsResponse) SetBody(v *DescribeKeyPairsResponseBody) *Des
 	return s
 }
 
+type DescribeMetricLastRequest struct {
+	AndroidInstanceIds []*string `json:"AndroidInstanceIds,omitempty" xml:"AndroidInstanceIds,omitempty" type:"Repeated"`
+	// example:
+	//
+	// 2019-01-31 11:00:00
+	EndTime *string `json:"EndTime,omitempty" xml:"EndTime,omitempty"`
+	// example:
+	//
+	// 1000
+	Length *string `json:"Length,omitempty" xml:"Length,omitempty"`
+	// This parameter is required.
+	MetricNames []*string `json:"MetricNames,omitempty" xml:"MetricNames,omitempty" type:"Repeated"`
+	// example:
+	//
+	// AAAAAV3MpHK1AP0pfERHZN5pu6l5V9uONHqPtDLM2U8s****
+	NextToken *string `json:"NextToken,omitempty" xml:"NextToken,omitempty"`
+	// example:
+	//
+	// 60
+	Period *int32 `json:"Period,omitempty" xml:"Period,omitempty"`
+	// example:
+	//
+	// 2019-01-31 10:00:00
+	StartTime *string `json:"StartTime,omitempty" xml:"StartTime,omitempty"`
+}
+
+func (s DescribeMetricLastRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeMetricLastRequest) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeMetricLastRequest) SetAndroidInstanceIds(v []*string) *DescribeMetricLastRequest {
+	s.AndroidInstanceIds = v
+	return s
+}
+
+func (s *DescribeMetricLastRequest) SetEndTime(v string) *DescribeMetricLastRequest {
+	s.EndTime = &v
+	return s
+}
+
+func (s *DescribeMetricLastRequest) SetLength(v string) *DescribeMetricLastRequest {
+	s.Length = &v
+	return s
+}
+
+func (s *DescribeMetricLastRequest) SetMetricNames(v []*string) *DescribeMetricLastRequest {
+	s.MetricNames = v
+	return s
+}
+
+func (s *DescribeMetricLastRequest) SetNextToken(v string) *DescribeMetricLastRequest {
+	s.NextToken = &v
+	return s
+}
+
+func (s *DescribeMetricLastRequest) SetPeriod(v int32) *DescribeMetricLastRequest {
+	s.Period = &v
+	return s
+}
+
+func (s *DescribeMetricLastRequest) SetStartTime(v string) *DescribeMetricLastRequest {
+	s.StartTime = &v
+	return s
+}
+
+type DescribeMetricLastResponseBody struct {
+	// example:
+	//
+	// 100
+	Count            *int32                                            `json:"Count,omitempty" xml:"Count,omitempty"`
+	MetricTotalModel []*DescribeMetricLastResponseBodyMetricTotalModel `json:"MetricTotalModel,omitempty" xml:"MetricTotalModel,omitempty" type:"Repeated"`
+	// example:
+	//
+	// AAAAAV3MpHK1AP0pfERHZN5pu6kU+SQXzm0H9mu/FiSc****
+	NextToken *string `json:"NextToken,omitempty" xml:"NextToken,omitempty"`
+	// example:
+	//
+	// 2B9E6946-0E2A-5D2B-B275-361DF81F****
+	RequestId *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+}
+
+func (s DescribeMetricLastResponseBody) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeMetricLastResponseBody) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeMetricLastResponseBody) SetCount(v int32) *DescribeMetricLastResponseBody {
+	s.Count = &v
+	return s
+}
+
+func (s *DescribeMetricLastResponseBody) SetMetricTotalModel(v []*DescribeMetricLastResponseBodyMetricTotalModel) *DescribeMetricLastResponseBody {
+	s.MetricTotalModel = v
+	return s
+}
+
+func (s *DescribeMetricLastResponseBody) SetNextToken(v string) *DescribeMetricLastResponseBody {
+	s.NextToken = &v
+	return s
+}
+
+func (s *DescribeMetricLastResponseBody) SetRequestId(v string) *DescribeMetricLastResponseBody {
+	s.RequestId = &v
+	return s
+}
+
+type DescribeMetricLastResponseBodyMetricTotalModel struct {
+	// example:
+	//
+	// acp-fkuit0cmyru4p****
+	AndroidInstanceId *string                                                          `json:"AndroidInstanceId,omitempty" xml:"AndroidInstanceId,omitempty"`
+	MetricModelList   []*DescribeMetricLastResponseBodyMetricTotalModelMetricModelList `json:"MetricModelList,omitempty" xml:"MetricModelList,omitempty" type:"Repeated"`
+}
+
+func (s DescribeMetricLastResponseBodyMetricTotalModel) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeMetricLastResponseBodyMetricTotalModel) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeMetricLastResponseBodyMetricTotalModel) SetAndroidInstanceId(v string) *DescribeMetricLastResponseBodyMetricTotalModel {
+	s.AndroidInstanceId = &v
+	return s
+}
+
+func (s *DescribeMetricLastResponseBodyMetricTotalModel) SetMetricModelList(v []*DescribeMetricLastResponseBodyMetricTotalModelMetricModelList) *DescribeMetricLastResponseBodyMetricTotalModel {
+	s.MetricModelList = v
+	return s
+}
+
+type DescribeMetricLastResponseBodyMetricTotalModelMetricModelList struct {
+	DataPoints []*DescribeMetricLastResponseBodyMetricTotalModelMetricModelListDataPoints `json:"DataPoints,omitempty" xml:"DataPoints,omitempty" type:"Repeated"`
+	// example:
+	//
+	// cpu_utilization
+	MetricName       *string                                                                          `json:"MetricName,omitempty" xml:"MetricName,omitempty"`
+	ProcessLastInfos []*DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos `json:"ProcessLastInfos,omitempty" xml:"ProcessLastInfos,omitempty" type:"Repeated"`
+}
+
+func (s DescribeMetricLastResponseBodyMetricTotalModelMetricModelList) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeMetricLastResponseBodyMetricTotalModelMetricModelList) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeMetricLastResponseBodyMetricTotalModelMetricModelList) SetDataPoints(v []*DescribeMetricLastResponseBodyMetricTotalModelMetricModelListDataPoints) *DescribeMetricLastResponseBodyMetricTotalModelMetricModelList {
+	s.DataPoints = v
+	return s
+}
+
+func (s *DescribeMetricLastResponseBodyMetricTotalModelMetricModelList) SetMetricName(v string) *DescribeMetricLastResponseBodyMetricTotalModelMetricModelList {
+	s.MetricName = &v
+	return s
+}
+
+func (s *DescribeMetricLastResponseBodyMetricTotalModelMetricModelList) SetProcessLastInfos(v []*DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos) *DescribeMetricLastResponseBodyMetricTotalModelMetricModelList {
+	s.ProcessLastInfos = v
+	return s
+}
+
+type DescribeMetricLastResponseBodyMetricTotalModelMetricModelListDataPoints struct {
+	// example:
+	//
+	// 99.52
+	Average *float64 `json:"Average,omitempty" xml:"Average,omitempty"`
+	// example:
+	//
+	// 100
+	Maximum *float64 `json:"Maximum,omitempty" xml:"Maximum,omitempty"`
+	// example:
+	//
+	// 93.1
+	Minimum *float64 `json:"Minimum,omitempty" xml:"Minimum,omitempty"`
+	// example:
+	//
+	// 1548777660000
+	Timestamp *int64 `json:"Timestamp,omitempty" xml:"Timestamp,omitempty"`
+}
+
+func (s DescribeMetricLastResponseBodyMetricTotalModelMetricModelListDataPoints) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeMetricLastResponseBodyMetricTotalModelMetricModelListDataPoints) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListDataPoints) SetAverage(v float64) *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListDataPoints {
+	s.Average = &v
+	return s
+}
+
+func (s *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListDataPoints) SetMaximum(v float64) *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListDataPoints {
+	s.Maximum = &v
+	return s
+}
+
+func (s *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListDataPoints) SetMinimum(v float64) *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListDataPoints {
+	s.Minimum = &v
+	return s
+}
+
+func (s *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListDataPoints) SetTimestamp(v int64) *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListDataPoints {
+	s.Timestamp = &v
+	return s
+}
+
+type DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos struct {
+	// example:
+	//
+	// 50
+	CpuUsage *float64 `json:"CpuUsage,omitempty" xml:"CpuUsage,omitempty"`
+	// example:
+	//
+	// 50
+	MemoryUsage *float64 `json:"MemoryUsage,omitempty" xml:"MemoryUsage,omitempty"`
+	// example:
+	//
+	// com.offerup
+	Name       *string  `json:"Name,omitempty" xml:"Name,omitempty"`
+	ProcessIds []*int32 `json:"ProcessIds,omitempty" xml:"ProcessIds,omitempty" type:"Repeated"`
+	// example:
+	//
+	// 1548777660000
+	Timestamp *int64 `json:"Timestamp,omitempty" xml:"Timestamp,omitempty"`
+}
+
+func (s DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos) SetCpuUsage(v float64) *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos {
+	s.CpuUsage = &v
+	return s
+}
+
+func (s *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos) SetMemoryUsage(v float64) *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos {
+	s.MemoryUsage = &v
+	return s
+}
+
+func (s *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos) SetName(v string) *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos {
+	s.Name = &v
+	return s
+}
+
+func (s *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos) SetProcessIds(v []*int32) *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos {
+	s.ProcessIds = v
+	return s
+}
+
+func (s *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos) SetTimestamp(v int64) *DescribeMetricLastResponseBodyMetricTotalModelMetricModelListProcessLastInfos {
+	s.Timestamp = &v
+	return s
+}
+
+type DescribeMetricLastResponse struct {
+	Headers    map[string]*string              `json:"headers,omitempty" xml:"headers,omitempty"`
+	StatusCode *int32                          `json:"statusCode,omitempty" xml:"statusCode,omitempty"`
+	Body       *DescribeMetricLastResponseBody `json:"body,omitempty" xml:"body,omitempty"`
+}
+
+func (s DescribeMetricLastResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s DescribeMetricLastResponse) GoString() string {
+	return s.String()
+}
+
+func (s *DescribeMetricLastResponse) SetHeaders(v map[string]*string) *DescribeMetricLastResponse {
+	s.Headers = v
+	return s
+}
+
+func (s *DescribeMetricLastResponse) SetStatusCode(v int32) *DescribeMetricLastResponse {
+	s.StatusCode = &v
+	return s
+}
+
+func (s *DescribeMetricLastResponse) SetBody(v *DescribeMetricLastResponseBody) *DescribeMetricLastResponse {
+	s.Body = v
+	return s
+}
+
 type DescribeRegionsRequest struct {
 	// The display language of the console. Valid values:
 	//
@@ -16113,6 +16413,90 @@ func (client *Client) DescribeKeyPairs(request *DescribeKeyPairsRequest) (_resul
 	runtime := &util.RuntimeOptions{}
 	_result = &DescribeKeyPairsResponse{}
 	_body, _err := client.DescribeKeyPairsWithOptions(request, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+// Summary:
+//
+// 查询指定监控项的最新监控数据
+//
+// @param request - DescribeMetricLastRequest
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return DescribeMetricLastResponse
+func (client *Client) DescribeMetricLastWithOptions(request *DescribeMetricLastRequest, runtime *util.RuntimeOptions) (_result *DescribeMetricLastResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	body := map[string]interface{}{}
+	if !tea.BoolValue(util.IsUnset(request.AndroidInstanceIds)) {
+		body["AndroidInstanceIds"] = request.AndroidInstanceIds
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.EndTime)) {
+		body["EndTime"] = request.EndTime
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.Length)) {
+		body["Length"] = request.Length
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.MetricNames)) {
+		body["MetricNames"] = request.MetricNames
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.NextToken)) {
+		body["NextToken"] = request.NextToken
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.Period)) {
+		body["Period"] = request.Period
+	}
+
+	if !tea.BoolValue(util.IsUnset(request.StartTime)) {
+		body["StartTime"] = request.StartTime
+	}
+
+	req := &openapi.OpenApiRequest{
+		Body: openapiutil.ParseToMap(body),
+	}
+	params := &openapi.Params{
+		Action:      tea.String("DescribeMetricLast"),
+		Version:     tea.String("2023-09-30"),
+		Protocol:    tea.String("HTTPS"),
+		Pathname:    tea.String("/"),
+		Method:      tea.String("POST"),
+		AuthType:    tea.String("AK"),
+		Style:       tea.String("RPC"),
+		ReqBodyType: tea.String("formData"),
+		BodyType:    tea.String("json"),
+	}
+	_result = &DescribeMetricLastResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+// Summary:
+//
+// 查询指定监控项的最新监控数据
+//
+// @param request - DescribeMetricLastRequest
+//
+// @return DescribeMetricLastResponse
+func (client *Client) DescribeMetricLast(request *DescribeMetricLastRequest) (_result *DescribeMetricLastResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	_result = &DescribeMetricLastResponse{}
+	_body, _err := client.DescribeMetricLastWithOptions(request, runtime)
 	if _err != nil {
 		return _result, _err
 	}
