5734a2040233fb131e6b7147fe9fe34e530d49c6
sdk-team <sdk-team@alibabacloud.com>
2025-11-25 18:11:20 +0000

    Update API ListTongyiConversationLogs: add response parameters Body.SessionFlowDebugInfo.

--- chatbot-20220408/client/list_tongyi_conversation_logs_response_body_model.go
+++ chatbot-20220408/client/list_tongyi_conversation_logs_response_body_model.go
@@ -15,6 +15,8 @@ type iListTongyiConversationLogsResponseBody interface {
 	GetDatas() []map[string]interface{}
 	SetRequestId(v string) *ListTongyiConversationLogsResponseBody
 	GetRequestId() *string
+	SetSessionFlowDebugInfo(v *ListTongyiConversationLogsResponseBodySessionFlowDebugInfo) *ListTongyiConversationLogsResponseBody
+	GetSessionFlowDebugInfo() *ListTongyiConversationLogsResponseBodySessionFlowDebugInfo
 }
 
 type ListTongyiConversationLogsResponseBody struct {
@@ -27,6 +29,9 @@ type ListTongyiConversationLogsResponseBody struct {
 	//
 	// 28805A7C-D695-548C-A31B-67E52C2C274F
 	RequestId *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
+	// if can be null:
+	// true
+	SessionFlowDebugInfo *ListTongyiConversationLogsResponseBodySessionFlowDebugInfo `json:"SessionFlowDebugInfo,omitempty" xml:"SessionFlowDebugInfo,omitempty" type:"Struct"`
 }
 
 func (s ListTongyiConversationLogsResponseBody) String() string {
@@ -49,6 +54,10 @@ func (s *ListTongyiConversationLogsResponseBody) GetRequestId() *string {
 	return s.RequestId
 }
 
+func (s *ListTongyiConversationLogsResponseBody) GetSessionFlowDebugInfo() *ListTongyiConversationLogsResponseBodySessionFlowDebugInfo {
+	return s.SessionFlowDebugInfo
+}
+
 func (s *ListTongyiConversationLogsResponseBody) SetCostTime(v string) *ListTongyiConversationLogsResponseBody {
 	s.CostTime = &v
 	return s
@@ -64,6 +73,51 @@ func (s *ListTongyiConversationLogsResponseBody) SetRequestId(v string) *ListTon
 	return s
 }
 
+func (s *ListTongyiConversationLogsResponseBody) SetSessionFlowDebugInfo(v *ListTongyiConversationLogsResponseBodySessionFlowDebugInfo) *ListTongyiConversationLogsResponseBody {
+	s.SessionFlowDebugInfo = v
+	return s
+}
+
 func (s *ListTongyiConversationLogsResponseBody) Validate() error {
+	if s.SessionFlowDebugInfo != nil {
+		if err := s.SessionFlowDebugInfo.Validate(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type ListTongyiConversationLogsResponseBodySessionFlowDebugInfo struct {
+	ApiParams  map[string]interface{} `json:"ApiParams,omitempty" xml:"ApiParams,omitempty"`
+	SlotParams map[string]interface{} `json:"SlotParams,omitempty" xml:"SlotParams,omitempty"`
+}
+
+func (s ListTongyiConversationLogsResponseBodySessionFlowDebugInfo) String() string {
+	return dara.Prettify(s)
+}
+
+func (s ListTongyiConversationLogsResponseBodySessionFlowDebugInfo) GoString() string {
+	return s.String()
+}
+
+func (s *ListTongyiConversationLogsResponseBodySessionFlowDebugInfo) GetApiParams() map[string]interface{} {
+	return s.ApiParams
+}
+
+func (s *ListTongyiConversationLogsResponseBodySessionFlowDebugInfo) GetSlotParams() map[string]interface{} {
+	return s.SlotParams
+}
+
+func (s *ListTongyiConversationLogsResponseBodySessionFlowDebugInfo) SetApiParams(v map[string]interface{}) *ListTongyiConversationLogsResponseBodySessionFlowDebugInfo {
+	s.ApiParams = v
+	return s
+}
+
+func (s *ListTongyiConversationLogsResponseBodySessionFlowDebugInfo) SetSlotParams(v map[string]interface{}) *ListTongyiConversationLogsResponseBodySessionFlowDebugInfo {
+	s.SlotParams = v
+	return s
+}
+
+func (s *ListTongyiConversationLogsResponseBodySessionFlowDebugInfo) Validate() error {
 	return dara.Validate(s)
 }
