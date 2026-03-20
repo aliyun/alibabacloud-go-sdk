fd4e3b4e75286d666686321eb8d3b4d031e45bad
sdk-team <sdk-team@alibabacloud.com>
2026-03-20 17:46:12 +0000

    Generated golang 2018-05-18 for nls-cloud-meta.

---
+++ nls-cloud-meta-20180518/client/client.go
@@ -0,0 +1,107 @@
+// This file is auto-generated, don't edit it. Thanks.
+package client
+
+import (
+	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
+	openapiutil "github.com/alibabacloud-go/darabonba-openapi/v2/utils"
+	"github.com/alibabacloud-go/tea/dara"
+)
+
+type Client struct {
+	openapi.Client
+	DisableSDKError *bool
+	EnableValidate  *bool
+}
+
+func NewClient(config *openapiutil.Config) (*Client, error) {
+	client := new(Client)
+	err := client.Init(config)
+	return client, err
+}
+
+func (client *Client) Init(config *openapiutil.Config) (_err error) {
+	_err = client.Client.Init(config)
+	if _err != nil {
+		return _err
+	}
+	client.EndpointRule = dara.String("")
+	_err = client.CheckConfig(config)
+	if _err != nil {
+		return _err
+	}
+	client.Endpoint, _err = client.GetEndpoint(dara.String("nls-cloud-meta"), client.RegionId, client.EndpointRule, client.Network, client.Suffix, client.EndpointMap, client.Endpoint)
+	if _err != nil {
+		return _err
+	}
+
+	return nil
+}
+
+func (client *Client) GetEndpoint(productId *string, regionId *string, endpointRule *string, network *string, suffix *string, endpointMap map[string]*string, endpoint *string) (_result *string, _err error) {
+	if !dara.IsNil(endpoint) {
+		_result = endpoint
+		return _result, _err
+	}
+
+	if !dara.IsNil(endpointMap) && !dara.IsNil(endpointMap[dara.StringValue(regionId)]) {
+		_result = endpointMap[dara.StringValue(regionId)]
+		return _result, _err
+	}
+
+	_body, _err := openapiutil.GetEndpointRules(productId, regionId, endpointRule, network, suffix)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+// Summary:
+//
+// 创建Token
+//
+// @param headers - map
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return CreateTokenResponse
+func (client *Client) CreateTokenWithOptions(headers map[string]*string, runtime *dara.RuntimeOptions) (_result *CreateTokenResponse, _err error) {
+	req := &openapiutil.OpenApiRequest{
+		Headers: headers,
+	}
+	params := &openapiutil.Params{
+		Action:      dara.String("CreateToken"),
+		Version:     dara.String("2018-05-18"),
+		Protocol:    dara.String("HTTPS"),
+		Pathname:    dara.String("/pop/2018-05-18/tokens"),
+		Method:      dara.String("POST"),
+		AuthType:    dara.String("AK"),
+		Style:       dara.String("ROA"),
+		ReqBodyType: dara.String("json"),
+		BodyType:    dara.String("json"),
+	}
+	_result = &CreateTokenResponse{}
+	_body, _err := client.CallApi(params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = dara.Convert(_body, &_result)
+	return _result, _err
+}
+
+// Summary:
+//
+// 创建Token
+//
+// @return CreateTokenResponse
+func (client *Client) CreateToken() (_result *CreateTokenResponse, _err error) {
+	runtime := &dara.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &CreateTokenResponse{}
+	_body, _err := client.CreateTokenWithOptions(headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
---
+++ nls-cloud-meta-20180518/client/client_context_func.go
@@ -0,0 +1,41 @@
+// This file is auto-generated, don't edit it. Thanks.
+package client
+
+import (
+	"context"
+	openapiutil "github.com/alibabacloud-go/darabonba-openapi/v2/utils"
+	"github.com/alibabacloud-go/tea/dara"
+)
+
+// Summary:
+//
+// 创建Token
+//
+// @param headers - map
+//
+// @param runtime - runtime options for this request RuntimeOptions
+//
+// @return CreateTokenResponse
+func (client *Client) CreateTokenWithContext(ctx context.Context, headers map[string]*string, runtime *dara.RuntimeOptions) (_result *CreateTokenResponse, _err error) {
+	req := &openapiutil.OpenApiRequest{
+		Headers: headers,
+	}
+	params := &openapiutil.Params{
+		Action:      dara.String("CreateToken"),
+		Version:     dara.String("2018-05-18"),
+		Protocol:    dara.String("HTTPS"),
+		Pathname:    dara.String("/pop/2018-05-18/tokens"),
+		Method:      dara.String("POST"),
+		AuthType:    dara.String("AK"),
+		Style:       dara.String("ROA"),
+		ReqBodyType: dara.String("json"),
+		BodyType:    dara.String("json"),
+	}
+	_result = &CreateTokenResponse{}
+	_body, _err := client.CallApiWithCtx(ctx, params, req, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = dara.Convert(_body, &_result)
+	return _result, _err
+}
