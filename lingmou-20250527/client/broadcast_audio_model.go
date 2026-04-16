9614bf4dd50bd172040ad10cf7077553edaba7bb
sdk-team <sdk-team@alibabacloud.com>
2026-04-16 01:58:43 +0000

    Generated golang 2025-05-27 for LingMou.

--- lingmou-20250527/client/broadcast_audio_model.go
+++ lingmou-20250527/client/broadcast_audio_model.go
@@ -13,6 +13,8 @@ type iBroadcastAudio interface {
 	GetAudioLength() *int32
 	SetCreateTime(v string) *BroadcastAudio
 	GetCreateTime() *string
+	SetErrorCode(v string) *BroadcastAudio
+	GetErrorCode() *string
 	SetId(v string) *BroadcastAudio
 	GetId() *string
 	SetModifiedTime(v string) *BroadcastAudio
@@ -32,6 +34,7 @@ type BroadcastAudio struct {
 	//
 	// 2026-01-22T01:59:03
 	CreateTime *string `json:"createTime,omitempty" xml:"createTime,omitempty"`
+	ErrorCode  *string `json:"errorCode,omitempty" xml:"errorCode,omitempty"`
 	// example:
 	//
 	// M1Ju6XhHog_e-lSeb80Slp9g
@@ -63,6 +66,10 @@ func (s *BroadcastAudio) GetCreateTime() *string {
 	return s.CreateTime
 }
 
+func (s *BroadcastAudio) GetErrorCode() *string {
+	return s.ErrorCode
+}
+
 func (s *BroadcastAudio) GetId() *string {
 	return s.Id
 }
@@ -86,6 +93,11 @@ func (s *BroadcastAudio) SetAudioLength(v int32) *BroadcastAudio {
 
 func (s *BroadcastAudio) SetCreateTime(v string) *BroadcastAudio {
 	s.CreateTime = &v
+	return s
+}
+
+func (s *BroadcastAudio) SetErrorCode(v string) *BroadcastAudio {
+	s.ErrorCode = &v
 	return s
 }
 
