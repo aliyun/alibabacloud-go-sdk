1563fba9cdc05730381946a9bd5b8c2c110a895b
sdk-team <sdk-team@alibabacloud.com>
2026-06-22 08:03:10 +0000

    Update API DescribeRegions: delete response parameters Body.regions.

--- apig-20240327/client/describe_regions_response_body_model.go
+++ apig-20240327/client/describe_regions_response_body_model.go
@@ -11,12 +11,10 @@ type iDescribeRegionsResponseBody interface {
 	GoString() string
 	SetCode(v string) *DescribeRegionsResponseBody
 	GetCode() *string
-	SetData(v *DescribeRegionsResponseBodyData) *DescribeRegionsResponseBody
-	GetData() *DescribeRegionsResponseBodyData
+	SetData(v []*DescribeRegionsResponseBodyData) *DescribeRegionsResponseBody
+	GetData() []*DescribeRegionsResponseBodyData
 	SetMessage(v string) *DescribeRegionsResponseBody
 	GetMessage() *string
-	SetRegions(v *DescribeRegionsResponseBodyRegions) *DescribeRegionsResponseBody
-	GetRegions() *DescribeRegionsResponseBodyRegions
 	SetRequestId(v string) *DescribeRegionsResponseBody
 	GetRequestId() *string
 }
@@ -25,14 +23,12 @@ type DescribeRegionsResponseBody struct {
 	// example:
 	//
 	// 200
-	Code *string                          `json:"code,omitempty" xml:"code,omitempty"`
-	Data *DescribeRegionsResponseBodyData `json:"data,omitempty" xml:"data,omitempty" type:"Struct"`
+	Code *string                            `json:"code,omitempty" xml:"code,omitempty"`
+	Data []*DescribeRegionsResponseBodyData `json:"data,omitempty" xml:"data,omitempty" type:"Repeated"`
 	// example:
 	//
 	// success
 	Message *string `json:"message,omitempty" xml:"message,omitempty"`
-	// Deprecated
-	Regions *DescribeRegionsResponseBodyRegions `json:"regions,omitempty" xml:"regions,omitempty" type:"Struct"`
 	// example:
 	//
 	// E6BD6C79-32B1-5D7E-A89A-93C5A6B7xxxx
@@ -51,16 +47,12 @@ func (s *DescribeRegionsResponseBody) GetCode() *string {
 	return s.Code
 }
 
-func (s *DescribeRegionsResponseBody) GetData() *DescribeRegionsResponseBodyData {
+func (s *DescribeRegionsResponseBody) GetData() []*DescribeRegionsResponseBodyData {
 	return s.Data
 }
 
 func (s *DescribeRegionsResponseBody) GetMessage() *string {
 	return s.Message
-}
-
-func (s *DescribeRegionsResponseBody) GetRegions() *DescribeRegionsResponseBodyRegions {
-	return s.Regions
 }
 
 func (s *DescribeRegionsResponseBody) GetRequestId() *string {
@@ -72,18 +64,13 @@ func (s *DescribeRegionsResponseBody) SetCode(v string) *DescribeRegionsResponse
 	return s
 }
 
-func (s *DescribeRegionsResponseBody) SetData(v *DescribeRegionsResponseBodyData) *DescribeRegionsResponseBody {
+func (s *DescribeRegionsResponseBody) SetData(v []*DescribeRegionsResponseBodyData) *DescribeRegionsResponseBody {
 	s.Data = v
 	return s
 }
 
 func (s *DescribeRegionsResponseBody) SetMessage(v string) *DescribeRegionsResponseBody {
 	s.Message = &v
-	return s
-}
-
-func (s *DescribeRegionsResponseBody) SetRegions(v *DescribeRegionsResponseBodyRegions) *DescribeRegionsResponseBody {
-	s.Regions = v
 	return s
 }
 
@@ -94,20 +81,20 @@ func (s *DescribeRegionsResponseBody) SetRequestId(v string) *DescribeRegionsRes
 
 func (s *DescribeRegionsResponseBody) Validate() error {
 	if s.Data != nil {
-		if err := s.Data.Validate(); err != nil {
-			return err
-		}
-	}
-	if s.Regions != nil {
-		if err := s.Regions.Validate(); err != nil {
-			return err
+		for _, item := range s.Data {
+			if item != nil {
+				if err := item.Validate(); err != nil {
+					return err
+				}
+			}
 		}
 	}
 	return nil
 }
 
 type DescribeRegionsResponseBodyData struct {
-	Regions []*DescribeRegionsResponseBodyDataRegions `json:"regions,omitempty" xml:"regions,omitempty" type:"Repeated"`
+	LocalName *string `json:"localName,omitempty" xml:"localName,omitempty"`
+	RegionId  *string `json:"regionId,omitempty" xml:"regionId,omitempty"`
 }
 
 func (s DescribeRegionsResponseBodyData) String() string {
@@ -118,134 +105,24 @@ func (s DescribeRegionsResponseBodyData) GoString() string {
 	return s.String()
 }
 
-func (s *DescribeRegionsResponseBodyData) GetRegions() []*DescribeRegionsResponseBodyDataRegions {
-	return s.Regions
+func (s *DescribeRegionsResponseBodyData) GetLocalName() *string {
+	return s.LocalName
 }
 
-func (s *DescribeRegionsResponseBodyData) SetRegions(v []*DescribeRegionsResponseBodyDataRegions) *DescribeRegionsResponseBodyData {
-	s.Regions = v
+func (s *DescribeRegionsResponseBodyData) GetRegionId() *string {
+	return s.RegionId
+}
+
+func (s *DescribeRegionsResponseBodyData) SetLocalName(v string) *DescribeRegionsResponseBodyData {
+	s.LocalName = &v
+	return s
+}
+
+func (s *DescribeRegionsResponseBodyData) SetRegionId(v string) *DescribeRegionsResponseBodyData {
+	s.RegionId = &v
 	return s
 }
 
 func (s *DescribeRegionsResponseBodyData) Validate() error {
-	if s.Regions != nil {
-		for _, item := range s.Regions {
-			if item != nil {
-				if err := item.Validate(); err != nil {
-					return err
-				}
-			}
-		}
-	}
-	return nil
-}
-
-type DescribeRegionsResponseBodyDataRegions struct {
-	LocalName *string `json:"localName,omitempty" xml:"localName,omitempty"`
-	RegionId  *string `json:"regionId,omitempty" xml:"regionId,omitempty"`
-}
-
-func (s DescribeRegionsResponseBodyDataRegions) String() string {
-	return dara.Prettify(s)
-}
-
-func (s DescribeRegionsResponseBodyDataRegions) GoString() string {
-	return s.String()
-}
-
-func (s *DescribeRegionsResponseBodyDataRegions) GetLocalName() *string {
-	return s.LocalName
-}
-
-func (s *DescribeRegionsResponseBodyDataRegions) GetRegionId() *string {
-	return s.RegionId
-}
-
-func (s *DescribeRegionsResponseBodyDataRegions) SetLocalName(v string) *DescribeRegionsResponseBodyDataRegions {
-	s.LocalName = &v
-	return s
-}
-
-func (s *DescribeRegionsResponseBodyDataRegions) SetRegionId(v string) *DescribeRegionsResponseBodyDataRegions {
-	s.RegionId = &v
-	return s
-}
-
-func (s *DescribeRegionsResponseBodyDataRegions) Validate() error {
-	return dara.Validate(s)
-}
-
-type DescribeRegionsResponseBodyRegions struct {
-	Region []*DescribeRegionsResponseBodyRegionsRegion `json:"Region,omitempty" xml:"Region,omitempty" type:"Repeated"`
-}
-
-func (s DescribeRegionsResponseBodyRegions) String() string {
-	return dara.Prettify(s)
-}
-
-func (s DescribeRegionsResponseBodyRegions) GoString() string {
-	return s.String()
-}
-
-func (s *DescribeRegionsResponseBodyRegions) GetRegion() []*DescribeRegionsResponseBodyRegionsRegion {
-	return s.Region
-}
-
-func (s *DescribeRegionsResponseBodyRegions) SetRegion(v []*DescribeRegionsResponseBodyRegionsRegion) *DescribeRegionsResponseBodyRegions {
-	s.Region = v
-	return s
-}
-
-func (s *DescribeRegionsResponseBodyRegions) Validate() error {
-	if s.Region != nil {
-		for _, item := range s.Region {
-			if item != nil {
-				if err := item.Validate(); err != nil {
-					return err
-				}
-			}
-		}
-	}
-	return nil
-}
-
-type DescribeRegionsResponseBodyRegionsRegion struct {
-	// example:
-	//
-	// 华东1（杭州）
-	LocalName *string `json:"localName,omitempty" xml:"localName,omitempty"`
-	// example:
-	//
-	// cn-hangzhou
-	RegionId *string `json:"regionId,omitempty" xml:"regionId,omitempty"`
-}
-
-func (s DescribeRegionsResponseBodyRegionsRegion) String() string {
-	return dara.Prettify(s)
-}
-
-func (s DescribeRegionsResponseBodyRegionsRegion) GoString() string {
-	return s.String()
-}
-
-func (s *DescribeRegionsResponseBodyRegionsRegion) GetLocalName() *string {
-	return s.LocalName
-}
-
-func (s *DescribeRegionsResponseBodyRegionsRegion) GetRegionId() *string {
-	return s.RegionId
-}
-
-func (s *DescribeRegionsResponseBodyRegionsRegion) SetLocalName(v string) *DescribeRegionsResponseBodyRegionsRegion {
-	s.LocalName = &v
-	return s
-}
-
-func (s *DescribeRegionsResponseBodyRegionsRegion) SetRegionId(v string) *DescribeRegionsResponseBodyRegionsRegion {
-	s.RegionId = &v
-	return s
-}
-
-func (s *DescribeRegionsResponseBodyRegionsRegion) Validate() error {
 	return dara.Validate(s)
 }
